9f52f47f820ca886b96e94eec5876ad608321413
David Mejia <davidsmejia@gmail.com>
2021-08-19 11:31:29 -0400

    implements basic sentry functionality

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import * as Sentry from "@sentry/react";
+import Error from "./_error";
+
+const Fallback = (sentry) => {
+  return <Error sentry={sentry} />;
+};
 
 const Portal = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  // configuring sentry
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.SENTRY_ENV,
+  });
 
-export default Portal
+  return (
+    <Sentry.ErrorBoundary fallback={Fallback} showDialog>
+      <Component {...pageProps} />
+    </Sentry.ErrorBoundary>
+  );
+};
+
+export default Portal;
