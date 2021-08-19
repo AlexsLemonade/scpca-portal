765a14146abd59373f6d6ca07c153e79dcbdb638
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-08-19 11:01:04 -0400

    Remove comment I no longer agree with.

--- api/scpca_portal/config/__init__.py
+++ api/scpca_portal/config/__init__.py
@@ -1,7 +1,5 @@
 import os
 
-# Having to do this kinda seems to defeat the purpose of this pattern,
-# but at least it's contained to this init file.
 DJANGO_CONFIGURATION = os.environ.get("DJANGO_CONFIGURATION", "Local")
 if DJANGO_CONFIGURATION == "Local":
     from scpca_portal.config.local import Local  # noqa
