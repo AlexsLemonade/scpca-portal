5eaa5149aaa11518c2ce1372ba25c6c0f8c3884a
David Mejia <davidsmejia@gmail.com>
2021-08-19 16:37:31 -0400

    clean up this file but will be filled out soon

--- client/next.config.js
+++ client/next.config.js
@@ -1,21 +1,19 @@
-const path = require("path");
-
 module.exports = () => {
-  const isProduction = process.env.VERCEL_GIT_COMMIT_REF === "master";
+  const isProduction = process.env.VERCEL_GIT_COMMIT_REF === 'master'
 
   const productionEnv = {
     SENTRY_DSN: process.env.SENTRY_DSN,
-    SENTRY_ENV: process.env.SENTRY_ENV,
-  };
+    SENTRY_ENV: process.env.SENTRY_ENV
+  }
 
   const stageEnv = {
     SENTRY_DSN: process.env.STAGE_SENTRY_DSN,
-    SENTRY_ENV: process.env.STAGE_SENTRY_ENV,
-  };
+    SENTRY_ENV: process.env.STAGE_SENTRY_ENV
+  }
 
-  const env = isProduction ? productionEnv : stageEnv;
+  const env = isProduction ? productionEnv : stageEnv
 
   return {
-    env,
-  };
-};
+    env
+  }
+}
