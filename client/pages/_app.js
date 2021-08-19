e3e1dd10070424fc4ec49fb52d53d6c5e7d61524
David Mejia <davidsmejia@gmail.com>
2021-08-19 16:27:44 -0400

    fix linter

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,23 +1,27 @@
-import "../styles/globals.css";
-import * as Sentry from "@sentry/react";
-import Error from "./_error";
+import React from 'react'
+import '../styles/globals.css'
+import * as Sentry from '@sentry/react'
+import Error from './_error'
 
 const Fallback = (sentry) => {
-  return <Error sentry={sentry} />;
-};
+  return <Error sentry={sentry} />
+}
 
 const Portal = ({ Component, pageProps }) => {
   // configuring sentry
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENV,
-  });
+    environment: process.env.SENTRY_ENV
+  })
 
   return (
     <Sentry.ErrorBoundary fallback={Fallback} showDialog>
-      <Component {...pageProps} />
+      <>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </>
     </Sentry.ErrorBoundary>
-  );
-};
+  )
+}
 
-export default Portal;
+export default Portal
