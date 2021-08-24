5619c92bb2d4cd1cb58881401da036ee0fad7202
David Mejia <davidsmejia@gmail.com>
2021-08-24 11:54:24 -0400

    adds hotjar

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -32,6 +32,15 @@ export default class MyDocument extends Document {
                 "window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);} gtag('js', new Date());gtag('config', 'G-3YR7L2222E');"
             }}
           />
+          {/* Hotjar */}
+          <script
+            async
+            //  eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html:
+                "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2569460,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
+            }}
+          />
           {this.props.styleTags}
         </Head>
         <body>
