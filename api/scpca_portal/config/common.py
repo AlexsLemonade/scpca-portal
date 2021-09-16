0308d6a994c82a273b17b96a8a2504da13dd8e33
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-09-16 13:56:33 -0400

    Why are django headers listed lowercase?

--- api/scpca_portal/config/common.py
+++ api/scpca_portal/config/common.py
@@ -150,6 +150,6 @@ class Common(Configuration):
     }
     # CORS - unrestricted
     CORS_ORIGIN_ALLOW_ALL = True
-    CORS_ALLOW_HEADERS = default_headers + ("API-KEY",)
+    CORS_ALLOW_HEADERS = default_headers + ("api-key",)
 
     TERMS_AND_CONDITIONS = "PLACEHOLDER"
