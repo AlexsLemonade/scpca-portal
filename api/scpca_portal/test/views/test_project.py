250817c931fdd65ce121d10096a2bbba9a6bf6e3
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-09-21 17:06:47 -0400

    Fix test now that bulk isn't in modalities

--- api/scpca_portal/test/views/test_project.py
+++ api/scpca_portal/test/views/test_project.py
@@ -22,7 +22,7 @@ class ProjectsTestCase(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_json = response.json()
-        self.assertEqual(response_json["modalities"], "CITE-seq, Bulk RNA-seq")
+        self.assertEqual(response_json["modalities"], "CITE-seq")
         self.assertEqual(response_json["computed_file"]["size_in_bytes"], 100)
 
     def test_get_list(self):
