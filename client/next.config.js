cc3944240176fbde69ba2be838581d25aadf5bb8
David Mejia <davidsmejia@gmail.com>
2021-09-22 15:25:48 -0400

    fix env vars

--- client/next.config.js
+++ client/next.config.js
@@ -2,14 +2,20 @@ module.exports = () => {
   const isProduction = process.env.VERCEL_GIT_COMMIT_REF === 'master'
 
   const productionEnv = {
+    API_HOST: process.env.API_HOST,
+    API_VERSION: process.env.API_VERSION,
     SENTRY_DSN: process.env.SENTRY_DSN,
     SENTRY_ENV: process.env.SENTRY_ENV
   }
 
   const stageEnv = {
+    API_HOST: process.env.STAGE_API_HOST,
+    API_VERSION: process.env.STAGE_API_VERSION,
     SENTRY_DSN: process.env.STAGE_SENTRY_DSN,
     SENTRY_ENV: process.env.STAGE_SENTRY_ENV
   }
+
+  console.log(stageEnv)
 
   const env = isProduction ? productionEnv : stageEnv
 
