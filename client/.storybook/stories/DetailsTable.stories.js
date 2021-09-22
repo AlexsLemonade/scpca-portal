23c5d7f26383f6c03ecb053d6196a3fcdde2e557
David Mejia <davidsmejia@gmail.com>
2021-09-22 12:17:13 -0400

    add better DetailsTable default example

--- client/.storybook/stories/DetailsTable.stories.js
+++ client/.storybook/stories/DetailsTable.stories.js
@@ -3,16 +3,21 @@ import { Box } from 'grommet'
 import { DetailsTable } from 'components/DetailsTable'
 import project from '../data/project'
 
+const defaultData = [
+  { label: 'String', value: 'This is a string.' },
+  { label: 'Array', value: ['This', 'is', 'an', 'array'] },
+  { label: 'Null', value: null },
+  { label: 'Undefined', value: undefined }
+]
+
 const order = ['abstract', 'disease_timings', 'human_readable_pi_name']
 
 export default {
   title: 'Components/DetailsTable',
-  args: { order, data: project }
+  args: { order, data: project, defaultData }
 }
 
-export const Default = (args) => (
-  <DetailsTable data={args.data} order={args.order} />
-)
+export const Default = (args) => <DetailsTable data={args.defaultData} />
 
 export const UsingOrder = (args) => (
   <DetailsTable data={args.data} order={args.order} />
