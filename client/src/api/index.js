41f7c6de3cd82be78f389ef9198d15a4b1ee44ea
David Mejia <davidsmejia@gmail.com>
2021-09-27 15:39:44 -0400

    add filter options to api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,10 +2,12 @@ import computedFiles from './resources/computedFiles'
 import projects from './resources/projects'
 import samples from './resources/samples'
 import tokens from './resources/tokens'
+import options from './resources/options'
 
 export default {
   computedFiles,
   projects,
   samples,
-  tokens
+  tokens,
+  options
 }
