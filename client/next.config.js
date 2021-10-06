87009695b12d86f7863b6faddc7a000f1bfc4937
David Mejia <davidsmejia@gmail.com>
2021-10-06 11:55:37 -0400

    add hubspot portal id to env vars

--- client/next.config.js
+++ client/next.config.js
@@ -5,14 +5,16 @@ module.exports = () => {
     API_HOST: process.env.API_HOST,
     API_VERSION: process.env.API_VERSION,
     SENTRY_DSN: process.env.SENTRY_DSN,
-    SENTRY_ENV: process.env.SENTRY_ENV
+    SENTRY_ENV: process.env.SENTRY_ENV,
+    HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID
   }
 
   const stageEnv = {
     API_HOST: process.env.STAGE_API_HOST,
     API_VERSION: process.env.STAGE_API_VERSION,
     SENTRY_DSN: process.env.STAGE_SENTRY_DSN,
-    SENTRY_ENV: process.env.STAGE_SENTRY_ENV
+    SENTRY_ENV: process.env.STAGE_SENTRY_ENV,
+    HUBSPOT_PORTAL_ID: process.env.STAGE_HUBSPOT_PORTAL_ID
   }
 
   const env = isProduction ? productionEnv : stageEnv
