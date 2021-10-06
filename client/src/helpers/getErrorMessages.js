fd8bfd32d11ea28c07c8f8113d9dbbffd8673a60
David Mejia <davidsmejia@gmail.com>
2021-10-06 12:00:14 -0400

    add helper for yup validation errors

---
+++ client/src/helpers/getErrorMessages.js
@@ -0,0 +1,21 @@
+import { getReadable } from 'helpers/getReadable'
+
+// convert Yup.js validationError object to error messages object
+export default (validationError) => {
+  const messages = {}
+  validationError.inner.forEach(({ path, errors }) => {
+    let message = messages
+    path.split('.').forEach((attribute, index, arr) => {
+      message[attribute] = message[attribute] || {}
+      if (index === arr.length - 1) {
+        message[attribute] = errors.map((error) =>
+          error.replace(path, getReadable(attribute))
+        )
+      } else {
+        message = message[attribute]
+      }
+    })
+  })
+
+  return messages
+}
