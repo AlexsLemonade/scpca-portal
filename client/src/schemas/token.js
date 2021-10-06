af05716aca2fc8685affe728d8b2b9aea274dd44
David Mejia <davidsmejia@gmail.com>
2021-10-06 15:43:00 -0400

    make fields required

--- client/src/schemas/token.js
+++ client/src/schemas/token.js
@@ -1,6 +1,8 @@
 import { boolean, string, object } from 'yup'
 
 export default object({
-  email: string().email('Please check your email.'),
-  is_activated: boolean().oneOf([true], 'Please accept the terms of service.')
+  email: string().required().email('Please check your email.'),
+  is_activated: boolean()
+    .required()
+    .oneOf([true], 'Please accept the terms of service.')
 })
