fb2da4c27ea167fe029b9d10ad5ed193ca710264
David Mejia <davidsmejia@gmail.com>
2021-10-06 11:57:57 -0400

    add stats to api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,11 +3,13 @@ import projects from './resources/projects'
 import samples from './resources/samples'
 import tokens from './resources/tokens'
 import options from './resources/options'
+import stats from './resources/stats'
 
 export default {
   computedFiles,
   projects,
   samples,
   tokens,
-  options
+  options,
+  stats
 }
