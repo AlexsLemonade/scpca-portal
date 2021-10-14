736957e18510b2a91b88d8aef9bd6ee1d9d0733e
David Mejia <davidsmejia@gmail.com>
2021-10-14 17:16:40 -0400

    add filterset_class to projects view

--- api/scpca_portal/views/project.py
+++ api/scpca_portal/views/project.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from django_filters import rest_framework as filters
 
 from scpca_portal.models import Project
 from scpca_portal.serializers import ProjectSerializer, SampleSerializer
@@ -10,25 +11,27 @@ class ProjectDetailSerializer(ProjectSerializer):
     samples = SampleSerializer(many=True, read_only=True)
 
 
+class ProjectFilter(filters.FilterSet):
+    diagnoses = filters.CharFilter(field_name="diagnoses", lookup_expr="icontains")
+    seq_units = filters.CharFilter(field_name="seq_units", lookup_expr="icontains")
+    technologies = filters.CharFilter(field_name="technologies", lookup_expr="icontains")
+    disease_timings = filters.CharFilter(field_name="disease_timings", lookup_expr="icontains")
+    human_readable_pi_name = filters.CharFilter(
+        field_name="human_readable_pi_name", lookup_expr="icontains"
+    )
+    title = filters.CharFilter(field_name="title", lookup_expr="icontains")
+    abstract = filters.CharFilter(field_name="abstract", lookup_expr="icontains")
+
+    class Meta:
+        model = Project
+        fields = ["scpca_id", "pi_name", "has_bulk_rna_seq", "has_spatial_data"]
+
+
 class ProjectViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Project.objects.all().order_by("-created_at")
     ordering_fields = "__all__"
     lookup_field = "scpca_id"
-
-    filterset_fields = {
-        "scpca_id": ["exact"],
-        "pi_name": ["exact"],
-        "human_readable_pi_name": ["icontains"],
-        "has_bulk_rna_seq": ["exact"],
-        "has_cite_seq_data": ["exact"],
-        "has_spatial_data": ["exact"],
-        "title": ["contains"],
-        "abstract": ["contains"],
-        "technologies": ["exact", "contains"],
-        "diagnoses": ["exact", "contains"],
-        "seq_units": ["exact", "contains"],
-        "disease_timings": ["exact", "contains"],
-    }
+    filterset_class = ProjectFilter
 
     def get_serializer_class(self):
         if self.action == "list":
