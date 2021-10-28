679f461d513e5384f262e848d8d6d9d409a71d74
David Mejia <davidsmejia@gmail.com>
2021-10-28 16:31:15 -0400

    manually clean up terms add some styles

--- client/src/components/MarkdownPage.js
+++ client/src/components/MarkdownPage.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Box, Markdown } from 'grommet'
+import { Box, Paragraph, Text, Markdown } from 'grommet'
+import styled from 'styled-components'
+
+const StyledLi = styled(Box)`
+  list-style: revert;
+  padding-left: 20px;
+`
+
+const StyledOl = styled(Box)`
+  list-style: revert;
+`
 
 export const MarkdownPage = ({ markdown }) => {
+  const components = {
+    p: { component: Paragraph, props: { margin: { bottom: 'medium' } } },
+    strong: { component: Text, props: { weight: 'bold' } },
+    ol: { component: StyledOl, props: { as: 'ol' } },
+    li: { component: StyledLi, props: { as: 'li' } }
+  }
+
   if (!markdown) return 'missing'
   return (
     <Box pad={{ vertical: 'large' }} justify="center">
       <Box width="large">
-        <Markdown>{markdown}</Markdown>
+        <Markdown components={components}>{markdown}</Markdown>
       </Box>
     </Box>
   )
