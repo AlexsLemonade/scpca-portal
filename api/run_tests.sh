303ac67ba668b2c05b15e387645beafb021c8e54
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-11-09 10:14:19 -0500

    Grasping at straws.

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -2,7 +2,8 @@
 
 python wait_for_postgres.py
 mkdir /home/user/code/test_data/
+export AWS_DEFAULT_REGION=us-east-1
 aws s3 ls s3://scpca-portal-inputs
-aws s3 sync --delete s3://scpca-portal-inputs /home/user/code/test_data/
+aws s3 sync --delete s3://scpca-portal-public-test-inputs /home/user/code/test_data/
 coverage run ./manage.py test -v 2 --no-input "$@"
 coverage report -m
