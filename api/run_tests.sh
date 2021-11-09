1723c844bc5335eedc464feca51eb7a7e05ae73e
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-11-09 09:55:03 -0500

    Does s3 ls even work? Maybe it'll have better output...

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -2,6 +2,7 @@
 
 python wait_for_postgres.py
 mkdir /home/user/code/test_data/
+aws s3 ls s3://scpca-portal-inputs
 aws s3 sync --delete s3://scpca-portal-inputs /home/user/code/test_data/
 coverage run ./manage.py test -v 2 --no-input "$@"
 coverage report -m
