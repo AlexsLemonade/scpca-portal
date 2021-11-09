7c343759c5805a004f0c8243fd99afa4e5b5e7f6
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-11-09 10:24:05 -0500

    Try no-sign-request.

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -3,7 +3,7 @@
 python wait_for_postgres.py
 mkdir /home/user/code/test_data/
 export AWS_DEFAULT_REGION=us-east-1
-aws s3 ls s3://scpca-portal-public-test-inputs
-aws s3 sync --delete s3://scpca-portal-public-test-inputs /home/user/code/test_data/
+aws s3 ls s3://scpca-portal-public-test-inputs --no-sign-request
+aws s3 sync --delete s3://scpca-portal-public-test-inputs /home/user/code/test_data/ --no-sign-request
 coverage run ./manage.py test -v 2 --no-input "$@"
 coverage report -m
