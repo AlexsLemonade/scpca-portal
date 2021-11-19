5b85acbd01ba1813f6a5655ce4de905713d3b601
Kurt Wheeler <kurt.wheeler91@gmail.com>
2021-11-19 12:18:51 -0500

    Just about there, just gotta figure out expires.

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -18,3 +18,24 @@ resource "aws_s3_bucket_public_access_block" "scpca_portal_bucket" {
   block_public_acls   = true
   block_public_policy = true
 }
+
+resource "aws_s3_bucket" "scpca_portal_cert_bucket" {
+  bucket = "scpca-portal-cert-${var.user}-${var.stage}"
+  acl = "private"
+  force_destroy = var.stage == "prod" ? false : true
+
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "scpca-portal-cert-${var.user}-${var.stage}"
+      Environment = var.stage
+    }
+  )
+}
+
+resource "aws_s3_bucket_public_access_block" "scpca_portal_cert_bucket" {
+  bucket = aws_s3_bucket.scpca_portal_cert_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
