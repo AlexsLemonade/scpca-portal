dd35b27b8d6415f294359afec9e07bb116a5f1e6
David Mejia <davidsmejia@gmail.com>
2022-01-28 15:43:17 -0500

    make header responsive

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Box, Header as GrommetHeader, Nav, ResponsiveContext } from 'grommet'
+import { Box, Header as GrommetHeader, Nav } from 'grommet'
+import { useResponsive } from 'hooks/useResponsive'
 import { DonateButton } from 'components/DonateButton'
 import { Link } from 'components/Link'
 import Logo from 'components/Logo'
+import { ResponsiveSheet } from 'components/ResponsiveSheet'
+import { Menu } from 'grommet-icons'
 import { config } from 'config'
 
 export const Header = ({ className, margin, donate = false }) => {
-  const size = React.useContext(ResponsiveContext)
+  const { size, responsive } = useResponsive()
+  const linksColor = responsive('brand', 'white')
+  const [showMenu, setShowMenu] = React.useState(false)
+
   return (
     <GrommetHeader
       className={className}
@@ -20,23 +26,35 @@ export const Header = ({ className, margin, donate = false }) => {
         width={{ max: 'xlarge' }}
         fill="horizontal"
         justify="between"
+        align="center"
       >
         <Box direction="row" align="center" gap="small">
           <Link href="/">
             <Logo />
           </Link>
         </Box>
-        <Nav
-          direction="row"
-          gap={size === 'large' ? 'xlarge' : 'medium'}
-          align="center"
-        >
-          <Link color="white" href="/" label="Home" />
-          <Link color="white" href="/about" label="About" />
-          <Link color="white" href="/projects" label="Browse" />
-          <Link color="white" href={config.links.help} label="Docs" />
-          {donate && <DonateButton yellow />}
-        </Nav>
+        <Box direction="row" gap={responsive('small', 'xlarge')}>
+          {size === 'small' && <Nav>{donate && <DonateButton yellow />}</Nav>}
+          <ResponsiveSheet
+            label={<Menu />}
+            show={showMenu}
+            setShow={setShowMenu}
+            alignSelf="center"
+          >
+            <Nav
+              direction={responsive('column', 'row')}
+              gap="xlarge"
+              align={responsive('start', 'center')}
+              width={responsive({ min: '300px' })}
+            >
+              <Link color={linksColor} href="/" label="Home" />
+              <Link color={linksColor} href="/about" label="About" />
+              <Link color={linksColor} href="/projects" label="Browse" />
+              <Link color={linksColor} href={config.links.help} label="Docs" />
+            </Nav>
+          </ResponsiveSheet>
+          {size !== 'small' && <Nav>{donate && <DonateButton yellow />}</Nav>}
+        </Box>
       </Box>
     </GrommetHeader>
   )
