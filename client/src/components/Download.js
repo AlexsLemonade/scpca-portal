6f5bb2f6dc1fbafb7f0346f9540f9cb42dfc942a
David Mejia <davidsmejia@gmail.com>
2022-02-08 15:58:11 -0500

    rework download modal

--- client/src/components/Download.js
+++ client/src/components/Download.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Anchor, Box, Text, FormField, TextInput, CheckBox } from 'grommet'
+import {
+  Anchor,
+  Box,
+  Grid,
+  Text,
+  FormField,
+  TextInput,
+  CheckBox
+} from 'grommet'
 import { Button } from 'components/Button'
 import { Modal } from 'components/Modal'
 import { Link } from 'components/Link'
@@ -8,6 +16,7 @@ import { AnalyticsContext } from 'contexts/AnalyticsContext'
 import { api } from 'api'
 import { formatBytes } from 'helpers/formatBytes'
 import { config } from 'config'
+import { useResponsive } from 'hooks/useResponsive'
 import DownloadSVG from '../images/download-folder.svg'
 
 // label for the checkbox needs to be component to show links
@@ -120,10 +129,13 @@ export const DownloadView = ({ computedFile }) => {
   const idText = project
     ? `Project ID: ${project.scpca_id}`
     : `Sample ID: ${sample.scpca_id}`
+
+  const { size: responsiveSize } = useResponsive()
+
   return (
-    <Box>
-      <Box
-        direction="row"
+    <>
+      <Grid
+        columns={['2/3', '1/3']}
         align="center"
         gap="large"
         pad={{ bottom: 'medium' }}
@@ -143,36 +155,38 @@ export const DownloadView = ({ computedFile }) => {
             <Text weight="bold">{idText}</Text>
             <Text weight="bold">Size: {formatBytes(size)}</Text>
           </Box>
-          <Text>
-            <Link label="Click here" href={href} /> if your download has not
-            started yet.
-          </Text>
-          <Text italic color="black-tint-40" margin={{ top: 'medium' }}>
-            Please ensure that pop-ups are not blocked to enable automatic
-            downloads.
-          </Text>
+          <Box gap="medium">
+            {responsiveSize !== 'small' && (
+              <Text italic color="black-tint-40">
+                If your download has not started, please ensure that pop-ups are
+                not blocked to enable automatic downloads. You can download now
+                by using the button below:
+              </Text>
+            )}
+            <Button
+              alignSelf="start"
+              label="Download Now"
+              href={href}
+              target="_blank"
+            />
+          </Box>
         </Box>
         <Box pad={{ bottom: 'medium', horizontal: 'medium' }}>
-          <DownloadSVG />
+          <DownloadSVG width="100%" height="auto" />
         </Box>
-      </Box>
+      </Grid>
       <Box
         direction="row"
         align="center"
         justify="between"
         pad={{ top: 'large' }}
       >
-        <Text>Learn about what you can expect in your download file.</Text>
-        <Link href={config.links.help}>
-          <Button
-            primary
-            label="Read Docs"
-            href={config.links.what_downloading}
-            target="_blank"
-          />
-        </Link>
+        <Text>
+          <Link href={config.links.help} label="Read the docs here" /> to learn
+          about what you can expect in your download file.
+        </Text>
       </Box>
-    </Box>
+    </>
   )
 }
 
@@ -223,7 +237,7 @@ export const Download = ({ Icon, computedFile: publicComputedFile }) => {
         />
       )}
       <Modal showing={showing} setShowing={setShowing} title={label}>
-        <Box width="medium">
+        <Box width={{ width: 'full' }}>
           {download ? <DownloadView computedFile={download} /> : <TokenView />}
         </Box>
       </Modal>
