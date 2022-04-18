15573f08ad3d80bd4cdc6b76b931455cf9b78750
David Mejia <davidsmejia@gmail.com>
2022-04-18 10:54:51 -0400

    run pre-commit on all files

--- api/scpca_portal/migrations/0008_sample_treatment.py
+++ api/scpca_portal/migrations/0008_sample_treatment.py
@@ -11,6 +11,8 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
-            model_name="sample", name="treatment", field=models.TextField(blank=True, null=True),
+            model_name="sample",
+            name="treatment",
+            field=models.TextField(blank=True, null=True),
         ),
     ]
