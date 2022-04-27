28be0c20adb0ea96d4db7ce3c32ca08c90f6a017
ark <ark@cho.red>
2022-04-26 20:55:00 -0700

    Fix typo.

--- api/scpca_portal/common.py
+++ api/scpca_portal/common.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # This allows us to run the same command on production or locally.
 CODE_DIR = "/home/user/code/" if os.path.exists("/home/user/code") else "/home/user/"
 
-DATA_DIR = os.path.join(CODE_DIR, "test_data" if settings.TEST else "date")
+DATA_DIR = os.path.join(CODE_DIR, "test_data" if settings.TEST else "data")
 INPUT_DATA_DIR = os.path.join(DATA_DIR, "input")
 OUTPUT_DATA_DIR = os.path.join(DATA_DIR, "output")
 
