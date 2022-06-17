79c83ef2255003c2603ce3cea6e1374dca188ef4
nozomione <nozomione.online@gmail.com>
2022-06-17 17:17:45 -0400

    destructure the config object for contact and links

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -14,8 +14,8 @@ const FooterImage = styled(Image)`
 
 export const Footer = () => {
   const { responsive } = useResponsive()
-
   const direction = responsive('column', 'row')
+  const { contact, links } = config
 
   return (
     <FooterBand
@@ -35,7 +35,7 @@ export const Footer = () => {
             margin={{ vertical: 'large' }}
             align={responsive('center', 'start')}
           >
-            {Object.keys(config.contact).map((k) => (
+            {Object.keys(contact).map((k) => (
               <Text
                 key={k}
                 size={responsive('small', 'medium')}
@@ -44,12 +44,12 @@ export const Footer = () => {
                 {k === 'email' ? (
                   <>
                     {k[0].toUpperCase() + k.substring(1)}:{' '}
-                    <Link href={`mailto:mailto:email@${config.contact[k]}`}>
-                      {config.contact[k]}
+                    <Link href={`mailto:mailto:email@${contact[k]}`}>
+                      {contact[k]}
                     </Link>
                   </>
                 ) : (
-                  config.contact[k]
+                  contact[k]
                 )}
               </Text>
             ))}
@@ -57,7 +57,7 @@ export const Footer = () => {
               <Button
                 primary
                 label="Donate"
-                href={config.links.donate}
+                href={links.donate}
                 target="_blank"
               />
             </Box>
@@ -96,13 +96,13 @@ export const Footer = () => {
                   justify="center"
                   align="center"
                 >
-                  <Link href={config.links.alsfTwitter}>
+                  <Link href={links.alsfTwitter}>
                     <Icon name="Twitter" color="black-tint-40" />
                   </Link>
-                  <Link href={config.links.alsfInstagram}>
+                  <Link href={links.alsfInstagram}>
                     <Icon name="Instagram" color="black-tint-40" />
                   </Link>
-                  <Link href={config.links.alsfFacebook}>
+                  <Link href={links.alsfFacebook}>
                     <Icon name="Facebook" color="black-tint-40" />
                   </Link>
                 </Box>
@@ -128,10 +128,10 @@ export const Footer = () => {
                   justify="center"
                   align="center"
                 >
-                  <Link href={config.links.ccdlGithub}>
+                  <Link href={links.ccdlGithub}>
                     <Icon name="Github" color="black-tint-40" />
                   </Link>
-                  <Link href={config.links.ccdlTwitter}>
+                  <Link href={links.ccdlTwitter}>
                     <Icon name="Twitter" color="black-tint-40" />
                   </Link>
                 </Box>
@@ -149,7 +149,7 @@ export const Footer = () => {
           <Nav direction="row" align="center" gap="large">
             <Link href="/terms-of-use" label="Terms of Use" />
             <Link href="/privacy-policy" label="Privacy Policy" />
-            <Link href={config.links.help} label="Docs" />
+            <Link href={links.help} label="Docs" />
           </Nav>
         </Box>
       </Box>
