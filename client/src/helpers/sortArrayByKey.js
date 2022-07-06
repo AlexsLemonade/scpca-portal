635b2205c8f6e8863d1f878ecdd6e01cb10e6384
nozomione <nozomione.online@gmail.com>
2022-07-06 09:52:54 -0400

    revise documentation

--- client/src/helpers/sortArrayByKey.js
+++ client/src/helpers/sortArrayByKey.js
@@ -1,18 +1,13 @@
 /*
 @name sortArrayByKey
-@description sort an array or an array of object
+@description sort an array of objects by a key
 @param {string} key - an object key that is used to sort
-@param {array} target - an arry to sort
-@param {array} sortOrder - a preferred sorting order
+@param {Object[]} target - an array of Objects to sort
+@param {string[]} sortOrder - a preferred sorting order
 @param {boolean} asc - a boolean indicates whether it's ascending order or not
 */
 
-export const sortArrayByKey = (
-  key = '',
-  target,
-  sortOrder = [],
-  asc = true
-) => {
+export const sortArrayByKey = (key, target, sortOrder = [], asc = true) => {
   const sortedArray = target.sort((a, b) => {
     if (sortOrder.length > 0) {
       return sortOrder.indexOf(a[key]) - sortOrder.indexOf(b[key])
