002e47dff81dc6d14c78be49c0f2339cba63d66f
Arkadii Yakovets <ark@ccdatalab.org>
2022-11-29 09:04:33 -0800

    Re-configure pre-commit. Ignore less, use per file ignores. Add forgotten `print` instructions check. Fix line length issues. Re-order setup.cfg options.

--- api/scpca_portal/config/production.py
+++ api/scpca_portal/config/production.py
@@ -21,7 +21,8 @@ class Production(Common):
     AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
 
     # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
-    # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
+    # Response can be cached by browser and any intermediary caches
+    # (i.e. it is "public") for up to 1 day
     # 86400 = (60 seconds x 60 minutes x 24 hours)
     AWS_HEADERS = {
         "Cache-Control": "max-age=86400, s-maxage=86400, must-revalidate",
