c9ebd5f26614408f0967341787b95db6d8fa8ae7
Arkadii Yakovets <ark@ccdatalab.org>
2023-04-25 08:20:50 -0700

    Scale down the volume size

--- infrastructure/api.tf
+++ infrastructure/api.tf
@@ -86,7 +86,7 @@ resource "aws_instance" "api_server_1" {
   # This should be approx x2 the size of the s3 data
   root_block_device {
     volume_type = "gp2"
-    volume_size = 350
+    volume_size = 50
     tags =  merge(
       var.default_tags,
       {
