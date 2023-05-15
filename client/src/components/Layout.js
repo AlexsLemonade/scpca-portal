05427aadd2bc88872c03ec0b1f643217668a7203
nozomione <nozomione.online@gmail.com>
2023-05-15 18:05:59 -0400

    optimize the Layout component: - delete FixedBox styled-component, its ref/state, its wrapper div (for preserving fixedBoxHeight) - add StickyBox styled-component with the ‘boxShadow’ prop (for box shadow visibility) - delete the duplicated page checks for home/about (widePaths/excludPadPaths) and combine them into one as ‘excludedPages’ - add the wrapper div that will hold a hero banner (e.g. contribute banner) and based on its height, toggle the top margin on project pages and delete the specific banner check with useBanner (banner['contribute-banner’] - to prevent typo etc)

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,21 +1,19 @@
-import React, { useRef, useState, useCallback } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
-import { useBanner } from 'hooks/useBanner'
 import { useResizeObserver } from 'hooks/useResizeObserver'
 import { Box, Main } from 'grommet'
 import { ContributeBanner } from 'components/ContributeBanner'
 import { Footer } from 'components/Footer'
 import { Header } from 'components/Header'
-import { RecruitNFBanner } from 'components/RecruitNFBanner'
 import styled, { css } from 'styled-components'
 
-const FixedBox = styled(Box)`
-  position: fixed;
-  width: 100%;
-  z-index: 3;
-  // padding-bottom: 8px;
-  ${({ showMargin }) =>
-    showMargin &&
+const StickyBox = styled(Box)`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  ${({ boxShadow }) =>
+    boxShadow &&
     css`
       box-shadow: 0px 2px 5px 5px #fdfdfd;
     `}
@@ -23,50 +21,43 @@ const FixedBox = styled(Box)`
 
 export const Layout = ({ children }) => {
   const router = useRouter()
-  const { banner } = useBanner()
 
-  // get the height of FixedBox for to preserve the margin
-  const [fixedBoxHeight, setFixedBoxHeight] = useState(0)
-  const fixedBoxRef = useRef(null)
+  // get the height of the wrapper for the hero banner (e.g. ContributeBanner)
+  const wrapperRef = useRef(null)
+  const [wrapperHeight, setWrapperHeights] = useState(0)
   useResizeObserver(
-    fixedBoxRef,
+    wrapperRef,
     useCallback((ref) => {
-      setFixedBoxHeight(`${ref.clientHeight}px`)
+      setWrapperHeights(ref.clientHeight)
     }, [])
   )
 
-  // homepage is full width
-  const widePaths = ['/', '/about']
-  const showWide = widePaths.includes(router.pathname)
+  // exclude css rules (padding /box shadow) on the following pages
+  const excludedPages = ['/', '/about']
+  const isExcludedPages = excludedPages.includes(router.pathname)
+  const isBoxShadow = !isExcludedPages && wrapperHeight === 0
 
-  // exclude the padding / and box shadow on the following pages
-  const excludPadPaths = ['/', '/about']
-  const showMargin = !excludPadPaths.includes(router.pathname)
-
-  // add the top margin to the following pages when the contribution banner is hidden
-  const addTopMarginPaths = ['/projects', '/projects/[scpca_id]']
-  const addTopMargin =
-    addTopMarginPaths.includes(router.pathname) &&
-    showMargin &&
-    !banner['contribute-banner']
+  // add the top margin & box shadow to the following pages when the contribution banner is hidden
+  const projectPages = ['/projects', '/projects/[scpca_id]']
+  const isProjectPages =
+    projectPages.includes(router.pathname) && wrapperHeight === 0
 
   return (
     <Box height={{ min: '100vh' }}>
-      <Box height={fixedBoxHeight}>
-        <FixedBox background="white" ref={fixedBoxRef} showMargin={showMargin}>
-          <RecruitNFBanner hidden />
-          <Header />
-        </FixedBox>
+      <StickyBox boxShadow={isBoxShadow}>
+        <Header />
+      </StickyBox>
+      <Box ref={wrapperRef}>
+        <ContributeBanner />
       </Box>
-      <ContributeBanner />
       <Main
-        width={showWide ? 'full' : 'xlarge'}
-        alignSelf="center"
-        overflow="visible"
         align="center"
+        alignSelf="center"
         justify="center"
-        margin={{ top: addTopMargin ? 'xlarge' : '' }}
-        pad={{ top: showMargin ? 'xlarge' : '' }}
+        overflow="visible"
+        margin={{ top: isProjectPages ? 'xlarge' : '' }}
+        pad={{ top: !isExcludedPages ? 'xlarge' : '' }}
+        width={isExcludedPages ? 'full' : 'xlarge'}
       >
         {children}
       </Main>
