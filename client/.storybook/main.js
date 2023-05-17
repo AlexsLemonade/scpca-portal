d48a1a497fd4c5f3dfbf879e52f686c8760c534f
nozomione <nozomione.online@gmail.com>
2023-05-17 11:03:54 -0400

    remove ‘DeprecationWarning: --static-dir CLI flag is deprecated’ at compile time

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -7,8 +7,9 @@ const envVars = {
 }
 
 module.exports = {
-  stories: ['./stories/**/*.stories.@(js|mdx)'],
   addons: ['@storybook/addon-storysource'],
+  staticDirs: ['./../public'],
+  stories: ['./stories/**/*.stories.@(js|mdx)'],
   webpackFinal: async (config) => {
     // Add src to imports (so this works with app webpack config)
     config.resolve.modules.push(path.resolve(__dirname, './../src'))
