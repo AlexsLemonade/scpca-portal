cec2e26a4d064adb75af7bc49dfd75bd0ea1fb94
nozomione <nozomione.online@gmail.com>
2023-05-17 10:58:28 -0400

    remove 'Received 'true' for a non-boolean attribute crossOrigin' warning at compile time

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -22,7 +22,11 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.svg" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&family=Zilla+Slab:wght@500&display=swap"
             rel="stylesheet"
