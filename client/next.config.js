328e0046ac18cbccaccca00d8e34230ba804b5c5
nozomione <nozomione.online@gmail.com>
2023-05-17 15:11:46 -0400

    remove ‘Warning: Reverting webpack devtool to 'eval-source-map'. Changing the webpack devtool in development mode will cause severe performance regressions’ at compile: - remove the custom devtool override in next.config since it reverts back to eval-source-map during the compilation (resource: https://nextjs.org/docs/messages/improper-devtool)

--- client/next.config.js
+++ client/next.config.js
@@ -39,12 +39,12 @@ module.exports = () => {
     },
     webpack: (baseConfig) => {
       const config = { ...baseConfig }
-      config.devtool = 'source-map'
       config.resolveLoader.modules.push(path.resolve(__dirname, 'loaders'))
       config.module.rules.push({
         test: /\.md$/,
         use: ['raw-loader', 'template-literal-loader']
       })
+
       return config
     }
   }
