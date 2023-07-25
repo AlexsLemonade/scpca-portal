08e2f4567ef3fe7f003249c95e27ad48ea76d2db
Arkadii Yakovets <ark@ccdatalab.org>
2023-07-25 10:29:23 -0700

    Attach a persistent data volume to the data loading container

--- infrastructure/api-configuration/run_command.tpl.sh
+++ infrastructure/api-configuration/run_command.tpl.sh
@@ -2,6 +2,13 @@
 
 API_DOCKER_IMAGE="${dockerhub_repo}/scpca_portal_api:latest"
 
+# Prepare the data volume.
+DATA_VOLUME_PATH="/home/ubuntu/data"
+if [ ! -d "$DATA_VOLUME_PATH" ]; then
+    mkdir "$DATA_VOLUME_PATH"
+fi
+chmod -R a+rwX "$DATA_VOLUME_PATH"
+
 # Pull the API image.
 docker pull "$API_DOCKER_IMAGE"
 
@@ -9,5 +16,7 @@ docker pull "$API_DOCKER_IMAGE"
 docker run \
     --env-file environment \
     --name scpca_portal_loader \
-    --rm "$API_DOCKER_IMAGE" \
+    --rm \
+    --volume "$DATA_VOLUME_PATH:/home/user/data" \
+    "$API_DOCKER_IMAGE" \
     python3 manage.py "$@"
