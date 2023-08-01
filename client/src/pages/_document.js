5cef733ab8d6c1bd72f027771d8e3d8998a014ae
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-08-01 12:43:43 -0400

    (fix) use DOM script tag instead of Netx.js Script tag (hotar doesn't load when using it)

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -33,14 +33,13 @@ export default class MyDocument extends Document {
             strategy="afterInteractive"
           />
           {/* Hotjar */}
-          <Script
+          <script
             async
             //  eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
               __html:
                 "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2569460,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
             }}
-            strategy="afterInteractive"
           />
           {this.props.styleTags}
         </Head>
