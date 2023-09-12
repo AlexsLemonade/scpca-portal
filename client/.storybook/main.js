5f82a56dcfa69657b17a16e43a61bfb955005472
Nozomi Ichihara <31800566+nozomione@users.noreply.github.com>
2023-09-12 16:22:11 -0400

    remove the error 'TypeError: Cannot read property 'definitions' of undefined’ at compile

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const Dotenv = require('dotenv-webpack')
 
 const envVars = {
   API_HOST: 'http://localhost:8000',
@@ -7,17 +6,21 @@ const envVars = {
 }
 
 module.exports = {
+  addons: ['@storybook/addon-storysource'],
   staticDirs: ['./../public'],
   stories: ['./stories/**/*.stories.@(js|mdx)'],
-  addons: ['@storybook/addon-storysource'],
   webpackFinal: async (config) => {
     // Add src to imports (so this works with app webpack config)
     config.resolve.modules.push(path.resolve(__dirname, './../src'))
-
     // Add env vars for helpers
-    Object.keys(envVars).forEach((key) => {
-      config.plugins.DefinePlugin.definitions[`process.env.${key}`] =
-        JSON.stringify(envVars[key])
+    config.plugins.forEach((plugin) => {
+      if (Object.keys(plugin)[0] === 'definitions') {
+        Object.keys(envVars).forEach((key) => {
+          plugin['definitions'][`process.env.${key}`] = JSON.stringify(
+            envVars[key]
+          )
+        })
+      }
     })
 
     return config
