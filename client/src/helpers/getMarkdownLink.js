7b3bfa8f85e73b6ca1aada617cade01265b98008
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-02 08:43:24 -0500

    (mionr) edit a comment of helper

--- client/src/helpers/getMarkdownLink.js
+++ client/src/helpers/getMarkdownLink.js
@@ -1,6 +1,7 @@
 import { config } from 'config'
 import formatStringToIdName from './formatStringToIdName'
-// Returns a link of the gien page route and section name
+// Returns a link of the given route and section name
+// a route can be grabbed by visiting the page (e.g., 'terms-of-use', refine.bio/terms-of-use)
 // all the linkable sections are listed in config.markdownLinkable[route]
 export default (route, section) =>
   config.markdownLinkable[route].includes(section)
