404e88d5066610db6fbd5f930a34c53e7b590b84
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-01 21:00:29 -0500

    (fix) use the formatStringToIdName helper in getMarkdownLink helper

--- client/src/helpers/getMarkdownLink.js
+++ client/src/helpers/getMarkdownLink.js
@@ -1,11 +1,8 @@
 import { config } from 'config'
+import formatStringToIdName from './formatStringToIdName'
 // Returns the like of the sections of the given markdown page route
 // all the linkable sections are listed in config.markdownLinkable[route]
 export default (route, section) =>
   config.markdownLinkable[route].includes(section)
-    ? `/${route}#${section
-        .toLowerCase()
-        .replace(/[^a-zA-Z ]/g, '')
-        .replace(/\s+/g, ' ')
-        .replace(/\s+/g, '-')}`
+    ? `/${route}#${formatStringToIdName(section)}`
     : ''
