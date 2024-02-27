c2512fdc15daa3624c4ade0cbbb710fc125dbbfb
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-27 16:45:14 -0500

    (edit) use the getLastUpdatedDate helper in pages/terms-of-use

--- client/src/pages/terms-of-use.js
+++ client/src/pages/terms-of-use.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { MarkdownPage } from 'components/MarkdownPage'
+import { getLastUpdatedDate } from 'helpers/getLastUpdatedDate'
 import termsOfUse from '../config/terms-of-use.md'
 
-export const TermsOfUse = () => <MarkdownPage markdown={termsOfUse} />
+export const TermsOfUse = () => {
+  const markdownContent = getLastUpdatedDate(
+    termsOfUse,
+    process.env.TOS_RELEASE
+  )
+
+  return <MarkdownPage markdown={markdownContent} />
+}
 
 export default TermsOfUse
