d95145468a4fc9b4f10ec8a3cf1d5d4170d26f7c
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-28 09:10:38 -0500

    (edit) use the getLastUpdatedDate helper in pages/privacy-policy

--- client/src/pages/privacy-policy.js
+++ client/src/pages/privacy-policy.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { MarkdownPage } from 'components/MarkdownPage'
+import { getLastUpdatedDate } from 'helpers/getLastUpdatedDate'
 import privacyPolicy from '../config/privacy-policy.md'
 
-export const PrivacyPolicy = () => <MarkdownPage markdown={privacyPolicy} />
+export const PrivacyPolicy = () => {
+  const markdownContent = getLastUpdatedDate(
+    privacyPolicy,
+    process.env.PRIVACY_POLICY_RELEASE
+  )
+
+  return <MarkdownPage markdown={markdownContent} />
+}
 
 export default PrivacyPolicy
