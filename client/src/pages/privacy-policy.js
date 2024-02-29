27eaca054c0c6ae5844e21763a1c846f2aacee62
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-29 13:24:59 -0500

    (edit) use the MarkdownLastUpdatedDate component in PrivacyPolicy  page component and clean up the unused code blocks

--- client/src/pages/privacy-policy.js
+++ client/src/pages/privacy-policy.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import { MarkdownPage } from 'components/MarkdownPage'
-import { getLastUpdatedDate } from 'helpers/getLastUpdatedDate'
+import { MarkdownLastUpdatedDate } from 'components/MarkdownLastUpdated'
 import privacyPolicy from '../config/privacy-policy.md'
 
-export const PrivacyPolicy = () => {
-  const markdownContent = getLastUpdatedDate(
-    privacyPolicy,
-    process.env.PRIVACY_POLICY_RELEASE
-  )
-
-  return <MarkdownPage markdown={markdownContent} />
-}
+export const PrivacyPolicy = () => (
+  <>
+    <MarkdownPage markdown={privacyPolicy} />
+    <MarkdownLastUpdatedDate env={process.env.PRIVACY_POLICY_RELEASE} />
+  </>
+)
 
 export default PrivacyPolicy
