fd4ad7dc3914ff21f977dd2041726722b37536a9
Nozomi Ichihara <nozomione.online@gmail.com>
2024-02-29 17:28:18 -0500

    (delete) remove the getMarkdownLink helper (no longer used) due to the markdown formatting update - now we use object instead of array of text nodes

--- client/src/helpers/getMarkdownLink.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'config'
-import slugify from './slugify'
-// Returns a link of the given route and section name
-// a route can be grabbed by visiting the page (e.g., 'terms-of-use', refine.bio/terms-of-use)
-// all the linkable sections are listed in config.markdownLinkable[route]
-export default (route, section) =>
-  config.markdownLinkable[route].includes(section)
-    ? `/${route}#${slugify(section)}`
-    : ''
