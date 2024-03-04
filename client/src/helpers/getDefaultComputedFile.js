2eb1f591c73fb44ed1d07e0e3357f828a7f5e4bb
David Mejia <davidsmejia@gmail.com>
2024-03-04 14:28:51 -0500

    add support for modality

--- client/src/helpers/getDefaultComputedFile.js
+++ client/src/helpers/getDefaultComputedFile.js
@@ -2,9 +2,17 @@
 @name getDefaultComputedFile
 @description returns the first element of the computed_files array fetched from the API
 @param {Object} resource - a resource object that contains a computed_files array
+@param {String} modality - a resource object that contains a computed_files array
 */
-export const getDefaultComputedFile = (resource) => {
-  return resource.computed_files[0]
+export const getDefaultComputedFile = (resource, computedFile) => {
+  const { computed_files: computedFiles } = resource
+  if (!computedFile) return computedFiles[0]
+
+  const { modality: fileModality, format: fileFormat} = computedFile
+  const defaultFile = computedFiles.find(
+    ({ modality, format }) => modality === fileModality && format === fileFormat
+  )
+  return defaultFile || computedFiles[0]
 }
 
 export default getDefaultComputedFile
