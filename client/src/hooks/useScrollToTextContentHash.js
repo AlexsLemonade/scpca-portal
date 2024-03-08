6a0987e44ca6fca2b303cc39750f477e315e4182
David Mejia <davidsmejia@gmail.com>
2024-03-08 15:46:31 -0500

    update comment

--- client/src/hooks/useScrollToTextContentHash.js
+++ client/src/hooks/useScrollToTextContentHash.js
@@ -2,9 +2,10 @@ import { useEffect } from 'react'
 import { slugify } from 'helpers/slugify'
 import { getHash } from 'helpers/getHash'
 
-// Given the hook is loaded with a hash ie. #some-slug
-// This hook will take a DOM ref and css selector
-// Will iterator over matches and check textContent
+// Given the hook is loaded at a location with a hash ie. #some-slug
+// This hook takes a DOM ref and css selector
+// Will iterate over matches on the css selector in the ref node
+// a match is a slug version of the textContent matching the slug from hash
 // If there is a match it will scroll to that element
 
 export const useScrollToTextContentHash = (ref, selector) => {
