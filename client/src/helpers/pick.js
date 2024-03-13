918bb7443e0fdbcbf3317eca59a2f50c9c8c1a4d
Nozomi Ichihara <nozomione.online@gmail.com>
2024-03-13 14:36:45 -0400

    (edit) revise the pick helper to prevent it from returning an array of 'undefined' values

--- client/src/helpers/pick.js
+++ client/src/helpers/pick.js
@@ -1,1 +1,23 @@
-export default (arr = [], key) => arr.map((i) => i[key])
+/**
+ *  @name pick
+ *  @param {any[]} arr - an array of objects
+ *  @param {string} key - a key to find
+ *  @return {any[]}
+ *  @description returns a new array containing the value(s) of the given 'key' found in 'arr'
+ *  @usage
+ *  pick([{ a: 'a', b: 'b'}, {b: 'b', c: 'c'},{c: 'c', d: 'd'}], 'c' )) // ['c', 'c']
+ *  pick(
+ *    [
+ *     { format: "SINGLE_CELL_EXPERIMENT" },
+ *     { format: "ANN_DATA" }
+ *    ], 'format'
+ *   ) // [ 'SINGLE_CELL_EXPERIMENT', 'ANN_DATA' ]
+ * */
+
+export const pick = (arr = [], key) =>
+  arr.reduce((acc, cur) => {
+    if (cur[key]) acc.push(cur[key])
+    return acc
+  }, [])
+
+export default pick
