53fe84cdaf72c919b2eb9aa45abea3b99226d233
Nozomi Ichihara <nozomione.online@gmail.com>
2024-03-12 21:19:22 -0400

    (edit) add a new boolean paramter to the captilize helper, and remove the capitalizeFirst helper to support a string with space or special characters specified in the regex pattern

--- client/src/helpers/capitalize.js
+++ client/src/helpers/capitalize.js
@@ -1,23 +1,25 @@
 /**
  * Capitalizes first letters of words in string.
- * @param {string} str String to be modified
- * @param {boolean=false} lower Whether all other letters should be lowercased
+ * @param {string} str - string to be modified
+ * @param {boolean} firstOnly - (optional) Whether to capitalize only the first letter of the first word
+ * @param {boolean} lower - (optional) Whether all other letters should be lowercased
  * @return {string}
  * @usage
+ *   By default:
  *   capitalize('fix this string');     // -> 'Fix This String'
- *   capitalize('javaSCrIPT');          // -> 'JavaSCrIPT'
- *   capitalize('javaSCrIPT', true);    // -> 'Javascript'
+ *   capitalize(' with space and "quotation" mark');     // -> 'With Space And "Quotation" Mark'
+ *   With 'firstOnly':
+ *   capitalize('fix this string', true);     // -> 'Fix this string'
+ *   capitalize(' "quotation" mark with space', true);     // -> '"Quotation" mark with space'
+ *   With 'lower':
+ *   capitalize('javaSCrIPT', false, true);    // -> 'Javascript'
  */
-export const capitalize = (string, lower = false) => {
-  const str = string || ''
-  return (lower ? str.toLowerCase() : str).replace(
-    /(?:^|\s|["'([{])+\S/g,
-    (match) => match.toUpperCase()
-  )
-}
+export const capitalize = (str = '', firstOnly = false, lower = false) => {
+  const re = new RegExp(/(?:^|\s|["'([{])+\S/, !firstOnly ? 'g' : '')
 
-export const capitalizeFirst = (string) => {
-  return `${string.charAt(0).toUpperCase()}${string.slice(1)}`
+  return (lower ? str.toLowerCase() : str).replace(re, (match) =>
+    match.toUpperCase()
+  )
 }
 
 export default capitalize
