3d13b49bed9c46a7996001f2d70899032e1ad1ed
Nozomi Ichihara <nozomione.online@gmail.com>
2024-03-13 18:00:33 -0400

    (minor) fix typo

--- client/src/helpers/uniqueArray.js
+++ client/src/helpers/uniqueArray.js
@@ -1,10 +1,10 @@
 /**
  *  @name uniqueArray
- *  @param {string|number[]} arr - an array to be filtered
+ *  @param {(string|number)[]} arr - an array to be filtered
  *  @return {(string | number)[]}
  *  @description returns a new array containing only the unique values of 'arr'
  *  @usage
- * uniqueArray([1,2,2,3,4,5,1,3,6]) // [1, 2, 3, 4, 5, 6]
+ *  uniqueArray([1,2,2,3,4,5,1,3,6]) // [1, 2, 3, 4, 5, 6]
  *
  */
 export const uniqueArray = (arr) => [...new Set(arr)]
