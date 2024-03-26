4fcb4673af2443ade1bd737a8b101b9418959e24
Nozomi Ichihara <nozomione.online@gmail.com>
2024-03-26 17:48:39 -0400

    (edit) use the selectedMerged state in CheckBoxMergedObjects for checkbox update

--- client/src/components/CheckBoxMergedObjects.js
+++ client/src/components/CheckBoxMergedObjects.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { CheckBox } from 'grommet'
-import { HelpLink } from 'components/HelpLink'
 import { config } from 'config'
+import { useDownloadOptionsContext } from 'hooks/useDownloadOptionsContext'
+import { HelpLink } from 'components/HelpLink'
 
 export const CheckBoxMergedObjects = ({ downloadable = false }) => {
+  const { selectedMerged, setSelectedMerged } = useDownloadOptionsContext()
   const link = downloadable
     ? config.links.when_downloading_merged_objects
     : config.links.which_projects_are_merged_objects
+  const handleChange = () => setSelectedMerged(!selectedMerged)
 
   return (
     <CheckBox
+      checked={selectedMerged && downloadable}
       disabled={!downloadable}
       label={<HelpLink label="Merge samples into 1 object" link={link} />}
+      onChange={handleChange}
     />
   )
 }
