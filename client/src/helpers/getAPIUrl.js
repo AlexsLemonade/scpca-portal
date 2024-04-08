294cadbbd2c8530f859ee043830dc7ffebb7b0bb
Nozomi Ichihara <nozomione.online@gmail.com>
2024-04-08 19:32:22 -0400

    (temp) use prod API for demo-purpose only

--- client/src/helpers/getAPIUrl.js
+++ client/src/helpers/getAPIUrl.js
@@ -2,7 +2,10 @@ import { urlSearchParamsFromKeys } from 'helpers/urlSearchParamsFromKeys'
 
 export const host = process.env.API_HOST
 export const version = process.env.API_VERSION
-export const path = `${host}/${version}/`
+export const path =
+  // TEMP: use prod API for demo-purpose only
+  // (currently staging s3 has no "SCPCP000009_multiplexed.zip - in the middle of updating etc?)
+  'https://api.scpca.alexslemonade.org/v1/' || `${host}/${version}/`
 
 export const getAPIUrl = (endpoint = '', query = {}) => {
   const url = new URL(endpoint, path)
