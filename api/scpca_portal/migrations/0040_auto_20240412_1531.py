6e09756ce0a0b55854b646035706e45152adb182
Nozomi Ichihara <nozomione.online@gmail.com>
2024-04-12 11:45:32 -0400

    (fix) run pre-commit

--- api/scpca_portal/migrations/0040_auto_20240412_1531.py
+++ api/scpca_portal/migrations/0040_auto_20240412_1531.py
@@ -6,23 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('scpca_portal', '0039_computedfile_includes_celltype_report'),
+        ("scpca_portal", "0039_computedfile_includes_celltype_report"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='computedfile',
-            name='includes_merged',
+            model_name="computedfile",
+            name="includes_merged",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='project',
-            name='includes_merged_anndata',
+            model_name="project",
+            name="includes_merged_anndata",
             field=models.BooleanField(default=False),
         ),
         migrations.AddField(
-            model_name='project',
-            name='includes_merged_sce',
+            model_name="project",
+            name="includes_merged_sce",
             field=models.BooleanField(default=False),
         ),
     ]
