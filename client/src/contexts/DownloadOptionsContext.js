a34cfe38cb6b70be9506858a3905b6ca696aa8a4
Nozomi Ichihara <nozomione.online@gmail.com>
2024-04-18 09:09:44 -0400

    550 - Add Merged Object Download Options (#602)
    * (minor) reduce the horizontal margin of the Grommet built-in CheckBock component in /theme
    
    * (edit) add ScPCA docs URLs for the merged objects to the config file
    
    * (edit) update the HelpLink component to preserve the space for the help icon rendered at top-right corner
    
    * create the CheckBoxMergedObjects component
    
    * (edit) add the mergedObjectsKeys (a list of modalites that support the merged objects download) to config/downloadOptions, and use it in useDownloadOptionsContext to create isMergedObjectsAvailable check"
    
    * (edit) add the checkbox and warning text for the merged objects to the DownloadOption/DownloadOptions components
    
    * (fix) run pre-commit
    
    * (edit) add the includes_merged flag check to isMergedObjectsAvailable in useDownloadOptionsContext
    
    * (edit) add the slectedMerge state to DownloadOptionsContextProvider for the merged object download
    
    * (edit) add the logic for the merged objects download to useDownloadOptionsContext
    
    * (edit) use the selectedMerged state in CheckBoxMergedObjects for checkbox update
    
    * create the WarningMergedObjects component (the warning text for the merged objects - samples are not integrated)
    
    * (edit) use WarningMergedObjects in DownloadOption and DownloadStarted, and minor edit in useDownloadOptionsContext
    
    * (edit) check the merged objects using 'includes_merged'
    
    * (rename) rename the 'selectedMerged/setSelectedMerged' state to 'includesMerged/setIncludesMerged'
    
    * (minor) fix a typo 'setComputeFiles' to 'setComputedFiles'
    
    * (edit) uncheck the checkbox (CheckBoxMergedObjects) when no merged objects available
    
    * (apply PR feedback) We'll no longer check modalily for the merged objects. The following changes were made:
    In config/downloadOptions:
    - remove the 'mergedObjectsKeys' and clean up its import statement
    In useDowbloadOptionsContext:
    - remove the 'isMergedObjectKey' check
    - add method 'filterComputedFilesByModality' which filters available computed files for the selected modality
    - add new state 'commputedFilesByModality' (available computedFiles per modality)
    - update the 'isMergedObjectsAvailable' check to use commputedFilesByModalityfor enablinh/disablinh the merged object checkbox/warning text
    
    * (fix) show 'WarningMergedObjects' only when the checkbox is selected
    
    * (fix) render the help link icon next to the checkbox and not as a part of the label prop
    
    * (minor) revise comments
    
    * (edit) remove the state 'commputedFilesByModality' and the logic for filter computed files by selected modality and instead check the computed files based on selected  modality and format, and the includes_merged flag
    
    * (rename) rename 'filterComputedFiles' to getFilteredComputedFiles
    
    * (edit) check length and improve comments, rename method name to getMergedObjectsComputedFiles

--- client/src/contexts/DownloadOptionsContext.js
+++ client/src/contexts/DownloadOptionsContext.js
@@ -16,13 +16,14 @@ export const DownloadOptionsContextProvider = ({
   // Download Options
   const [modality, setModality] = useState(null)
   const [format, setFormat] = useState(null)
+  const [includesMerged, setIncludesMerged] = useState(false)
 
   // Potential Values for Download Options
   const [modalityOptions, setModalityOptions] = useState([])
   const [formatOptions, setFormatOptions] = useState([])
 
   // Computed Files used to derive available options.
-  const [computedFiles, setComputeFiles] = useState([])
+  const [computedFiles, setComputedFiles] = useState([])
 
   // Automatically updated computed file for resource
   // This does *not* work when attribute specified.
@@ -31,7 +32,7 @@ export const DownloadOptionsContextProvider = ({
   // Only on mount or when resource / collection name change
   useEffect(() => {
     if (resource) {
-      setComputeFiles(() => {
+      setComputedFiles(() => {
         if (!resourceAttribute) return resource.computed_files
         return pick(resource[resourceAttribute], 'computed_files').flat()
       })
@@ -41,23 +42,26 @@ export const DownloadOptionsContextProvider = ({
   return (
     <DownloadOptionsContext.Provider
       value={{
+        computedFile,
+        setComputedFile,
+        computedFiles,
+        setComputedFiles,
+        format,
+        setFormat,
+        formatOptions,
+        setFormatOptions,
+        modality,
+        setModality,
+        modalityOptions,
+        setModalityOptions,
+        resource,
+        resourceAttribute,
+        includesMerged,
+        setIncludesMerged,
         userModality,
         setUserModality,
         userFormat,
-        setUserFormat,
-        modality,
-        setModality,
-        format,
-        setFormat,
-        modalityOptions,
-        formatOptions,
-        computedFile,
-        resource,
-        resourceAttribute,
-        setModalityOptions,
-        setFormatOptions,
-        computedFiles,
-        setComputedFile
+        setUserFormat
       }}
     >
       {children}
