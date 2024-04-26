98fae473098188367b1fdc20097c124c3e337402
David Mejia <davidsmejia@gmail.com>
2024-04-26 12:34:50 -0400

    fair, eval is evil

--- client/src/helpers/getTemplateLines.js
+++ client/src/helpers/getTemplateLines.js
@@ -1,7 +1,9 @@
 import escapeBackticks from 'helpers/escapeBackticks'
 
 // Returns an array of template strings ready for Markdown component
+// NOTE: This function calls `eval` and therefore *must* never be called on user entered input.
 export default (contentString, newLineChar = '\\n') =>
   contentString
     .split(newLineChar)
+    // eslint-disable-next-line no-eval
     .map((line) => eval(`\`${escapeBackticks(line.trim())}\``))
