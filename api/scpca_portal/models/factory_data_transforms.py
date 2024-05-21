a3eaa63f492f29376c0398d0add2d1579a3ff562
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-05-21 14:46:23 -0400

    extend project_data_transform to update project_data keys for Conact, ExternalAccession, and Publication models

--- api/scpca_portal/models/factory_data_transforms.py
+++ api/scpca_portal/models/factory_data_transforms.py
@@ -2,6 +2,10 @@ from typing import Dict
 
 
 def project_data_transform(data: Dict) -> Dict:
+    """
+    Transforms keys in project data dictionary to match project (and associated) model attributes.
+    """
+    # Transforms for Project model object creation
     data["has_bulk_rna_seq"] = data.pop("has_bulk", False)
     data["has_cite_seq_data"] = data.pop("has_CITE", False)
     data["has_multiplexed_data"] = data.pop("has_multiplex", False)
@@ -10,16 +14,30 @@ def project_data_transform(data: Dict) -> Dict:
     data["pi_name"] = data.pop("submitter", None)
     data["title"] = data.pop("project_title", None)
 
+    # Transforms for Contact model object creation
+    data["email"] = data.pop("contact_email", None)
+    data["name"] = data.pop("contact_name", None)
+
+    # Transforms for ExternalAccession model object creation
+    data["accession"] = data.pop("external_accession", None)
+    data["has_raw"] = data.pop("external_accession_raw", False)
+    data["accession_url"] = data.pop("external_accession_url", None)
+
+    # Transforms for Publication model object creation
+    data["doi"] = data.pop("citation_doi", None)
+
     return data
 
 
 def sample_data_transform(data: Dict) -> Dict:
+    """Transform keys in sample data dictionary to match sample model attributes"""
     data["age_at_diagnosis"] = data.pop("age")
 
     return data
 
 
 def library_data_transform(data: Dict) -> Dict:
+    """Transform keys in library data dictionary to match sample model attributes"""
     data["scpca_library_id"] = data.pop("library_id")
     data["scpca_sample_id"] = data.pop("sample_id")
 
