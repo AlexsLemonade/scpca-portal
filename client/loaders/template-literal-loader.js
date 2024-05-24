2fd36251e1eb94307880a2fe93045e91458e23d6
David Mejia <davidsmejia@gmail.com>
2024-05-24 14:23:46 -0400

    assume unescaped backticks

--- client/loaders/template-literal-loader.js
+++ client/loaders/template-literal-loader.js
@@ -1,10 +1,10 @@
 // This is a simple loader used with raw-loader for importing
 // markdown to be consumed by grommet/Markdown
+// escape backticks to prevent build errors
+// TODO: Update to support blocked (ie: ```)
 
 module.exports = (source) => {
-  // escape backticks to prevent build errors
-  const formatted = source.replace(/[^\\]`/g, '\\`')
-
+  const formatted = source.replace(/`/g, '\\`')
   // eslint-disable-next-line
   return eval(`\`${formatted}\``)
 }
