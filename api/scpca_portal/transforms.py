c436e45a19846273f0ad63d089121c31f78394b4
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-05-24 10:08:55 -0400

    exchange metadata key transform functions for data structures

--- api/scpca_portal/transforms.py
+++ api/scpca_portal/transforms.py
@@ -1,86 +1,56 @@
 from collections import namedtuple
-from typing import Dict, List, Type
+from typing import Dict, Type
 
 from django.apps import apps
 from django.db import models
 
-KeyTransform = namedtuple("KeyTransform", ["new_key", "old_key", "default_value"])
+PROJECT_METADATA_KEYS = [
+    # Fields used in Project model object creation
+    ("has_bulk", "has_bulk_rna_seq", False),
+    ("has_CITE", "has_cite_seq_data", False),
+    ("has_multiplex", "has_multiplexed_data", False),
+    ("has_spatial", "has_spatial_data", False),
+    ("PI", "human_readable_pi_name", None),
+    ("submitter", "pi_name", None),
+    ("project_title", "title", None),
+    # Fields used in Contact model object creation
+    ("contact_email", "email", None),
+    ("contact_name", "name", None),
+    # Fields used in ExternalAccession model object creation
+    ("external_accession", "accession", None),
+    ("external_accession_raw", "has_raw", False),
+    ("external_accession_url", "accession_url", None),
+    # Field used in Publication model object creation
+    ("citation_doi", "doi", None),
+]
 
+SAMPLE_METADATA_KEYS = [
+    ("age", "age_at_diagnosis", None),
+]
 
-def project_data_transform() -> List[KeyTransform]:
-    """
-    Transforms keys in project data dictionary to match project (and associated) model attributes.
-    """
-    # Transforms for Project model object creation
-    project_data_key_transforms = [
-        ("has_bulk_rna_seq", "has_bulk", False),
-        ("has_cite_seq_data", "has_CITE", False),
-        ("has_multiplexed_data", "has_multiplex", False),
-        ("has_spatial_data", "has_spatial", False),
-        ("human_readable_pi_name", "PI", None),
-        ("pi_name", "submitter", None),
-        ("title", "project_title", None),
-    ]
+LIBRARY_METADATA_KEYS = [
+    ("library_id", "scpca_library_id", None),
+    ("sample_id", "scpca_sample_id", None),
+    # Field only included in Single cell (and Multiplexed) libraries
+    ("filtered_cells", "filtered_cell_count", None),
+]
 
-    # Transforms for Contact model object creation
-    project_data_key_transforms.extend(
-        [
-            ("email", "contact_email", None),
-            ("name", "contact_name", None),
-        ]
-    )
-
-    # Transforms for ExternalAccession model object creation
-    project_data_key_transforms.extend(
-        [
-            ("accession", "external_accession", None),
-            ("has_raw", "external_accession_raw", False),
-            ("accession_url", "external_accession_url", None),
-        ]
-    )
-
-    # Transforms for Publication model object creation
-    project_data_key_transforms.extend(
-        [
-            ("doi", "citation_doi", None),
-        ]
-    )
-
-    return [KeyTransform._make(element) for element in project_data_key_transforms]
-
-
-def sample_data_transform() -> Dict:
-    """Transform keys in sample data dictionary to match sample model attributes"""
-    sample_data_key_transforms = [("age_at_diagnosis", "age", None)]
-
-    return [KeyTransform._make(element) for element in sample_data_key_transforms]
-
-
-def library_data_transform(data: Dict) -> Dict:
-    """Transform keys in library data dictionary to match sample model attributes"""
-    library_data_key_transforms = [
-        ("scpca_library_id", "library_id", None),
-        ("scpca_sample_id", "sample_id", None),
-    ]
-
-    if "filtered_cells" in data:
-        library_data_key_transforms.append(("filtered_cell_count", "filtered_cells", None))
-
-    return [KeyTransform._make(element) for element in library_data_key_transforms]
+KeyTransform = namedtuple("KeyTransform", ["old_key", "new_key", "default_value"])
 
 
 def transform_keys(model: Type[models.Model], data_dict: Dict) -> Dict:
     key_transforms = []
     if model == apps.get_model("scpca_portal", "Project"):
-        key_transforms = project_data_transform()
+        key_transforms = [KeyTransform._make(element) for element in PROJECT_METADATA_KEYS]
     elif model == apps.get_model("scpca_portal", "Sample"):
         key_transforms = (
-            library_data_transform(data_dict)
+            [KeyTransform._make(element) for element in LIBRARY_METADATA_KEYS]
             if "library_id" in data_dict
-            else sample_data_transform()
+            else [KeyTransform._make(element) for element in SAMPLE_METADATA_KEYS]
         )
 
     for element in key_transforms:
-        data_dict[element.new_key] = data_dict.pop(element.old_key, element.default_value)
+        if element.old_key in data_dict:
+            data_dict[element.new_key] = data_dict.pop(element.old_key, element.default_value)
 
     return data_dict
