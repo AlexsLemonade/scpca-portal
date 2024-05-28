589ebdece8d04be06801608a4f73497ca884e444
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-05-28 12:43:17 -0400

    add Library::bulk_create_from_dicts method

--- api/scpca_portal/models/library.py
+++ api/scpca_portal/models/library.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Dict, List
+
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
@@ -36,14 +39,37 @@ class Library(TimestampedModel):
     workflow_version = models.TextField()
 
     @classmethod
-    def get_from_dict(cls, data, formats, is_multiplexed, modality):
+    def get_from_dict(cls, data, formats):
         library = cls(
             formats=formats,
-            is_multiplexed=is_multiplexed,
+            is_multiplexed=("demux_samples" in data),
             metadata=data,
-            modality=modality,
+            modality=(
+                Library.Modalities.SINGLE_CELL
+                if "filtered_cell_count" in data
+                else Library.Modalities.SPATIAL
+            ),
             scpca_id=data["library_id"],
             workflow_version=data["workflow_version"],
         )
 
         return library
+
+    @classmethod
+    def bulk_create_from_dicts(cls, library_jsons: List[Dict], sample, sample_dir: Path) -> None:
+        libraries = []
+        for library_json in library_jsons:
+            formats = []
+            if "filtered_cell_count" in library_json:
+                if any(sample_dir.glob("*.rds")):
+                    formats.append(Library.FileFormats.SINGLE_CELL_EXPERIMENT)
+                if any(sample_dir.glob("*.h5ad")):
+                    formats.append(Library.FileFormats.ANN_DATA)
+            else:
+                if any(sample_dir.glob("*_spatial/*.rds")):
+                    formats.append(Library.FileFormats.SINGLE_CELL_EXPERIMENT)
+
+            libraries.append(Library.get_from_dict(library_json, formats))
+
+        Library.objects.bulk_create(libraries)
+        sample.libraries.add(*libraries)
