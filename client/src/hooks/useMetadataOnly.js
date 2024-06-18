06be077ebea7c8e7c84ac5e5d90f71a377cac85a
Nozomi Ichihara <nozomione.online@gmail.com>
2024-06-18 17:43:18 -0400

    (rename) rename the variable name pural, metadataComputedFiles in useMetadataOnly

--- client/src/hooks/useMetadataOnly.js
+++ client/src/hooks/useMetadataOnly.js
@@ -2,16 +2,16 @@ import { filterWhere } from 'helpers/filterWhere'
 
 // TODO: Implement the logic to handle multiple metadata files (i.e. project and portal-wide) later
 export const useMetadataOnly = (computedFiles) => {
-  const metadataComputedFile = filterWhere(computedFiles, {
+  const metadataComputedFiles = filterWhere(computedFiles, {
     metadata_only: true
   })
   // Check the availability of metadata_only file
-  const isMetadataOnlyAvailable = metadataComputedFile.length > 0
+  const isMetadataOnlyAvailable = metadataComputedFiles.length > 0
 
   // NOTE: The current logic only supports the project level metadata file,
   // thus the first item is returned
   return {
-    metadataComputedFile: metadataComputedFile[0],
+    metadataComputedFile: metadataComputedFiles[0],
     isMetadataOnlyAvailable
   }
 }
