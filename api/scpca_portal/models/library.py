d60dce6487f43b9d9a32bf51bd967c4b866a9883
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-07-17 12:23:32 -0400

    add support in new implementation for merged sample level html reports to be nested in individual_reports directory

--- api/scpca_portal/models/library.py
+++ api/scpca_portal/models/library.py
@@ -246,6 +246,10 @@ class Library(TimestampedModel):
         # Transform merged project data files to no longer be nested in a merged directory
         if file_path.parent.name == "merged":
             output_path = file_path.relative_to(path_parts[0] / path_parts[1])
+        # Nest sample reports into individual_reports directory in merged download
+        # The merged summmary html file should not go into this directory
+        elif download_config.get("includes_merged", False) and output_path.suffix == ".html":
+            output_path = Path("individual_reports") / output_path
 
         # Comma separated lists of multiplexed samples should become underscore separated
         return Path(str(output_path).replace(",", "_"))
