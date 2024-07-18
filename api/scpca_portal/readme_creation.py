2ae7aa6908dabaf588734192d38f5764bcfda210
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-07-18 17:14:12 -0400

    add readme_creation::create_readme_file, add TEMPLATE_PATHS data structure in readme_creation module

--- api/scpca_portal/readme_creation.py
+++ api/scpca_portal/readme_creation.py
@@ -1,6 +1,8 @@
 from typing import Dict
 
-from scpca_portal import common
+from django.template.loader import render_to_string
+
+from scpca_portal import common, utils
 
 OUTPUT_README_FILE_NAME = "README.md"
 
@@ -51,3 +53,37 @@ def get_readme_from_download_config(download_config: Dict):
             return README_SINGLE_CELL_FILE_PATH
         case {"modality": "SPATIAL"}:
             return README_SPATIAL_FILE_PATH
+
+
+TEMPLATE_PATHS = {
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT": README_TEMPLATE_PATH / "single_cell.md",
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MERGED": README_TEMPLATE_PATH / "single_cell_merged.md",
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MULTIPLEXED": README_TEMPLATE_PATH / "metadata_only.md",
+    "SINGLE_CELL_ANN_DATA": README_TEMPLATE_PATH / "anndata.md",
+    "SINGLE_CELL_ANN_DATA_MERGED": README_TEMPLATE_PATH / "anndata_merged.md",
+    "SPATIAL_SINGLE_CELL_EXPERIMENT": README_TEMPLATE_PATH / "spatial.md",
+    "METADATA_ONLY": README_TEMPLATE_PATH / "metadata_only.md",
+}
+
+
+def create_readme_file(download_config: Dict, project=None, sample=None) -> str:
+    readme_template_key_parts = [download_config["modality"], download_config["format"]]
+    if download_config["includes_merged"]:
+        readme_template_key_parts.append("MERGED")
+    elif not download_config["excludes_multiplexed"]:
+        readme_template_key_parts.append("MULTIPLEXED")
+    elif download_config["metadata_only"]:
+        readme_template_key_parts = ["METADATA_ONLY"]
+
+    if sample:
+        project = sample.project
+
+    return render_to_string(
+        TEMPLATE_PATHS["_".join(readme_template_key_parts)],
+        context={
+            "additional_terms": project.get_additional_terms(),
+            "date": utils.get_today_string(),
+            "project_accession": project.scpca_id,
+            "project_url": project.url,
+        },
+    ).strip()
