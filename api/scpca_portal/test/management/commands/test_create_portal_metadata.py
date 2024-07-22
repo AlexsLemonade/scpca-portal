f4df1159ca5ee2b20cc3e45b165b7f31e79cf74b
Nozomi Ichihara <nozomione.online@gmail.com>
2024-07-22 19:26:20 -0400

    (edit) rename the setup_database method to load_test_data, remove 'project_id' kwarg when calling the load_data command, check PROJECT_COUNT, SAMPLES_COUNT, and LIBRARIES_COUNT by quering libraries metadata from the Library model to verify database setup instead of checking an explicit list of project ids

--- api/scpca_portal/test/management/commands/test_create_portal_metadata.py
+++ api/scpca_portal/test/management/commands/test_create_portal_metadata.py
@@ -4,7 +4,7 @@ from django.test import TransactionTestCase
 
 from scpca_portal import common
 from scpca_portal.management.commands import create_portal_metadata, load_data
-from scpca_portal.models import Project
+from scpca_portal.models import Library
 
 # NOTE: Test data bucket is defined in `scpca_porta/common.py`.
 # When common.INPUT_BUCKET_NAME is changed, please delete the contents of
@@ -23,26 +23,37 @@ class TestCreatePortalMetadata(TransactionTestCase):
         super().tearDownClass()
         shutil.rmtree(common.OUTPUT_DATA_PATH, ignore_errors=True)
 
-    def setup_database(self, project_ids):
-        for project_id in project_ids:
-            self.loader.load_data(
-                allowed_submitters=list(ALLOWED_SUBMITTERS),
-                clean_up_input_data=False,
-                clean_up_output_data=False,
-                max_workers=4,
-                reload_all=False,
-                reload_existing=False,
-                scpca_project_id=project_id,
-                update_s3=False,
-            )
+    def load_test_data(self):
+        self.loader.load_data(
+            allowed_submitters=list(ALLOWED_SUBMITTERS),
+            clean_up_input_data=False,
+            clean_up_output_data=False,
+            max_workers=4,
+            reload_all=False,
+            reload_existing=False,
+            update_s3=False,
+        )
 
     def test_create_portal_metadata(self):
         # Set up the database
-        project_ids = ["SCPCP999990", "SCPCP999991", "SCPCP999992"]
-        self.setup_database(project_ids)
+        PROJECT_COUNT = 3
+        SAMPLES_COUNT = 8
+        LIBRARIES_COUNT = 7
 
-        # Make sure that the number of projects created matches the length of project_ids
-        self.assertEqual(Project.objects.count(), len(project_ids))
+        self.load_test_data()
+
+        libraries = Library.objects.all()
+        libraries_metadata = [
+            lib for library in libraries for lib in library.get_combined_library_metadata()
+        ]
+
+        self.assertEqual(
+            len(set(lib["scpca_project_id"] for lib in libraries_metadata)), PROJECT_COUNT
+        )
+        self.assertEqual(
+            len(set(lib["scpca_sample_id"] for lib in libraries_metadata)), SAMPLES_COUNT
+        )
+        self.assertEqual(libraries.count(), LIBRARIES_COUNT)
 
         # Once the database is set up correctly, run the create_portal_metadata management command
         self.processor.create_portal_metadata(clean_up_output_data=False)
