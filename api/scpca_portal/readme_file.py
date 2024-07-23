b8f5ef15d4889f8d2665d832294a3c702e424b32
Nozomi Ichihara <nozomione.online@gmail.com>
2024-07-23 10:26:48 -0400

    Merge dev updates into feature/portal-medatada-command (#807)
    * create readme_creation.py module, move all readme constants and ComputedFile::get_readme_from_download_config method to new file, update references in Project and ComputedFile models that call these constants
    
    * add readme_creation::create_readme_file, add TEMPLATE_PATHS data structure in readme_creation module
    
    * fix bug in readme_creation::create_readme_file
    
    * swap out readme implementations in ComputedFile::get_project_file and ComputedFile::get_sample_file
    
    * remove eager readme generation logic from Project model and no longer used constants in readme_creation module
    
    * update constant names and method name and signature in readme_creation module, propogated changes to caller methods in ComputedFile model
    
    * change file name from readme_creation to readme_file
    
    * update references to readme_file in ComputedFile in light of name change
    
    * remove leftover project keyword arg reference in ComputedFile
    
    ---------
    
    Co-authored-by: Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
    Co-authored-by: Avrohom Gottlieb <108290446+avrohomgottlieb@users.noreply.github.com>

--- api/scpca_portal/readme_file.py
+++ api/scpca_portal/readme_file.py
@@ -7,7 +7,19 @@ from scpca_portal import common, utils
 OUTPUT_NAME = "README.md"
 
 TEMPLATE_ROOT = common.TEMPLATE_PATH / "readme"
+<<<<<<< HEAD
 TEMPLATE_FILE_PATH = TEMPLATE_ROOT / "readme.md"
+=======
+TEMPLATE_PATHS = {
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT": TEMPLATE_ROOT / "single_cell.md",
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MERGED": TEMPLATE_ROOT / "single_cell_merged.md",
+    "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MULTIPLEXED": TEMPLATE_ROOT / "metadata_only.md",
+    "SINGLE_CELL_ANN_DATA": TEMPLATE_ROOT / "anndata.md",
+    "SINGLE_CELL_ANN_DATA_MERGED": TEMPLATE_ROOT / "anndata_merged.md",
+    "SPATIAL_SINGLE_CELL_EXPERIMENT": TEMPLATE_ROOT / "spatial.md",
+    "METADATA_ONLY": TEMPLATE_ROOT / "metadata_only.md",
+}
+>>>>>>> fd428a8 (Merge dev updates into feature/portal-medatada-command (#807))
 
 
 def get_file_contents(download_config: Dict, project) -> str:
@@ -21,6 +33,7 @@ def get_file_contents(download_config: Dict, project) -> str:
         if download_config["metadata_only"]:
             readme_template_key_parts = ["METADATA_ONLY"]
 
+<<<<<<< HEAD
     # For the contents section
     contents_template = f"{TEMPLATE_ROOT}/contents/{'_'.join(readme_template_key_parts)}.md"
 
@@ -31,5 +44,14 @@ def get_file_contents(download_config: Dict, project) -> str:
             "download_config": download_config,
             "contents_template": contents_template,
             "projects": [project],
+=======
+    return render_to_string(
+        TEMPLATE_PATHS["_".join(readme_template_key_parts)],
+        context={
+            "additional_terms": project.get_additional_terms(),
+            "date": utils.get_today_string(),
+            "project_accession": project.scpca_id,
+            "project_url": project.url,
+>>>>>>> fd428a8 (Merge dev updates into feature/portal-medatada-command (#807))
         },
     ).strip()
