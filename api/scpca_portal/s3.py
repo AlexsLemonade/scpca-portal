57e14dcf2efca87da32c430c7f9a285cfcf006d8
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-07-25 16:43:11 -0400

    replace s3::download_data_files with s3::download_input_files, replace s3::download_metadata_files with s3::download_input_metadta, delete no longer used s3::download_s3_files, rename s3::list_s3_paths to s3::list_input_paths, and move function higher up in module

--- api/scpca_portal/s3.py
+++ api/scpca_portal/s3.py
@@ -36,13 +36,78 @@ def configure_aws_cli(**params):
         subprocess.check_call(command.split())
 
 
-def download_s3_files(bucket_name: str = common.INPUT_BUCKET_NAME, filters: List[str] = None):
-    """Download all files in a given bucket or a group of files according to a filter set.'"""
+def list_input_paths(
+    relative_path: Path = Path(),
+    *,
+    bucket_path: Path = Path(common.INPUT_BUCKET_NAME),
+    recursive: bool = True,
+):
+    """
+    Queries a path on an inputted s3 bucket
+    and returns bucket's existing content as a list of Path objects,
+    relative to (without) the bucket prefix.
+    """
+    root_path = Path(*bucket_path.parts, *relative_path.parts)
+    command_inputs = ["aws", "s3", "ls", f"s3://{root_path}"]
+
+    if recursive:
+        command_inputs.append("--recursive")
+
+    if "public" in str(bucket_path):
+        command_inputs.append("--no-sign-request")
+
+    try:
+        result = subprocess.run(command_inputs, capture_output=True, text=True, check=True)
+        output = result.stdout
+    except subprocess.CalledProcessError as error:
+        logger.error(
+            """
+            `{}`: Cause of error not returned, note: folder must exist and be non-empty
+            """.format(
+                error
+            )
+        )
+        return []
+
+    # The `aws s3 ls <bucket>` command returns a list of two types of entries:
+    # - Bucket Object Entries
+    # - Bucket Prefix Entries
+    # In order to create a standard API, where `entry.file_path` could be accessed
+    # irrespective of the entry type, we've created two named tuples which follow the return format
+    # of each of the bucket entry types.
+    BucketObjectEntry = namedtuple(
+        "BucketObjectEntry", ["date", "time", "size_in_bytes", "file_path"]
+    )
+    BucketPrefixEntry = namedtuple("BucketPrefixEntry", ["prefix_designation", "file_path"])
+    bucket_entries = []
+
+    for line in output.splitlines():
+        if line.strip().startswith("PRE"):
+            bucket_entries.append(BucketPrefixEntry._make(line.split()))
+        else:
+            bucket_entries.append(BucketObjectEntry._make(line.split()))
+
+    file_paths = [Path(entry.file_path) for entry in bucket_entries]
+
+    # recursive returns an absolute path (see docstring)
+    if recursive:
+        bucket_keys = Path(*bucket_path.parts[1:])
+        return [entry.relative_to(bucket_keys) for entry in file_paths]
+
+    return file_paths
+
+
+def download_input_files(file_paths: List[Path], *, bucket_name: str = common.INPUT_BUCKET_NAME):
+    """Download all passed data file paths which have not previously been downloaded.'"""
     command_parts = ["aws", "s3", "sync", f"s3://{bucket_name}", common.INPUT_DATA_PATH]
 
-    if filters:
-        command_parts.append("--exclude=*")
-        command_parts.extend([filter for filter in filters])
+    download_queue = [fp for fp in file_paths if not fp.exists()]
+    # If download_queue is empty, exit early
+    if not download_queue:
+        return
+
+    command_parts.append("--exclude=*")
+    command_parts.extend([f"--include={file_path}" for file_path in download_queue])
 
     if "public-test" in bucket_name:
         command_parts.append("--no-sign-request")
@@ -50,34 +115,16 @@ def download_s3_files(bucket_name: str = common.INPUT_BUCKET_NAME, filters: List
     subprocess.check_call(command_parts)
 
 
-def download_metadata_files(project_id: str = None) -> None:
+def download_input_metadata(*, bucket_name: str = common.INPUT_BUCKET_NAME):
     """Download all metadata files to the local file system."""
-    filters = (
-        ["--include=project_metadata.csv", f"--include={project_id}/*_metadata.*"]
-        if project_id
-        else ["--include=*_metadata.*"]
-    )
-    download_s3_files(filters=filters)
+    command_parts = ["aws", "s3", "sync", f"s3://{bucket_name}", common.INPUT_DATA_PATH]
 
+    command_parts.append("--exclude=*")
+    command_parts.append("--include=*_metadata.*")
 
-def download_data_files(data_file_paths: List[Path]):
-    """Download all passed data files paths which have not previously been downloaded.'"""
-    data_file_download_queue = [fp for fp in data_file_paths if not fp.exists()]
-    # If download_queue is empty, exit early
-    if not data_file_download_queue:
-        return
+    if "public-test" in bucket_name:
+        command_parts.append("--no-sign-request")
 
-    filters = [f"--include={data_file}" for data_file in data_file_download_queue]
-    download_s3_files(filters=filters)
-
-
-def upload_s3_file(computed_file) -> None:
-    """Upload a computed file to S3 using the AWS CLI tool."""
-
-    aws_path = f"s3://{settings.AWS_S3_BUCKET_NAME}/{computed_file.s3_key}"
-    command_parts = ["aws", "s3", "cp", str(computed_file.zip_file_path), aws_path]
-
-    logger.info(f"Uploading {computed_file}")
     subprocess.check_call(command_parts)
 
 
@@ -118,72 +165,3 @@ def create_download_url(self):
             },
             ExpiresIn=60 * 60 * 24 * 7,  # 7 days in seconds.
         )
-
-
-"""
-The `aws s3 ls <bucket>` command called in `list_s3_paths()` returns a list of two types of entries:
-- Bucket Object Entries
-- Bucket Prefix Entries
-In order to create a standard API, where `entry.file_path` could be accessed
-irrespective of the entry type, we've created two named tuples which follow the return format
-of each of the bucket entry types.
-"""
-BucketObjectEntry = namedtuple("BucketObjectEntry", ["date", "time", "size_in_bytes", "file_path"])
-BucketPrefixEntry = namedtuple("BucketPrefixEntry", ["prefix_designation", "file_path"])
-
-
-def list_s3_paths(
-    relative_path: Path = Path(),
-    *,
-    bucket_path: Path = Path(common.INPUT_BUCKET_NAME),
-    recursive: bool = True,
-):
-    """
-    Queries a path on an inputted s3 bucket
-    and returns bucket's existing content as a list of Path objects,
-    relative to (without) the bucket prefix.
-
-    The `aws s3 ls <bucket>` command returns a list of two types of entries:
-    - Bucket Object Entries
-    - Bucket Prefix Entries
-    In order to create a standard API, where `entry.file_path` could be accessed
-    irrespective of the entry type, we've created two named tuples which follow the return format
-    of each of the bucket entry types.
-    """
-    root_path = Path(*bucket_path.parts, *relative_path.parts)
-    command_inputs = ["aws", "s3", "ls", f"s3://{root_path}"]
-
-    if recursive:
-        command_inputs.append("--recursive")
-
-    if "public" in str(bucket_path):
-        command_inputs.append("--no-sign-request")
-
-    try:
-        result = subprocess.run(command_inputs, capture_output=True, text=True, check=True)
-        output = result.stdout
-    except subprocess.CalledProcessError as error:
-        logger.error(
-            """
-            `{}`: Cause of error not returned, note: folder must exist and be non-empty
-            """.format(
-                error
-            )
-        )
-        return []
-
-    bucket_entries = []
-    for line in output.splitlines():
-        if line.strip().startswith("PRE"):
-            bucket_entries.append(BucketPrefixEntry._make(line.split()))
-        else:
-            bucket_entries.append(BucketObjectEntry._make(line.split()))
-
-    file_paths = [Path(entry.file_path) for entry in bucket_entries]
-
-    # recursive returns an absolute path (see docstring)
-    if recursive:
-        bucket_keys = Path(*bucket_path.parts[1:])
-        return [entry.relative_to(bucket_keys) for entry in file_paths]
-
-    return file_paths
