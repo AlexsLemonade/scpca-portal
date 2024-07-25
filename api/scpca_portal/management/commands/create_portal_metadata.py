4fde00352ca35e5ebc945a951cfb9af6ba613d9e
Nozomi Ichihara <nozomione.online@gmail.com>
2024-07-25 17:15:08 -0400

    797 - Scaffolding the porta-wide metadata management command and test files (#804)
    * create the management command file for the portal-wide metadata and register it to sportal
    
    * create the test file for the portal-wide matadata management command test_create_portal_metadata
    
    * (edit) rename the setup_database method to load_test_data, remove 'project_id' kwarg when calling the load_data command, check PROJECT_COUNT, SAMPLES_COUNT, and LIBRARIES_COUNT by quering libraries metadata from the Library model to verify database setup instead of checking an explicit list of project ids
    
    * (edit) check against queryset objects count rather than IDs for projects, samples, and libraries
    
    * (minor) fix a typo and remove comments

---
+++ api/scpca_portal/management/commands/create_portal_metadata.py
@@ -0,0 +1,36 @@
+import logging
+from argparse import BooleanOptionalAction
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+class Command(BaseCommand):
+    help = """Creates a computed file and zip for portal-wide metadata,
+     saves the instance to the databse, and
+     uploads the zip file to S3 bucket.
+    """
+
+    @staticmethod
+    def clean_up_output_data():
+        """Cleans up the output data files after processing the computed file"""
+        logger.info("Cleaning up output data")
+        # This static method may not be required using buffers
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--clean-up-output-data", action=BooleanOptionalAction, default=settings.PRODUCTION
+        )
+
+    def handle(self, *args, **kwargs):
+        self.create_portal_metadata(**kwargs)
+
+    def create_portal_metadata(self, **kwargs):
+        logger.info("Creating the portal-wide metadata computed file")
+
+        if kwargs["clean_up_output_data"]:
+            self.clean_up_output_data()
