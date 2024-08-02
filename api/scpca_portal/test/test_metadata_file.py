e23259ae5a51f35a929ff6b9f7586969da926b80
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-02 11:55:40 -0400

    update comments and code readability in test_metadata_file::TestGetFileContents tests

--- api/scpca_portal/test/test_metadata_file.py
+++ api/scpca_portal/test/test_metadata_file.py
@@ -14,12 +14,14 @@ class TestGetFileContents(TestCase):
         for library in self.libraries:
             library.samples.add(SampleFactory(project=library.project))
 
-        # Allow easy access to one library for easy modification
+        # Allow quick access to one library for easy modification
         self.first_library = self.libraries[0]
 
+        # Pre-compute value for easy reference (not all tests use this value)
         self.libraries_metadata = self.get_libraries_metadata(self.libraries)
 
     def get_libraries_metadata(self, libraries: List) -> List[Dict]:
+        """Lazy compute libraries_metadata value if libraries are mutated."""
         return [
             lib_md for library in libraries for lib_md in library.get_combined_library_metadata()
         ]
@@ -34,12 +36,9 @@ class TestGetFileContents(TestCase):
         with io.StringIO(output_file_contents) as output_buffer:
             output_list_of_dicts = list(csv.DictReader(output_buffer, delimiter=common.TAB))
             for output_dict in output_list_of_dicts:
-                input_dict = next(
-                    library_metadata
-                    for library_metadata in self.libraries_metadata
-                    if library_metadata["scpca_library_id"] == output_dict["scpca_library_id"]
-                )
-                # Cast all values in input_dict to string to match types in string buffer
+                id = "scpca_library_id"
+                input_dict = next(lm for lm in self.libraries_metadata if lm[id] == output_dict[id])
+                # Cast all values in input_dict to str to match str types of string buffer
                 formatted_input_dict = {k: str(v) for k, v in input_dict.items()}
                 self.assertEqual(output_dict, formatted_input_dict)
 
