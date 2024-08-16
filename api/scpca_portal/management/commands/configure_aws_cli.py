ce28d6c2ed9ede9ffd4a1adae55b80f44047de17
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-16 15:11:44 -0400

    remove .aws dir volume mount in docker-compose, remove extraneous sudo-ing of aws configure commands

--- api/scpca_portal/management/commands/configure_aws_cli.py
+++ api/scpca_portal/management/commands/configure_aws_cli.py
@@ -40,18 +40,17 @@ class Command(BaseCommand):
     ):
         commands = [
             # https://docs.aws.amazon.com/cli/latest/topic/s3-config.html#payload-signing-enabled
-            "sudo aws configure set default.s3.payload_signing_enabled false",
+            "aws configure set default.s3.payload_signing_enabled false",
             # https://docs.aws.amazon.com/cli/latest/topic/s3-config.html#max-concurrent-requests
-            "sudo aws configure set default.s3.max_concurrent_requests"
-            f"{s3_max_concurrent_requests}",
+            f"aws configure set default.s3.max_concurrent_requests {s3_max_concurrent_requests}"
             # https://docs.aws.amazon.com/cli/latest/topic/s3-config.html#multipart-chunksize
-            f"sudo aws configure set default.s3.multipart_chunksize {s3_multipart_chunk_size}MB",
+            f"aws configure set default.s3.multipart_chunksize {s3_multipart_chunk_size}MB",
         ]
 
         if s3_max_bandwidth:
             commands.append(
                 # https://docs.aws.amazon.com/cli/latest/topic/s3-config.html#max-bandwidth
-                f"sudo aws configure set default.s3.max_bandwidth {s3_max_bandwidth}MB/s",
+                f"aws configure set default.s3.max_bandwidth {s3_max_bandwidth}MB/s",
             )
 
         logger.info("Configuring AWS CLI...")
