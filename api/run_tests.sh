37255f099834bba3d6ef2823c29160e95404651f
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-20 15:39:57 -0400

    add .aws/ dir creation check in run_tests script and add back in configure_aws_cli command

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -2,5 +2,7 @@
 
 python wait_for_postgres.py
 export AWS_DEFAULT_REGION=us-east-1
+mkdir -p ~/.aws
+./manage.py configure_aws_cli
 coverage run ./manage.py test -v 2 --no-input "$@"
 coverage report -m
