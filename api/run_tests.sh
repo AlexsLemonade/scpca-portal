3ca5024ff30398ef7b79183712a2d4b396d8a34d
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-20 16:13:30 -0400

    add step to push.yml gha file, remove tapping .aws dir and config files in run_tests script

--- api/run_tests.sh
+++ api/run_tests.sh
@@ -2,8 +2,6 @@
 
 python wait_for_postgres.py
 export AWS_DEFAULT_REGION=us-east-1
-mkdir -p ~/.aws
-touch ~/.aws/config
 ./manage.py configure_aws_cli
 coverage run ./manage.py test -v 2 --no-input "$@"
 coverage report -m
