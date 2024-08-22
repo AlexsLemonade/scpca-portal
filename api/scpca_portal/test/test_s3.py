0da9c7d6451b01c533ea51d66bb1e37143cccda6
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-22 15:58:49 -0400

    add input tests to test_s3 on function s3::list_input_paths

--- api/scpca_portal/test/test_s3.py
+++ api/scpca_portal/test/test_s3.py
@@ -13,12 +13,56 @@ class TestListInputPaths(TestCase):
         self.input_bucket_name = settings.AWS_S3_INPUT_BUCKET_NAME
         self.nested_input_bucket_dirs = self.get_nested_input_bucket_dirs(self.input_bucket_name)
         self.empty_relative_path = Path()
+        self.default_bucket_name = "input-bucket"
+        self.default_relative_path = Path("relative-path")
 
     def get_nested_input_bucket_dirs(self, bucket_name: str) -> str:
         if nested_input_bucket_path_parts := bucket_name.split("/")[1:]:
             return "/".join(nested_input_bucket_path_parts)
 
         return ""
+
+    @patch("subprocess.run")
+    def test_list_input_paths_correct_command_inputs(self, mock_run):
+        s3.list_input_paths(self.default_relative_path, self.default_bucket_name, recursive=False)
+        mock_run.assert_called_once()
+        expected_command_inputs = [
+            "aws",
+            "s3",
+            "ls",
+            f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+        ]
+        actual_command_inputs = mock_run.call_args.args[0]
+        self.assertEqual(expected_command_inputs, actual_command_inputs)
+
+    @patch("subprocess.run")
+    def test_list_input_paths_recursive_flag_passed(self, mock_run):
+        s3.list_input_paths(self.default_relative_path, self.default_bucket_name, recursive=True)
+        mock_run.assert_called_once()
+        expected_command_inputs = [
+            "aws",
+            "s3",
+            "ls",
+            f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+            "--recursive",
+        ]
+        actual_command_inputs = mock_run.call_args.args[0]
+        self.assertEqual(expected_command_inputs, actual_command_inputs)
+
+    @patch("subprocess.run")
+    def test_list_input_paths_public_in_bucket_name(self, mock_run):
+        public_bucket_name = "public-input-bucket"
+        s3.list_input_paths(self.default_relative_path, public_bucket_name, recursive=False)
+        mock_run.assert_called_once()
+        expected_command_inputs = [
+            "aws",
+            "s3",
+            "ls",
+            f"s3://{public_bucket_name}/{self.default_relative_path}",
+            "--no-sign-request",
+        ]
+        actual_command_inputs = mock_run.call_args.args[0]
+        self.assertEqual(expected_command_inputs, actual_command_inputs)
 
     @patch("subprocess.run")
     def test_list_input_paths_success(self, mock_run):
