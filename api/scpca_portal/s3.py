c3a4f51b9322ff115ebfa5d49d537fe0bdaba9d1
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-08-23 12:31:57 -0400

    fix possible end of s3 resource slash bug in s3::list_input_paths, clean up tests in test_s3 module

--- api/scpca_portal/s3.py
+++ api/scpca_portal/s3.py
@@ -26,7 +26,9 @@ def list_input_paths(
     """
     bucket_path = Path(bucket_name)
     root_path = Path(*bucket_path.parts, *relative_path.parts)
-    command_inputs = ["aws", "s3", "ls", f"s3://{root_path}"]
+    # Note (when recursive is not passed): without a '/' at the end of the s3 resource (see below),
+    # dir contents will not be listed, but rather an entry of the relative path itself
+    command_inputs = ["aws", "s3", "ls", f"s3://{root_path}/"]
 
     if recursive:
         command_inputs.append("--recursive")
--- api/scpca_portal/test/test_s3.py
+++ api/scpca_portal/test/test_s3.py
@@ -10,50 +10,50 @@ from scpca_portal import s3
 class TestListInputPaths(TestCase):
     def setUp(self):
         self.default_bucket_name = "input-bucket"
-        self.default_relative_path = Path("relative-path")
+        self.default_relative_path = Path("relative-path/")
 
     @patch("subprocess.run")
     def test_list_input_paths_correct_command_inputs(self, mock_run):
         s3.list_input_paths(self.default_relative_path, self.default_bucket_name, recursive=False)
-        mock_run.assert_called_once()
         expected_command_inputs = [
             "aws",
             "s3",
             "ls",
-            f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+            f"s3://{self.default_bucket_name}/{self.default_relative_path}/",
         ]
         actual_command_inputs = mock_run.call_args.args[0]
         self.assertEqual(expected_command_inputs, actual_command_inputs)
+        mock_run.assert_called_once()
 
     @patch("subprocess.run")
     def test_list_input_paths_recursive_flag_passed_by_default(self, mock_run):
         s3.list_input_paths(self.default_relative_path, self.default_bucket_name)
-        mock_run.assert_called_once()
         expected_command_inputs = [
             "aws",
             "s3",
             "ls",
-            f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+            f"s3://{self.default_bucket_name}/{self.default_relative_path}/",
             # No need to pass recursive=True to function call as this is default behavior
             "--recursive",
         ]
         actual_command_inputs = mock_run.call_args.args[0]
         self.assertEqual(expected_command_inputs, actual_command_inputs)
+        mock_run.assert_called_once()
 
     @patch("subprocess.run")
     def test_list_input_paths_public_in_bucket_name(self, mock_run):
         public_bucket_name = "public-input-bucket"
         s3.list_input_paths(self.default_relative_path, public_bucket_name, recursive=False)
-        mock_run.assert_called_once()
         expected_command_inputs = [
             "aws",
             "s3",
             "ls",
-            f"s3://{public_bucket_name}/{self.default_relative_path}",
+            f"s3://{public_bucket_name}/{self.default_relative_path}/",
             "--no-sign-request",
         ]
         actual_command_inputs = mock_run.call_args.args[0]
         self.assertEqual(expected_command_inputs, actual_command_inputs)
+        mock_run.assert_called_once()
 
     @patch("subprocess.run")
     def test_list_input_paths_command_success_recursive(self, mock_run):
@@ -62,17 +62,21 @@ class TestListInputPaths(TestCase):
                 "aws",
                 "s3",
                 "ls",
-                f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+                f"s3://{self.default_bucket_name}/{self.default_relative_path}/",
             ],
             returncode=0,
-            stdout=f"2024-06-10 10:00:00 1234 {self.default_relative_path}/file.txt\n"
-            f"PRE {self.default_relative_path}/dir/",
+            stdout=f"2024-06-10 10:00:00 1234 {self.default_relative_path}/file1.txt\n"
+            f"2024-06-10 10:00:00 1234 {self.default_relative_path}/file2.txt",
         )
         result = s3.list_input_paths(
             self.default_relative_path, self.default_bucket_name, recursive=True
         )
-        expected = [self.default_relative_path / "file.txt", self.default_relative_path / "dir/"]
+        expected = [
+            self.default_relative_path / "file1.txt",
+            self.default_relative_path / "file2.txt",
+        ]
         self.assertEqual(result, expected)
+        mock_run.assert_called_once()
 
     @patch("subprocess.run")
     def test_list_input_paths_command_success_non_recursive(self, mock_run):
@@ -81,21 +85,26 @@ class TestListInputPaths(TestCase):
                 "aws",
                 "s3",
                 "ls",
-                f"s3://{self.default_bucket_name}/{self.default_relative_path}",
+                # Without a '/' at the end of the s3 resource location,
+                # dir contents will not be listed,
+                # but rather an entry of the relative path itself
+                f"s3://{self.default_bucket_name}/{self.default_relative_path}/",
             ],
             returncode=0,
-            stdout=f"PRE {self.default_relative_path}",
+            stdout="PRE dir1/\nPRE dir2/",
         )
-        expected = [self.default_relative_path]
+        expected = [Path("dir1/"), Path("dir2/")]
         result = s3.list_input_paths(self.default_relative_path, self.default_bucket_name)
         self.assertEqual(expected, result)
+        mock_run.assert_called_once()
 
     @patch("subprocess.run")
     def test_list_input_paths_command_failure(self, mock_run):
         mock_run.side_effect = subprocess.CalledProcessError(
             returncode=1,
-            cmd=f"aws s3 ls s3://{self.default_bucket_name}/{self.default_relative_path}",
+            cmd=f"aws s3 ls s3://{self.default_bucket_name}/{self.default_relative_path}/",
         )
         result = s3.list_input_paths(self.default_relative_path, self.default_bucket_name)
         expected = []
         self.assertEqual(result, expected)
+        mock_run.assert_called_once()
