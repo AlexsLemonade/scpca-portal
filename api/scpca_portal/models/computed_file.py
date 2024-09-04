93bfd4d243281a8176d364c4dd761b4a594616e0
Nozomi Ichihara <nozomione.online@gmail.com>
2024-09-04 13:21:25 -0400

    (pre-commit) remove the old unused redandant method for the portal metadata from computed_file model

--- api/scpca_portal/models/computed_file.py
+++ api/scpca_portal/models/computed_file.py
@@ -142,31 +142,6 @@ class ComputedFile(CommonDataAttributes, TimestampedModel):
         return computed_file
 
     @classmethod
-    def get_portal_metadata_file(cls, projects, download_config: Dict) -> Self:
-        """
-        Queries all libraries to aggregate the combined metadata,
-        writes the aggregated combined metadata to a portal metadata file,
-        computes a zip archive with metadata and readme files, and
-        creates a ComputedFile object which it then saves to the db.
-        """
-        with ZipFile(common.OUTPUT_PORTAL_METADATA_ZIP_FILE_PATH, "w") as zip_file:
-            zip_file.writestr(
-                readme_file.OUTPUT_NAME,
-                readme_file.get_file_contents(
-                    download_config,
-                    projects,
-                ),
-            )
-        computed_file = cls(
-            portal_metadata_only=True,
-            s3_bucket=settings.AWS_S3_OUTPUT_BUCKET_NAME,
-            s3_key=common.PORTAL_METADATA_COMPUTED_FILE_NAME,
-            size_in_bytes=common.OUTPUT_PORTAL_METADATA_ZIP_FILE_PATH.stat().st_size,
-        )
-
-        return computed_file
-
-    @classmethod
     def get_project_file(cls, project, download_config: Dict, computed_file_name: str) -> Self:
         """
         Queries for a project's libraries according to the given download options configuration,
