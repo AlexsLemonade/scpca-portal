3ff3441219cc2a473997c645a8beb40c26966ed2
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-09-19 13:12:00 -0400

    add batch/computed_environment.tf

---
+++ infrastructure/batch/compute_environment.tf
@@ -0,0 +1,22 @@
+resource "aws_batch_compute_environment" "scpca_portal" {
+  compute_environment_name = "scpca-portal-compute-${var.user}-${var.stage}"
+
+  compute_resources {
+    max_vcpus = 32
+    security_group_ids = [
+        aws_security_group.scpca_portal_batch.id
+    ]
+
+    subnets = [
+        aws_subnet.scpca_portal_1a
+    ]
+
+    type = "FARGATE"
+  }
+
+  service_role = aws_iam_role.batch_service_role.arn
+  type = "MANAGED"
+  depends_on = [aws_iam_role_policy_attachment.batch_service_role]
+
+  tags = var.default_tags
+}
