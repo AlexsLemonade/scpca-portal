4f2868eb4ec8854a89e0cb8d93f3d53f6446cf04
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-09-23 14:36:49 -0400

    add useCopyToClipboard hook

---
+++ client/src/hooks/useCopyToClipboard.js
@@ -0,0 +1,30 @@
+// (resource) https://usehooks-ts.com/react-hook/use-copy-to-clipboard
+import { useState } from 'react'
+
+export const useCopyToClipboard = () => {
+  const [value, setValue] = useState(null)
+
+  const copyText = async (text) => {
+    if (!navigator?.clipboard) {
+      // eslint-disable-next-line no-console
+      console.warn('Clipboard not supported')
+
+      return false
+    }
+
+    try {
+      await navigator.clipboard.writeText(text)
+      setValue(text)
+
+      return true
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn('Copy failed', error)
+      setValue(null)
+
+      return false
+    }
+  }
+
+  return [value, copyText]
+}
