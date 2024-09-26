695004a6c068a2bba9c494dcbe7a67eaffe29bf6
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-09-26 12:44:55 -0400

    update queue and compute_environment names

--- infrastructure/batch/job_queue.tf
+++ infrastructure/batch/job_queue.tf
@@ -1,4 +1,4 @@
-resource "aws_batch_job_queue" "scpca_portal_queue" {
+resource "aws_batch_job_queue" "scpca_portal_project" {
   name     = "scpca-portal-queue-${var.user}-${var.stage}"
   state    = "ENABLED"
   priority = 1
@@ -6,7 +6,7 @@ resource "aws_batch_job_queue" "scpca_portal_queue" {
   # compute_environments = [] array is deprecated
   compute_environment_order {
     order               = 1
-    compute_environment = aws_batch_compute_environments.scpca_portal.arn
+    compute_environment = aws_batch_compute_environments.scpca_portal_project.arn
   }
 
   tags = var.default_tags
