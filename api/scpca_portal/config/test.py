8628c3a38298416aca0c3314bd059f37dcb8c5e6
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-10-02 13:07:16 -0400

    remove unncessary reference to output bucket in test config file

--- api/scpca_portal/config/test.py
+++ api/scpca_portal/config/test.py
@@ -5,7 +5,6 @@ class Test(Local):
     # AWS S3
     # Note: Data must be resynced when test bucket is updated
     AWS_S3_INPUT_BUCKET_NAME = "scpca-portal-public-test-inputs/2024-09-10/"
-    AWS_S3_OUTPUT_BUCKET_NAME = "scpca-local-data"
 
     # Code Paths
     DATA_PATH = Local.CODE_PATH / "test_data"
