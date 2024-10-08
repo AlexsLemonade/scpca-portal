33c7b6bc794a05bb083eee145d42d3241bc5abf2
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-10-08 15:17:10 -0400

    update computed file expected values

--- api/scpca_portal/test/expected_values/computed_file_sample.py
+++ api/scpca_portal/test/expected_values/computed_file_sample.py
@@ -30,7 +30,7 @@ class Computed_File_Sample:
             "metadata_only": False,
             "s3_bucket": settings.AWS_S3_OUTPUT_BUCKET_NAME,
             "s3_key": "SCPCS999990_SINGLE-CELL_SINGLE-CELL-EXPERIMENT.zip",
-            "size_in_bytes": 7089,
+            "size_in_bytes": 7057,
             "workflow_version": "development",
             "includes_celltype_report": True,
         }
@@ -60,7 +60,7 @@ class Computed_File_Sample:
             "metadata_only": False,
             "s3_bucket": settings.AWS_S3_OUTPUT_BUCKET_NAME,
             "s3_key": "SCPCS999990_SINGLE-CELL_ANN-DATA.zip",
-            "size_in_bytes": 7401,
+            "size_in_bytes": 7369,
             "workflow_version": "development",
             "includes_celltype_report": True,
         }
