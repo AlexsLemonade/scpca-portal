b2d3b705ca33742c9f0d863f09a0f8fa35b8b8bf
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-11-04 11:01:04 -0500

    keep job_queue::compute_environments for now (will update to job_queue::compute_environment_order when we update the aws provider to version 5.40.0)

--- infrastructure/batch/job_queue.tf
+++ infrastructure/batch/job_queue.tf
@@ -3,14 +3,9 @@ resource "aws_batch_job_queue" "scpca_portal_project" {
   state    = "ENABLED"
   priority = 1
 
-  # compute_environments = [] array is deprecated
   compute_environments = [
     aws_batch_compute_environment.scpca_portal_project.arn,
   ]
-  # compute_environment_order {
-  #   order               = 1
-  #   compute_environment = aws_batch_compute_environments.scpca_portal_project.arn
-  # }
 
   tags = var.default_tags
 }
