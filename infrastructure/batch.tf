fe5260c669c2f16709677d16188cc1cc4ad9cc9b
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-11-21 16:33:24 -0500

    comment out batch to go around new feature usages while changing aws provider version

--- infrastructure/batch.tf
+++ infrastructure/batch.tf
@@ -1,24 +1,24 @@
-module "batch" {
-  source = "./batch"
+# module "batch" {
+#   source = "./batch"
 
   # networking
-  scpca_portal_vpc = aws_vpc.scpca_portal_vpc
-  scpca_portal_subnet_1a = aws_subnet.scpca_portal_1a
+#  scpca_portal_vpc = aws_vpc.scpca_portal_vpc
+#  scpca_portal_subnet_1a = aws_subnet.scpca_portal_1a
 
   # job_definition envars
-  dockerhub_account = var.dockerhub_account
-  django_secret_key = var.django_secret_key
-  database_password = var.database_password
-  region = var.region
-  scpca_portal_bucket = aws_s3_bucket.scpca_portal_bucket
-  sentry_dsn = var.sentry_dsn
-  postgres_db = aws_db_instance.postgres_db
+#  dockerhub_account = var.dockerhub_account
+#  django_secret_key = var.django_secret_key
+#  database_password = var.database_password
+#  region = var.region
+#  scpca_portal_bucket = aws_s3_bucket.scpca_portal_bucket
+#  sentry_dsn = var.sentry_dsn
+#  postgres_db = aws_db_instance.postgres_db
 
   # security
-  scpca_portal_db_security_group = aws_security_group.scpca_portal_db
+#  scpca_portal_db_security_group = aws_security_group.scpca_portal_db
 
   # general configuration
-  user = var.user
-  stage = var.stage
-  default_tags = var.default_tags
-}
+#  user = var.user
+#  stage = var.stage
+#  default_tags = var.default_tags
+#}
