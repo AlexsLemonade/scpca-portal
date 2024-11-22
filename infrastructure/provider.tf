3deb7083f305b60e87cd7bfffa1acd0c75e2eab1
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-11-22 13:42:53 -0500

    upgrade to aws provider version >4.x.x, add upgrade flag to init script

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "-/aws"
-      version = ">= 3.37.0, < 4.0.0"
+      version = ">= 4.9.0, < 5.0.0"
     }
   }
   required_version = "0.13.0"
