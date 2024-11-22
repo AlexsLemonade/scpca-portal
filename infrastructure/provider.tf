0508ad7cb7290d9a1c5ab9ca1d87c5b1d2b45e66
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2024-11-22 13:58:55 -0500

    upgrade aws provider to version 5.x

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "-/aws"
-      version = ">= 4.9.0, < 5.0.0"
+      version = ">= 5.0.0, < 6.0.0"
     }
   }
   required_version = "0.13.0"
