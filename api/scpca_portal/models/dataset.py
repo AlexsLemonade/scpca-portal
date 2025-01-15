6f89ee0014b27ee96be89bdfd0e595c7dd9a2c5f
Nozomi Ichihara <nozomione.online@gmail.com>
2025-01-15 17:55:26 -0500

    (edit) apply team PR feedback

--- api/scpca_portal/models/dataset.py
+++ api/scpca_portal/models/dataset.py
@@ -22,51 +22,46 @@ class Dataset(TimestampedModel):
         )
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # User-editable
     data = models.JSONField(default=dict)
     format = models.TextField(choices=FileFormats.CHOICES, null=True)
-    email = models.EmailField(max_length=254, null=True)
-    regenerated_from = models.ForeignKey(
-        "self",
-        null=True,
-        on_delete=models.CASCADE,
-        related_name="original_dataset",
-        help_text="Reference to the original dataset if regenerated.",
-    )
-    start = models.BooleanField(
-        null=True,
-        help_text="Indicates if the dataset process has started.",
-    )
+    email = models.EmailField(null=True)
+    start = models.BooleanField(null=True)
 
-    token = models.OneToOneField(
-        APIToken,
-        null=True,
-        on_delete=models.CASCADE,
-        related_name="dataset_token",
-        help_text="Token used to process the dataset.",
-    )
-    download_tokens = models.ManyToManyField(
-        APIToken,
-        related_name="dataset_download_tokens",
-        help_text="Tokens used to create download.",
-    )
-
+    # Non user-editable
     started_at = models.DateTimeField(null=True)
     is_started = models.BooleanField(default=False)
     is_processing = models.BooleanField(default=False)
     processed_at = models.DateTimeField(null=True)
     is_processed = models.BooleanField(default=False)
-    expires_at = models.DateTimeField(null=True)
-    is_expired = models.BooleanField(default=False)
     errored_at = models.DateTimeField(null=True)
     is_errored = models.BooleanField(default=False)
     error_message = models.TextField(null=True)
+    expires_at = models.DateTimeField(null=True)
+    is_expired = models.BooleanField(default=False)  # Set by clonejob
 
     computed_file = models.OneToOneField(
         ComputedFile,
         null=True,
-        on_delete=models.CASCADE,
-        related_name="dataset_computed_file",
-        help_text="A computed file generated for the dataset.",
+        on_delete=models.SET_NULL,
+        related_name="datasets",
+    )
+    regenerated_from = models.ForeignKey(
+        "self",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="regenerated_datasets",
+    )
+    token = models.ForeignKey(
+        APIToken,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="dataset_token",
+    )
+    download_tokens = models.ManyToManyField(
+        APIToken,
+        related_name="downloaded_datasets",
     )
 
     def __str__(self):
