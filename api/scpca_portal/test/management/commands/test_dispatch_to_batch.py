4a6c2803f799a2b0369fbfa8ea119efcbe89264a
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-01-16 16:41:32 -0500

    add multiple projects per test to safeguard about queryset immutability without proper assignment bug

--- api/scpca_portal/test/management/commands/test_dispatch_to_batch.py
+++ api/scpca_portal/test/management/commands/test_dispatch_to_batch.py
@@ -13,24 +13,33 @@ class TestDispatchToBatch(TestCase):
 
     @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
     def test_generate_all_missing_files(self, mock_submit_job):
-        project_with_files = ProjectFactory()
-        self.assertTrue(project_with_files.computed_files.exists())
+        projects_with_files = [ProjectFactory() for _ in range(3)]
+        for project_with_files in projects_with_files:
+            self.assertTrue(project_with_files.computed_files.exists())
 
-        project_no_files = ProjectFactory(computed_file=None)
-        self.assertFalse(project_no_files.computed_files.exists())
+        projects_no_files = [ProjectFactory(computed_file=None) for _ in range(2)]
+        for project_no_files in projects_no_files:
+            self.assertFalse(project_no_files.computed_files.exists())
 
         self.dispatch_to_batch()
         mock_submit_job.assert_called()
 
         submitted_project_ids = set(
-            call.kwargs.get("project_id") for call in mock_submit_job.call_args_list
+            call.kwargs.get("project_id")
+            for call in mock_submit_job.call_args_list
+            if call.kwargs.get("project_id") is not None
         )
-        self.assertIn(project_no_files.scpca_id, submitted_project_ids)
-        self.assertNotIn(project_with_files.scpca_id, submitted_project_ids)
+        self.assertEqual(len(projects_no_files), len(submitted_project_ids))
+        for project_no_files in projects_no_files:
+            self.assertIn(project_no_files.scpca_id, submitted_project_ids)
+        for project_with_files in projects_with_files:
+            self.assertNotIn(project_with_files.scpca_id, submitted_project_ids)
 
-        sample_no_files = project_no_files.samples.first()
+        sample_no_files = projects_no_files[0].samples.first()
         submitted_sample_ids = set(
-            call.kwargs.get("sample_id") for call in mock_submit_job.call_args_list
+            call.kwargs.get("sample_id")
+            for call in mock_submit_job.call_args_list
+            if call.kwargs.get("sample_id") is not None
         )
         self.assertIn(sample_no_files.scpca_id, submitted_sample_ids)
 
@@ -38,45 +47,59 @@ class TestDispatchToBatch(TestCase):
     def test_generate_missing_files_for_passed_project(self, mock_submit_job):
         project = ProjectFactory(computed_file=None)
         self.assertFalse(project.computed_files.exists())
+        adtl_projects = [ProjectFactory(computed_file=None) for _ in range(3)]
 
         self.dispatch_to_batch(project_id=project.scpca_id)
         mock_submit_job.assert_called()
 
-        submitted_project_id = next(
-            call.kwargs["project_id"]
+        submitted_project_ids = set(
+            call.kwargs.get("project_id")
             for call in mock_submit_job.call_args_list
-            if "project_id" in call.kwargs
+            if call.kwargs.get("project_id") is not None
         )
-        self.assertEqual(project.scpca_id, submitted_project_id)
+        self.assertEqual(len(submitted_project_ids), 1)
+        self.assertIn(project.scpca_id, submitted_project_ids)
+        for adtl_project in adtl_projects:
+            self.assertNotIn(adtl_project.scpca_id, submitted_project_ids)
 
         sample = project.samples.first()
-        submitted_sample_id = next(
-            call.kwargs["sample_id"]
+        submitted_sample_ids = set(
+            call.kwargs.get("sample_id")
             for call in mock_submit_job.call_args_list
-            if "sample_id" in call.kwargs
+            if call.kwargs.get("sample_id") is not None
         )
-        self.assertEqual(sample.scpca_id, submitted_sample_id)
+        self.assertEqual(len(submitted_sample_ids), 1)
+        self.assertIn(sample.scpca_id, submitted_sample_ids)
 
     @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
     def test_regenerate_all_files(self, mock_submit_job):
-        project_with_files = ProjectFactory()
-        self.assertTrue(project_with_files.computed_files.exists())
+        projects_with_files = [ProjectFactory() for _ in range(3)]
+        for project_with_files in projects_with_files:
+            self.assertTrue(project_with_files.computed_files.exists())
 
-        project_no_files = ProjectFactory(computed_file=None)
-        self.assertFalse(project_no_files.computed_files.exists())
+        projects_no_files = [ProjectFactory(computed_file=None) for _ in range(2)]
+        for project_no_files in projects_no_files:
+            self.assertFalse(project_no_files.computed_files.exists())
+
+        projects = projects_with_files + projects_no_files
 
         self.dispatch_to_batch(regenerate_all=True)
         mock_submit_job.assert_called()
 
         submitted_project_ids = set(
-            call.kwargs.get("project_id") for call in mock_submit_job.call_args_list
+            call.kwargs.get("project_id")
+            for call in mock_submit_job.call_args_list
+            if call.kwargs.get("project_id") is not None
         )
-        self.assertIn(project_no_files.scpca_id, submitted_project_ids)
-        self.assertIn(project_with_files.scpca_id, submitted_project_ids)
+        self.assertEqual(len(projects), len(submitted_project_ids))
+        for project in projects:
+            self.assertIn(project.scpca_id, submitted_project_ids)
 
-        sample_no_files = project_no_files.samples.first()
+        sample_no_files = projects[0].samples.first()
         submitted_sample_ids = set(
-            call.kwargs.get("sample_id") for call in mock_submit_job.call_args_list
+            call.kwargs.get("sample_id")
+            for call in mock_submit_job.call_args_list
+            if call.kwargs.get("sample_id") is not None
         )
         self.assertIn(sample_no_files.scpca_id, submitted_sample_ids)
 
@@ -84,6 +107,9 @@ class TestDispatchToBatch(TestCase):
     def test_regenerate_files_for_passed_project(self, mock_submit_job):
         project = ProjectFactory()
         self.assertTrue(project.computed_files.exists())
+        adtl_projects = [ProjectFactory() for _ in range(3)]
+        for adtl_project in adtl_projects:
+            self.assertTrue(adtl_project.computed_files.exists())
 
         self.dispatch_to_batch(project_id=project.scpca_id, regenerate_all=True)
         mock_submit_job.assert_called()
@@ -95,6 +121,8 @@ class TestDispatchToBatch(TestCase):
         )
         self.assertEqual(len(submitted_project_ids), 1)
         self.assertIn(project.scpca_id, submitted_project_ids)
+        for adtl_project in adtl_projects:
+            self.assertNotIn(adtl_project.scpca_id, submitted_project_ids)
 
         sample = project.samples.first()
         submitted_sample_ids = set(
