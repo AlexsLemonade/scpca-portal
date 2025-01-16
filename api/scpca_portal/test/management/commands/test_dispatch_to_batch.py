b55d51477a0f3f5696b0c75106da4d507d3212f0
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-01-16 10:31:27 -0500

    update test methods to account for addition of --regenate-all flag

--- api/scpca_portal/test/management/commands/test_dispatch_to_batch.py
+++ api/scpca_portal/test/management/commands/test_dispatch_to_batch.py
@@ -12,11 +12,9 @@ class TestDispatchToBatch(TestCase):
         self.dispatch_to_batch = partial(call_command, "dispatch_to_batch")
 
     @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
-    def test_only_projects_without_computed_files_submitted(self, mock_submit_job):
+    def test_generate_all_missing_files(self, mock_submit_job):
         project_with_files = ProjectFactory()
-
-        project_no_files = ProjectFactory()
-        project_no_files.computed_files.delete()
+        project_no_files = ProjectFactory(computed_file=None)
 
         self.dispatch_to_batch()
         mock_submit_job.assert_called()
@@ -34,14 +32,49 @@ class TestDispatchToBatch(TestCase):
         self.assertIn(sample_no_files.scpca_id, submitted_sample_ids)
 
     @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
-    def test_all_projects_have_computed_files(self, mock_submit_job):
-        ProjectFactory()
+    def test_generate_missing_files_for_passed_project(self, mock_submit_job):
+        project = ProjectFactory(computed_file=None)
 
-        self.dispatch_to_batch()
-        mock_submit_job.assert_not_called()
+        self.dispatch_to_batch(project_id=project.scpca_id)
+        mock_submit_job.assert_called()
+
+        submitted_project_id = next(
+            call.kwargs["project_id"]
+            for call in mock_submit_job.call_args_list
+            if "project_id" in call.kwargs
+        )
+        self.assertEqual(project.scpca_id, submitted_project_id)
+
+        sample = project.samples.first()
+        submitted_sample_id = next(
+            call.kwargs["sample_id"]
+            for call in mock_submit_job.call_args_list
+            if "sample_id" in call.kwargs
+        )
+        self.assertEqual(sample.scpca_id, submitted_sample_id)
 
     @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
-    def test_project_id_submission(self, mock_submit_job):
+    def test_regenerate_all_files(self, mock_submit_job):
+        project_with_files = ProjectFactory()
+        project_no_files = ProjectFactory(computed_file=None)
+
+        self.dispatch_to_batch(regenerate_all=True)
+        mock_submit_job.assert_called()
+
+        submitted_project_ids = set(
+            call.kwargs.get("project_id") for call in mock_submit_job.call_args_list
+        )
+        self.assertIn(project_no_files.scpca_id, submitted_project_ids)
+        self.assertIn(project_with_files.scpca_id, submitted_project_ids)
+
+        sample_no_files = project_no_files.samples.first()
+        submitted_sample_ids = set(
+            call.kwargs.get("sample_id") for call in mock_submit_job.call_args_list
+        )
+        self.assertIn(sample_no_files.scpca_id, submitted_sample_ids)
+
+    @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
+    def test_regenerate_files_for_passed_project(self, mock_submit_job):
         project = ProjectFactory()
 
         self.dispatch_to_batch(project_id=project.scpca_id, regenerate_all=True)
@@ -61,6 +94,13 @@ class TestDispatchToBatch(TestCase):
             if "sample_id" in call.kwargs
         )
         self.assertEqual(sample.scpca_id, submitted_sample_id)
+
+    @patch("scpca_portal.management.commands.dispatch_to_batch.Command.submit_job")
+    def test_no_missing_computed_files(self, mock_submit_job):
+        ProjectFactory()
+
+        self.dispatch_to_batch()
+        mock_submit_job.assert_not_called()
 
     def test_project_missing_samples(self):
         """
