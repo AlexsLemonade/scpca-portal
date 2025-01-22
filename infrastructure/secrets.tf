b6923269d83c84e1b582fd2053f6d4289046693b
David <davidsmejia@gmail.com>
2025-01-22 11:39:29 -0500

    add batch secrets

---
+++ infrastructure/secrets.tf
@@ -0,0 +1,25 @@
+resource "aws_secretsmanager_secret" "django_secret_key" {
+  name_prefix = "scpca-portal-${var.user}-${var.stage}-django-secret-key"
+}
+
+resource "aws_secretsmanager_secret_version" "django_secret_key" {
+  secret_id = aws_secretsmanager_secret.django_secret_key.id
+  secret_string = var.django_secret_key
+}
+
+resource "aws_secretsmanager_secret" "database_password" {
+  name_prefix = "scpca-portal-${var.user}-${var.stage}-database-password"
+}
+
+resource "aws_secretsmanager_secret_version" "database_password" {
+  secret_id = aws_secretsmanager_secret.database_password.id
+  secret_string = var.database_password
+}
+
+resource "aws_secretsmanager_secret" "sentry_dsn" {
+  name_prefix = "scpca-portal-${var.user}-${var.stage}-raven-dsn"
+}
+
+resource "aws_secretsmanager_secret_version" "sentry_dsn" {
+  secret_id = aws_secretsmanager_secret.sentry_dsn.id
+  secret_string = var.sentry_dsn
