90d8ea6466b1d747a0205cf0c236892c303376aa
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-01-31 11:11:20 -0500

    add send_test_email and dispatch_send_test_email management commands

---
+++ api/scpca_portal/management/commands/dispatch_send_test_email.py
@@ -0,0 +1,50 @@
+import logging
+from datetime import datetime
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+import boto3
+
+batch = boto3.client(
+    "batch",
+    region_name=settings.AWS_REGION,
+)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+class Command(BaseCommand):
+    help = """
+    Dispatch send-test-email to Batch.
+    """
+
+    def add_arguments(self, parser):
+        parser.add_arguments("--sender", type=str)
+        parser.add_arguments("--recipient", type=str)
+
+    def handle(self, *args, **kwargs):
+        self.dispatch_send_email(**kwargs)
+
+    def dispatch_send_email(self, sender: str, recipient: str, **kwargs):
+        sender_flag = f"--sender {sender}" if sender else ""
+        recipient_flag = f"--recipient {recipient}" if recipient else ""
+        job_name = f"Test email from {sender} - {str(datetime.now())}"
+
+        response = batch.submit_job(
+            jobName=job_name,
+            jobQueue=settings.AWS_BATCH_JOB_QUEUE_NAME,
+            jobDefinition=settings.AWS_BATCH_JOB_DEFINITION_NAME,
+            containerOverrides={
+                "command": [
+                    "python",
+                    "manage.py",
+                    "send_test_email",
+                    sender_flag,
+                    recipient_flag,
+                ],
+            },
+        )
+
+        logger.info(f'{job_name} submitted to Batch with jobId {response["jobId"]}')
