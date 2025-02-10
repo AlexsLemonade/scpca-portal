a59f0fe01bc93f1aef3abe415b3ae8f14ee085d5
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-02-10 12:28:57 -0500

    thin out email template, convert html updating to django templating language, update body text

--- api/scpca_portal/notifications.py
+++ api/scpca_portal/notifications.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-
 from django.conf import settings
+from django.template.loader import render_to_string
 
 import boto3
 
@@ -9,19 +8,22 @@ from scpca_portal.config.logging import get_and_configure_logger
 logger = get_and_configure_logger(__name__)
 ses = boto3.client("ses", region_name=settings.AWS_REGION)
 
+TEMPLATE_ROOT = settings.TEMPLATE_PATH / "email_templates"
+TEMPLATE_FILE_PATH = TEMPLATE_ROOT / "default.html"
 
-def send_job_completed_email(resource_id: str, download_config_name: str) -> None:
+
+def send_project_files_completed_email(project_id: str) -> None:
     SENDER = ""
     RECIPIENT = ""
     CHARSET = "UTF-8"
 
-    SUBJECT = f"Job Completed: `{resource_id} - {download_config_name}`"
+    SUBJECT = f"All files generated for {project_id}"
     BODY_TEXT = (
         "Hot off the presses:\n\n"
-        + f"Job is completed for {resource_id} - {download_config_name}"
+        f"All files have been generated for project {project_id}"
         + "\n\nLove!,\nThe ScPCA Portal Team"
     )
-    BODY_HTML = Path("email_templates/default.html").read_text().replace("\n", "")
+    BODY_HTML = render_to_string(TEMPLATE_FILE_PATH, context={"project_id": project_id})
 
     ses.send_email(
         Source=SENDER,
