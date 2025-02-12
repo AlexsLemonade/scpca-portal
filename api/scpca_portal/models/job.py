7c61b49b6e67b2e434a6f8772ed422ccb4c5627d
Nozomi Ichihara <nozomione.online@gmail.com>
2025-02-12 12:30:37 -0500

    (minor) reorder fields and minor comment revision

--- api/scpca_portal/models/job.py
+++ api/scpca_portal/models/job.py
@@ -13,18 +13,19 @@ class Job(TimestampedModel):
 
     # Internal Attributes
     attempt = models.PositiveIntegerField(default=1)  # Incremented on every retry
+    critical_error = models.BooleanField(default=False)  # Set to True if the job is irrecoverable
+    failure_reason = models.TextField(blank=True, null=True)
+    retry_on_termination = models.BooleanField(default=False)
     state = models.TextField(choices=JobState.CHOICES, default=JobState.CREATED)
+
     submitted_at = models.DateTimeField(null=True)
     completed_at = models.DateTimeField(null=True)
     terminated_at = models.DateTimeField(null=True)
-    failure_reason = models.TextField(blank=True, null=True)
-    critical_error = models.BooleanField(default=False)
-    retry_on_termination = models.BooleanField(default=False)
 
     # Job Information Sent to AWS (via Request)
     batch_job_name = models.TextField(null=True)
-    batch_job_queue = models.TextField(null=True)
     batch_job_definition = models.TextField(null=True)
+    batch_job_queue = models.TextField(null=True)
     batch_container_overrides = models.JSONField(default=dict)
 
     # Job Information Defined from AWS (via Response)
@@ -51,6 +52,6 @@ class Job(TimestampedModel):
         """
         Create and return a new Job instance to retry a terminated job via boto3,
         Updates attempt and state for the new job instance.
-        Called only when critical_error is false and retry_on_termination is true.
+        Called only when critical_error is False and retry_on_termination is True.
         """
         pass
