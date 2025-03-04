7a92c67b438820e009c699661c7c83e9bcb6877a
Nozomi Ichihara <nozomione.online@gmail.com>
2025-03-04 13:59:42 -0500

    (revise 1) remove data/projects mock file and fetch API data wihtin the DatasetAddProjectModal (previously DatasetAddToModal) story and pass the fetched data to this comonent with a new prop 'project'

---
+++ client/.storybook/stories/DatasetAddProjectModal.stories.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react'
+import 'regenerator-runtime/runtime'
+import {api} from '../../src/api'
+import { Box, Heading } from 'grommet'
+import { DatasetOptionsContextProvider } from 'contexts/DatasetOptionsContext'
+import { DatasetAddProjectModal } from 'components/DatasetAddProjectModal'
+
+const projectIds = {
+    SCPCP000001: 'Single-cell with Bulk RNA-seq',
+    SCPCP000004: 'Single-cell',
+    SCPCP000006: 'Spatial',
+    SCPCP000009: 'Single-cell with multiplexed samples',
+}
+
+export default {
+  title: 'Components/DatasetAddProjectModal'
+}
+
+export const Default = (args) => {
+    const [projects, setProjects] = useState([])
+
+    useEffect(()=> {
+       const fetchProjects = async () => {
+            const projectResponses = await Promise.all(
+                Object.keys(projectIds)
+                    .map((id) => api.projects.get(id))
+            )
+
+            setProjects(projectResponses.map(p => p.response))
+        }
+
+        fetchProjects()
+    }, [])
+
+    return (
+    <>
+     {projects.map((project)=> (
+        <DatasetOptionsContextProvider key={project.scpca_id} resource={project}>
+            <Heading level={4} margin={{ top: 'medium', bottom: '0' }}>
+                {project.scpca_id}
+            </Heading>
+            <Box margin={{ bottom: 'small' }}>
+                ({projectIds[project.scpca_id]})
+            </Box>
+            <Box width='160px'>
+                <DatasetAddProjectModal project={project} />
+            </Box>
+        </DatasetOptionsContextProvider>
+     ))}
+    </>
+)}
