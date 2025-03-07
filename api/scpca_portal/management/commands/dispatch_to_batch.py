1d0886715ec34e0d89411833b4fa0d6b32fbe4ac
Nozomi Ichihara <nozomione.online@gmail.com>
2025-03-07 15:13:40 -0500

    remove dispatch_to_batch.submit_job and flatten notify flag logic

--- api/scpca_portal/management/commands/dispatch_to_batch.py
+++ api/scpca_portal/management/commands/dispatch_to_batch.py
@@ -38,42 +38,6 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.dispatch_to_batch(**kwargs)
 
-    # def submit_job(
-    #     self,
-    #     *,
-    #     download_config_name: str,
-    #     project_id: str = "",
-    #     sample_id: str = "",
-    #     notify: bool = False,
-    # ) -> None:
-    #     """
-    #     Submit job to AWS Batch, accordingly to the resource_id and download_config combination.
-    #     """
-    #     resource_flag = "--project-id" if project_id else "--sample-id"
-    #     resource_id = project_id if project_id else sample_id
-    #     job_name = f"{resource_id}-{download_config_name}"
-    #     notify_flag = "--notify" if notify else ""
-
-    #     response = batch.submit_job(
-    #         jobName=job_name,
-    #         jobQueue=settings.AWS_BATCH_JOB_QUEUE_NAME,
-    #         jobDefinition=settings.AWS_BATCH_JOB_DEFINITION_NAME,
-    #         containerOverrides={
-    #             "command": [
-    #                 "python",
-    #                 "manage.py",
-    #                 "generate_computed_file",
-    #                 resource_flag,
-    #                 resource_id,
-    #                 "--download-config-name",
-    #                 download_config_name,
-    #                 notify_flag,
-    #             ],
-    #         },
-    #     )
-
-    #     logger.info(f'{job_name} submitted to Batch with jobId {response["jobId"]}')
-
     def dispatch_to_batch(self, project_id: str, regenerate_all: bool, notify: bool, **kwargs):
         """
         Iterate over all projects that fit the criteria of the passed flags
@@ -93,17 +57,14 @@ class Command(BaseCommand):
         for project in project_list:
             project_valid_download_config_names = project.valid_download_config_names
             for download_config_name in project_valid_download_config_names:
-                may_notify = False
-                if (
+                last_project = (
                     project == project_list[-1]
                     and download_config_name == project_valid_download_config_names[-1]
-                ):
-                    may_notify = notify
-
+                )
                 job = Job.get_project_job(
                     project_id=project.scpca_id,
                     download_config_name=download_config_name,
-                    notify=may_notify,
+                    notify=last_project and notify,
                 )
 
                 job.submit()
