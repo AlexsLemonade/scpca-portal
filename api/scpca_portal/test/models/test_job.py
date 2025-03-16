1fbf8c0593ffd0fb5cdc47f34a98dd437312c075
Nozomi Ichihara <nozomione.online@gmail.com>
2025-03-16 16:27:16 -0400

    add tests for Job::terminate using JobFactory, remove no longer used attributes from TestJob::setUp, and make small adjustments to test_submit_job

--- api/scpca_portal/test/models/test_job.py
+++ api/scpca_portal/test/models/test_job.py
@@ -4,64 +4,102 @@ from unittest.mock import patch
 from django.conf import settings
 from django.test import TestCase
 
+from botocore.exceptions import ClientError
+
 from scpca_portal.enums import JobStates
 from scpca_portal.models import Job
+from scpca_portal.test.factories import JobFactory
 
 
 class TestJob(TestCase):
     def setUp(self):
-        self.mock_job_id = "FAKE_JOB_ID"
-        self.project_id = "FAKE_PROJECT_ID"
-        self.download_config_name = "FAKE_DOWNLOAD_CONFIG"
-        self.batch_job_name = f"{self.project_id}-{self.download_config_name}"
-        self.project_job_batch_container_overrides = {
-            "command": [
-                "python",
-                "manage.py",
-                "generate_computed_file",
-                "--project-id",
-                self.project_id,
-                "--download-config-name",
-                self.download_config_name,
-                "",
-            ]
-        }
+        self.mock_project_id = "SCPCP000000"
+        self.mock_sample_id = "SCPCS000000"
+        self.mock_batch_job_id = "MOCK_JOB_ID"  # The job id via AWS Batch response
 
     @patch("scpca_portal.models.Job._batch")
     def test_submit_job(self, mock_batch_client):
-        # Set the mock return value of submit_job
-        mock_batch_client.submit_job.return_value = {"jobId": self.mock_job_id}
+        # Set up mock for submit_job
+        mock_batch_client.submit_job.return_value = {"jobId": self.mock_batch_job_id}
 
         job = Job.get_project_job(
-            project_id=self.project_id, download_config_name=self.download_config_name
+            project_id=self.mock_project_id, download_config_name="MOCK_DOWNLOAD_CONFIG"
         )
 
         # Before submission, the job instance should not have an ID
         self.assertIsNone(job.id)
 
         job.submit()
-
-        mock_batch_client.submit_job.assert_called_once_with(
-            jobName=self.batch_job_name,
-            jobQueue=settings.AWS_BATCH_JOB_QUEUE_NAME,
-            jobDefinition=settings.AWS_BATCH_JOB_DEFINITION_NAME,
-            containerOverrides=self.project_job_batch_container_overrides,
-        )
+        mock_batch_client.submit_job.assert_called_once()
 
         # After submission, the job should be updated
-        self.assertEqual(job.batch_job_id, self.mock_job_id)
+        self.assertEqual(job.batch_job_id, self.mock_batch_job_id)
         self.assertEqual(job.state, JobStates.SUBMITTED)
         self.assertIsInstance(job.submitted_at, datetime)
 
         # Make sure that the job is saved in the db with correct field values
         self.assertEqual(Job.objects.count(), 1)
         saved_job = Job.objects.first()
-        self.assertEqual(saved_job.batch_job_name, self.batch_job_name)
+        self.assertEqual(saved_job.batch_job_id, self.mock_batch_job_id)
         self.assertEqual(saved_job.batch_job_queue, settings.AWS_BATCH_JOB_QUEUE_NAME)
         self.assertEqual(saved_job.batch_job_definition, settings.AWS_BATCH_JOB_DEFINITION_NAME)
-        self.assertEqual(
-            saved_job.batch_container_overrides, self.project_job_batch_container_overrides
-        )
-        self.assertIn(self.project_id, saved_job.batch_container_overrides["command"])
+        self.assertIn("--project-id", saved_job.batch_container_overrides["command"])
+        self.assertIn(self.mock_project_id, saved_job.batch_container_overrides["command"])
         self.assertEqual(saved_job.state, JobStates.SUBMITTED)
         self.assertIsInstance(saved_job.submitted_at, datetime)
+
+    @patch("scpca_portal.models.Job._batch")
+    def test_terminate_job(self, mock_batch_client):
+        # Set up mock for terminate_job
+        mock_batch_client.terminate_job.return_value = True
+        job = JobFactory.create(
+            batch_job_id=self.mock_batch_job_id,
+            state=JobStates.SUBMITTED,
+            project_id=self.mock_project_id,  # Test-only
+        )
+
+        response = job.terminate(retry_on_termination=True)
+        mock_batch_client.terminate_job.assert_called_once()
+        self.assertTrue(response)
+
+        # After termination, the job should be saved with correct field values
+        saved_job = Job.objects.first()
+        self.assertEqual(saved_job.state, JobStates.TERMINATED)
+        self.assertTrue(saved_job.retry_on_termination)
+        self.assertIsInstance(saved_job.terminated_at, datetime)
+
+    @patch("scpca_portal.models.Job._batch")
+    def test_terminate_job_failure(self, mock_batch_client):
+        job = JobFactory.create(
+            batch_job_id=self.mock_batch_job_id,
+            state=JobStates.SUBMITTED,
+            project_id=self.mock_project_id,  # Test-only
+        )
+
+        # Set up mock for ClientException
+        mock_batch_client.terminate_job.side_effect = ClientError(
+            {"Error": {"Code": "400", "Message": "ClientException"}}, "MockTerminatingJob"
+        )
+
+        response = job.terminate(retry_on_termination=True)
+        mock_batch_client.terminate_job.assert_called()
+        self.assertFalse(response)
+
+        # The job should not be updated in the db
+        saved_job = Job.objects.first()
+        self.assertEqual(saved_job.state, JobStates.SUBMITTED)
+        self.assertFalse(saved_job.critical_error)  # Still recoverable
+        self.assertFalse(saved_job.retry_on_termination)
+
+        # Set up mock for ServerException
+        mock_batch_client.terminate_job.side_effect = Exception("ServerException")
+
+        response = job.terminate(retry_on_termination=True)
+        mock_batch_client.terminate_job.assert_called()
+        self.assertFalse(response)
+
+        # The job should be saved with correct field values
+        saved_job = Job.objects.first()
+        self.assertEqual(saved_job.state, JobStates.TERMINATED)
+        self.assertTrue(saved_job.critical_error)  # No longer recoverable
+        self.assertFalse(saved_job.retry_on_termination)
