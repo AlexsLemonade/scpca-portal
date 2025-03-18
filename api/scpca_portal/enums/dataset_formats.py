f0b19ac28d068da1380627434ce65549f3a75624
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-03-18 14:08:20 -0400

    add metadata option to dataset_formats enums

--- api/scpca_portal/enums/dataset_formats.py
+++ api/scpca_portal/enums/dataset_formats.py
@@ -4,3 +4,4 @@ from django.db.models import TextChoices
 class DatasetFormats(TextChoices):
     ANN_DATA = "ANN_DATA"
     SINGLE_CELL_EXPERIMENT = "SINGLE_CELL_EXPERIMENT"
+    METADATA = "METADATA"
