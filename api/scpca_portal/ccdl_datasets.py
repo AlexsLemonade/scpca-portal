eef739d80fdf7786f106e357956fc4f413f551c5
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-03-19 14:51:43 -0400

    remove metadata_only ccdl_dataset type attr, replace magic strings with DatasetFormats and Modalities enums

--- api/scpca_portal/ccdl_datasets.py
+++ api/scpca_portal/ccdl_datasets.py
@@ -1,65 +1,63 @@
+from scpca_portal.enums import DatasetFormats, Modalities
+
 TYPES = {
     "ALL_METADATA": {
         "modality": None,
-        "format": None,
+        "format": DatasetFormats.METADATA,
         "excludes_multiplexed": False,
         "includes_merged": False,
-        "metadata_only": True,
     },
     "SINGLE_CELL_SINGLE_CELL_EXPERIMENT": {
-        "modality": "SINGLE_CELL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
         "excludes_multiplexed": True,
         "includes_merged": False,
-        "metadata_only": False,
     },
     "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MULTIPLEXED": {
-        "modality": "SINGLE_CELL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
         "excludes_multiplexed": False,
         "includes_merged": False,
-        "metadata_only": False,
     },
     # Notes about merged objects (accoring to scpca portal documentation):
     #   Only Single-cell (not spatial) for sce and anndata
     #   Only projects with non-multiplexed libraries can be merged
     #   Merged objects are unavailable for projects with > 100 samples
     "SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MERGED": {
-        "modality": "SINGLE_CELL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
         "excludes_multiplexed": True,
         "includes_merged": True,
-        "metadata_only": False,
     },
     "SINGLE_CELL_ANN_DATA": {
-        "modality": "SINGLE_CELL",
-        "format": "ANN_DATA",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.ANN_DATA,
         "excludes_multiplexed": True,
         "includes_merged": False,
-        "metadata_only": False,
     },
     "SINGLE_CELL_ANN_DATA_MERGED": {
-        "modality": "SINGLE_CELL",
-        "format": "ANN_DATA",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.ANN_DATA,
         "excludes_multiplexed": True,
         "includes_merged": True,
-        "metadata_only": False,
     },
     "SPATIAL_SINGLE_CELL_EXPERIMENT": {
-        "modality": "SPATIAL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "modality": Modalities.SPATIAL,
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
         "excludes_multiplexed": True,
         "includes_merged": False,
-        "metadata_only": False,
     },
     "SAMPLE_SINGLE_CELL_SINGLE_CELL_EXPERIMENT": {
-        "modality": "SINGLE_CELL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
     },
-    "SAMPLE_SINGLE_CELL_ANN_DATA": {"modality": "SINGLE_CELL", "format": "ANN_DATA"},
+    "SAMPLE_SINGLE_CELL_ANN_DATA": {
+        "modality": Modalities.SINGLE_CELL,
+        "format": DatasetFormats.ANN_DATA,
+    },
     "SAMPLE_SPATIAL_SINGLE_CELL_EXPERIMENT": {
         "modality": "SPATIAL",
-        "format": "SINGLE_CELL_EXPERIMENT",
+        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT,
     },
 }
 
