f6fa55bfef1515bd650edb1a3ca4938a5200fb98
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-03-21 17:03:05 -0400

    rename Notification story to Notifications, and clean up

---
+++ client/.storybook/stories/Notifications.stories.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { useNotification } from 'hooks/useNotification'
+import { Box } from 'grommet'
+import { Button } from 'components/Button'
+import { Notifications } from 'components/Notifications'
+
+export default {
+    title: 'Components/Notifications'
+}
+
+export const Default = () => <Notifications />
