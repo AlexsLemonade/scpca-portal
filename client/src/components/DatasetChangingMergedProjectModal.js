c8718e4f4b0a402fc891d4866405aa03c71e9962
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-04-08 13:11:58 -0400

    add first pass at DatasetChangingMergedProjectModal

---
+++ client/src/components/DatasetChangingMergedProjectModal.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { Box, Text } from 'grommet'
+import { config } from 'config'
+import { useResponsive } from 'hooks/useResponsive'
+import { Button } from 'components/Button'
+import { Modal, ModalBody } from 'components/Modal'
+import { InfoText } from 'components/InfoText'
+import { Link } from 'components/Link'
+
+export const DatasetChangingMergedProjectModal = ({
+  label = 'Save Changes',
+  title = 'Changing a Merged Project',
+  disabled = false
+}) => {
+  const [showing, setShowing] = useState(false)
+  const { responsive } = useResponsive()
+
+  const handleClick = () => {
+    setShowing(true)
+  }
+
+  return (
+    <>
+      <Button
+        aria-label={label}
+        flex="grow"
+        primary
+        label={label}
+        disabled={disabled}
+        onClick={handleClick}
+      />
+      <Modal title={title} showing={showing} setShowing={setShowing}>
+        <ModalBody>
+          <Text margin={{ left: 'xsmall' }}>
+            You have selected to merge all single-cell samples into one object.
+            <br />
+            <br />
+            We cannot merge samples into one object if you remove single-cell
+            samples from the project.
+          </Text>
+          <Box margin={{ top: 'medium' }}>
+            <InfoText>
+              <Link
+                href={config.links.why_changing_merged_project}
+                label="Learn more"
+              />
+            </InfoText>
+          </Box>
+          <Box
+            align="center"
+            direction={responsive('column', 'row')}
+            gap="xlarge"
+            justify="end"
+            margin={{ top: 'large' }}
+          >
+            <Button aria-label="Cancel" label="Cancel" />
+            <Button primary aria-label="Continue" label="Continue" />
+          </Box>
+        </ModalBody>
+      </Modal>
+    </>
+  )
+}
