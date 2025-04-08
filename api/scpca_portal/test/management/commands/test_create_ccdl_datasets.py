b451be60cbefa137ec49e7058e52ca7061e690d8
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-04-08 11:20:01 -0400

    add Dataset and Job count assertions to TestCreateCCDLDatasets::test_correct_datasets_and_jobs_processed

--- api/scpca_portal/test/management/commands/test_create_ccdl_datasets.py
+++ api/scpca_portal/test/management/commands/test_create_ccdl_datasets.py
@@ -5,6 +5,7 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from scpca_portal import loader
+from scpca_portal.models import Dataset, Job
 
 
 class TestCreateCCDLDatasets(TestCase):
@@ -25,4 +26,8 @@ class TestCreateCCDLDatasets(TestCase):
     def test_correct_datasets_and_jobs_processed(self, mock_batch_submit_job):
         mock_batch_job_id = "MOCK_JOB_ID"
         mock_batch_submit_job.return_value = mock_batch_job_id
+
         call_command("create_ccdl_datasets")
+
+        self.assertEqual(Dataset.objects.count(), 21)
+        self.assertEqual(Job.objects.count(), 21)
