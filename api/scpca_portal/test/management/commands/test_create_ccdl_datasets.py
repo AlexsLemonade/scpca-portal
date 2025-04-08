b65552a58fcb45b58f98c754e72a8483911a95ca
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-04-08 09:53:00 -0400

    add test_create_ccdl_datasets file with test_correct_datasets_and_jobs_processed test

---
+++ api/scpca_portal/test/management/commands/test_create_ccdl_datasets.py
@@ -0,0 +1,28 @@
+from unittest.mock import patch
+
+from django.conf import settings
+from django.core.management import call_command
+from django.test import TestCase
+
+from scpca_portal import loader
+
+
+class TestCreateCCDLDatasets(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        call_command("sync_original_files", bucket=settings.AWS_S3_INPUT_BUCKET_NAME)
+
+        for project_metadata in loader.get_projects_metadata():
+            loader.create_project(
+                project_metadata,
+                submitter_whitelist={"scpca"},
+                input_bucket_name=settings.AWS_S3_INPUT_BUCKET_NAME,
+                reload_existing=True,
+                update_s3=False,
+            )
+
+    @patch("scpca_portal.batch.submit_job")
+    def test_correct_datasets_and_jobs_processed(self, mock_batch_submit_job):
+        mock_batch_job_id = "MOCK_JOB_ID"
+        mock_batch_submit_job.return_value = mock_batch_job_id
+        call_command("create_ccdl_datasets")
