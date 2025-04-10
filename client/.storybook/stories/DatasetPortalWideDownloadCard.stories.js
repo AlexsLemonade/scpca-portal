02683b5ab777f3e04a52b0dba62cc381d616475a
Nozomi Ichihara <nozomione.online@gmail.com>
2025-04-10 18:41:02 -0400

    add DatasetPortalWideDownloadCard and the corresponsing Story to Storybook

---
+++ client/.storybook/stories/DatasetPortalWideDownloadCard.stories.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Grid } from 'grommet'
+import { useResponsive } from 'hooks/useResponsive'
+import { DatasetPortalWideDownloadCard } from 'components/DatasetPortalWideDownloadCard'
+
+
+const datasets = [
+    {
+        id: 1,
+        format: 'SINGLE_CELL_EXPERIMENT',
+        modality: 'SINGLE_CELL',
+        has_bulk_rna_seq: true,
+        has_cite_seq_data: true,
+        includes_merged: true,
+        metadata_only: false,
+        size_in_bytes: 429496729600
+    },
+    {
+        id: 2,
+        format: 'ANN_DATA',
+        modality: 'SINGLE_CELL',
+        has_bulk_rna_seq: true,
+        has_cite_seq_data: true,
+        includes_merged: true,
+        metadata_only: false,
+        size_in_bytes: 966367641600
+    },
+    {
+        id: 3,
+        format: 'SINGLE_CELL_EXPERIMENT',
+        modality: 'SPATIAL',
+        has_bulk_rna_seq: true,
+        has_cite_seq_data: false,
+        includes_merged: false,
+        metadata_only: false,
+        size_in_bytes: 429496729600
+    }
+]
+
+const portalMetadataDataset = {
+    id: 1,
+    format: null,
+    modality: null,
+    has_bulk_rna_seq: false,
+    has_cite_seq_data: false,
+    includes_merged: false,
+    metadata_only: true,
+    size_in_bytes: 608270
+  }
+
+
+export default {
+  title: 'Components/DatasetPortalWideDownloadCard',
+  args: {datasets, portalMetadataDataset}
+}
+
+export const DataFormat = (args) => {
+    const { responsive } = useResponsive()
+    return (
+    <Grid columns={responsive( '100%' , '420px')} gap="xxlarge">
+        { datasets.map((d) =>
+            <DatasetPortalWideDownloadCard key={d.id} dataset={d} {...args} />
+        )}
+    </Grid>)
+}
+
+export const PortalMetadata = (args) => (
+    <DatasetPortalWideDownloadCard dataset={portalMetadataDataset} {...args}   />
+)
---
+++ client/src/components/DatasetPortalWideDownloadCard.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import { Box, CheckBox, Text } from 'grommet'
+import { useResponsive } from 'hooks/useResponsive'
+import { Button } from 'components/Button'
+import { CopyLinkButton } from 'components/CopyLinkButton'
+import { HelpLink } from 'components/HelpLink'
+import { Link } from 'components/Link'
+import { config } from 'config'
+import { formatBytes } from 'helpers/formatBytes'
+import { getReadable, getReadableFiles } from 'helpers/getReadable'
+
+const Li = ({ children }) => (
+  <Box as="li" style={{ display: 'list-item' }}>
+    {children}
+  </Box>
+)
+
+// NOTE: This component temporaily accepts 'dataset' but it's subject to change
+// Currently mock data is used via Storybook for development
+export const DatasetPortalWideDownloadCard = ({ dataset }) => {
+  const { responsive } = useResponsive()
+
+  const {
+    modality,
+    includes_merged: includeMerged,
+    metadata_only: metadataOnly
+  } = dataset
+
+  const fileItems = [
+    modality,
+    ...['has_cite_seq_data', 'has_bulk_rna_seq'].filter((key) => dataset[key])
+  ].map((key) => getReadableFiles(key))
+
+  const [mergeObject, setMergeObject] = useState(false)
+  const handleChangeMergedObject = () => setMergeObject(!mergeObject)
+
+  return (
+    <Box elevation="medium" pad="24px" width="full">
+      <Box
+        border={{ side: 'bottom' }}
+        margin={{ bottom: '24px' }}
+        pad={{ bottom: 'medium' }}
+      >
+        <Link href="#demo">
+          <Text weight="bold" color="brand" size="large">
+            {getReadable(dataset.format)} Download
+          </Text>
+        </Link>
+      </Box>
+      <Box
+        as="ul"
+        margin={{ top: '0', bottom: 'large' }}
+        pad={{ left: 'large' }}
+        style={{ listStyle: 'disc' }}
+      >
+        {metadataOnly ? (
+          <Li>Sample metadata from all projects</Li>
+        ) : (
+          <>
+            {fileItems.map((item) => (
+              <Li key={item}>{item}</Li>
+            ))}
+            <Li>Project and Sample Metadata</Li>
+          </>
+        )}
+      </Box>
+      {includeMerged && (
+        <Box direction="row" margin={{ bottom: '24px' }}>
+          <CheckBox
+            checked={mergeObject}
+            label="Merge samples into one object per project"
+            onChange={handleChangeMergedObject}
+          />
+          <HelpLink link={config.links.when_downloading_merged_objects} />
+        </Box>
+      )}
+      <Box
+        align={responsive('start', 'center')}
+        direction={responsive('column', 'row')}
+        gap="large"
+      >
+        <Box direction="column">
+          {!metadataOnly && (
+            <Text margin={{ bottom: 'small' }} weight="bold">
+              Size: {formatBytes(dataset.size_in_bytes)}
+            </Text>
+          )}
+          <Box
+            direction={responsive('column', 'row')}
+            gap="24px"
+            margin={{ bottom: 'small' }}
+          >
+            <Button primary aria-label="Download" label="Download" />
+            {!metadataOnly && <CopyLinkButton computedFile={{}} />}
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default DatasetPortalWideDownloadCard
