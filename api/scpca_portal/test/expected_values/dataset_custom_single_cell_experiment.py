1e8b10a1560ea20071b1c02c7381a05530cbc452
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-04-23 17:02:00 -0400

    add to exp val init, update file list to comment out metadata for now, remove computed file values

--- api/scpca_portal/test/expected_values/dataset_custom_single_cell_experiment.py
+++ api/scpca_portal/test/expected_values/dataset_custom_single_cell_experiment.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-
 from scpca_portal.enums import DatasetFormats, Modalities
 
 
@@ -44,6 +42,9 @@ class DatasetCustomSingleCellExperiment:
         "is_expired": False,
     }
     COMPUTED_FILE_LIST = [
+        "README.md",
+        "SCPCP999990_bulk_rna/SCPCP999990_bulk_metadata.tsv",
+        "SCPCP999990_bulk_rna/SCPCP999990_bulk_quant.tsv",
         "SCPCP999990_single_cell/SCPCS999990/SCPCL999990_celltype-report.html",
         "SCPCP999990_single_cell/SCPCS999990/SCPCL999990_filtered.rds",
         "SCPCP999990_single_cell/SCPCS999990/SCPCL999990_processed.rds",
@@ -54,9 +55,7 @@ class DatasetCustomSingleCellExperiment:
         "SCPCP999990_single_cell/SCPCS999997/SCPCL999997_processed.rds",
         "SCPCP999990_single_cell/SCPCS999997/SCPCL999997_qc.html",
         "SCPCP999990_single_cell/SCPCS999997/SCPCL999997_unfiltered.rds",
-        "SCPCP999990_single-cell/single-cell_metadata.tsv",
-        "SCPCP999990_bulk_rna/SCPCP999990_bulk_metadata.tsv",
-        "SCPCP999990_bulk_rna/SCPCP999990_bulk_quant.tsv",
+        # "SCPCP999990_single-cell/single-cell_metadata.tsv",
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/SCPCL999991_metadata.json",
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/SCPCL999991_spaceranger-summary.html",
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/filtered_feature_bc_matrix/barcodes.tsv.gz",  # noqa
@@ -71,7 +70,7 @@ class DatasetCustomSingleCellExperiment:
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/spatial/tissue_hires_image.png",
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/spatial/tissue_lowres_image.png",
         "SCPCP999990_spatial/SCPCS999991/SCPCL999991_spatial/spatial/tissue_positions_list.csv",
-        "SCPCP999990_spatial/spatial_metadata.tsv",
+        # "SCPCP999990_spatial/spatial_metadata.tsv",
         "SCPCP999991_single_cell/SCPCS999992_SCPCS999993/SCPCL999992_celltype-report.html",
         "SCPCP999991_single_cell/SCPCS999992_SCPCS999993/SCPCL999992_filtered.rds",
         "SCPCP999991_single_cell/SCPCS999992_SCPCS999993/SCPCL999992_processed.rds",
@@ -82,26 +81,13 @@ class DatasetCustomSingleCellExperiment:
         "SCPCP999991_single_cell/SCPCS999995/SCPCL999995_processed.rds",
         "SCPCP999991_single_cell/SCPCS999995/SCPCL999995_qc.html",
         "SCPCP999991_single_cell/SCPCS999995/SCPCL999995_unfiltered.rds",
-        "SCPCP999991_single-cell/single-cell_metadata.tsv",
+        # "SCPCP999991_single-cell/single-cell_metadata.tsv",
         "SCPCP999992_single-cell_merged/SCPCP999992_merged-summary-report.html",
         "SCPCP999992_single-cell_merged/SCPCP999992_merged.rds",
         "SCPCP999992_single-cell_merged/individual_reports/SCPCS999996/SCPCL999996_celltype-report.html",  # noqa
         "SCPCP999992_single-cell_merged/individual_reports/SCPCS999996/SCPCL999996_qc.html",
         "SCPCP999992_single-cell_merged/individual_reports/SCPCS999998/SCPCL999998_celltype-report.html",  # noqa
         "SCPCP999992_single-cell_merged/individual_reports/SCPCS999998/SCPCL999998_qc.html",
-        "SCPCP999992_single-cell_merged/single-cell_metadata.tsv",
-        "README.md",
+        # "SCPCP999992_single-cell_merged/single-cell_metadata.tsv",
+        "metadata.tsv",  # TODO: delete this when Metadata logic is updated
     ]
-    COMPUTED_FILE_VALUES = {
-        "format": DatasetFormats.SINGLE_CELL_EXPERIMENT.value,
-        "has_bulk_rna_seq": True,
-        "has_cite_seq_data": False,
-        "has_multiplexed_data": False,
-        "includes_merged": False,
-        "modality": Modalities.SINGLE_CELL.value,
-        "metadata_only": False,
-        "s3_bucket": settings.AWS_S3_OUTPUT_BUCKET_NAME,
-        "size_in_bytes": 9113,
-        "workflow_version": "development",
-        "includes_celltype_report": True,
-    }
