7051c8234921bc7d9224a3a3f91c4d07789ae34f
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-04-29 10:13:31 -0400

    add tests for disabled http methods

--- api/scpca_portal/test/views/test_dataset.py
+++ api/scpca_portal/test/views/test_dataset.py
@@ -9,9 +9,34 @@ class DatasetsTestCase(APITestCase):
     """Tests /datasets/ operations."""
 
     def setUp(self):
-        self.project = DatasetFactory()
+        self.dataset = DatasetFactory()
+
+    def test_get_single(self):
+        url = reverse("datasets-detail", args=[self.dataset.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_get_list(self):
-        response = self.client.get(reverse("datasets-list"))
+        url = reverse("datasets-list")
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()["count"], 1)
+
+    def test_post_is_not_allowed(self):
+        url = reverse("datasets-list", args=[])
+        response = self.client.post(url, data={})
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_is_not_allowed(self):
+        url = reverse("datasets-detail", args=[self.dataset.id])
+        response = self.client.put(url, data={})
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_is_not_allowed(self):
+        url = reverse("datasets-detail", args=[self.dataset.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
