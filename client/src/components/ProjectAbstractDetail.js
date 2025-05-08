e7af87068c0fb9dc0713e91b2cc0f13230268f2c
David <davidsmejia@gmail.com>
2025-05-08 13:39:02 -0400

    prettier

--- client/src/components/ProjectAbstractDetail.js
+++ client/src/components/ProjectAbstractDetail.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { Paragraph } from 'grommet'
 
 export const ProjectAbstractDetail = ({ abstract }) =>
-  abstract
-    .split('\\n')
-    .map((line, i) => (
-      <Paragraph key={line} margin={{ top: i && 'small' }}>
-        {line}
-      </Paragraph>
-    ))
+  abstract.split('\\n').map((line, i) => (
+    <Paragraph key={line} margin={{ top: i && 'small' }}>
+      {line}
+    </Paragraph>
+  ))
