5bcb3a4dbd26da6449251d9868ffff5d7a0c757e
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-12 17:34:50 -0400

    add batch ec2 job queue

--- infrastructure/batch/job_queue.tf
+++ infrastructure/batch/job_queue.tf
@@ -10,3 +10,16 @@ resource "aws_batch_job_queue" "scpca_portal_project" {
 
   tags = var.batch_tags
 }
+
+resource "aws_batch_job_queue" "scpca_portal_ec2" {
+  name     = "scpca-portal-project-queue-${var.user}-${var.stage}"
+  state    = "ENABLED"
+  priority = 1
+
+  compute_environment_order {
+    order = 2
+    compute_environment = aws_batch_compute_environment.scpca_portal_ec2.arn
+  }
+
+  tags = var.batch_tags
+}
