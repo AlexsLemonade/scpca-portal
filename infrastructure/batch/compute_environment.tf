4001e030d6b5f8dcaaf7701eede526b8ab310838
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-13 16:33:20 -0400

    add launch_template with ebs volume to batch compute environment

--- infrastructure/batch/compute_environment.tf
+++ infrastructure/batch/compute_environment.tf
@@ -37,6 +37,12 @@ resource "aws_batch_compute_environment" "scpca_portal_ec2" {
     # t2.large is the api's instance type but we should decide if we to change it
     instance_type = "t2.large"
     instance_role = aws_iam_instance_profile.batch_instance_profile.arn
+
+    launch_template {
+      launch_template_id = aws_launch_template.scpca_portal_ec2.id
+      version = aws_launch_template.scpca_portal_ec2.latest_version
+    }
+
   }
 
   service_role = aws_iam_role.batch_service_role.arn
@@ -44,4 +50,31 @@ resource "aws_batch_compute_environment" "scpca_portal_ec2" {
   depends_on   = [aws_iam_role_policy_attachment.batch_service_role]
 
   tags = var.batch_tags
+}
+
+resource "aws_launch_template" "scpca_portal_ec2" {
+  name = "scpca-portal-ec2-launch-template-${var.user}-${var.stage}-"
+  image_id = data.aws_ssm_parameter.ecs_ami.value
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 500
+      volume_type = "gp3"
+      delete_on_termination = true
+    }
+  }
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.batch_instance_profile.name
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = var.batch_tags
+  }
+}
+
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
