57fe0f1c144bdeb36ca3cc9b7fc97b52a5a51092
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-13 09:02:42 -0400

    move new batch iam roles to batch/roles

--- infrastructure/batch/roles.tf
+++ infrastructure/batch/roles.tf
@@ -167,3 +167,23 @@ resource "aws_iam_role_policy_attachment" "batch_ses_send_policy" {
   role       = aws_iam_role.batch_job_role.name
   policy_arn = aws_iam_policy.batch_ses_send_email.arn
 }
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "scpca-portal-ec2-instance-profile-${var.user}-${var.stage}"
+  role = aws_iam_role.ec2_instance_role.name
+}
+
+resource "aws_iam_role" "ec2_instance_role" {
+  name = "scpca-portal-ec2-instance-role-${var.user}-${var.stage}"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ec2_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
