ef37567bc12e2fdafb472ff91573c7e936c2cf7c
Nozomi Ichihara <nozomione.online@gmail.com>
2025-05-16 11:32:20 -0400

    (revert) revert the changes made to common.METADATA_EXTENSIONS (adding a '.tsv' extension) and InputBucketS3KeyInfo._is_metadata for now, as they'are inconsequential - we'll revisit them after the conclusion of #1261 as required

--- api/scpca_portal/utils/input_bucket_s3_key_info.py
+++ api/scpca_portal/utils/input_bucket_s3_key_info.py
@@ -108,6 +108,4 @@ class InputBucketS3KeyInfo:
 
     @property
     def _is_metadata(self):
-        # TODO: Revisit once the download option for bulk metadata is finalized
-        # ensure the file is metadata, not bulk (bulk is metadata but also included in downloads)
-        return not self._is_bulk and self.s3_key_path.suffix in common.METADATA_EXTENSIONS
+        return self.s3_key_path.suffix in common.METADATA_EXTENSIONS
