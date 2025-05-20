37a94d6ed20796bd6253b00c5c8425b271d1b0dd
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-20 09:44:18 -0400

    clarify ec2 pipeline output names

--- infrastructure/batch/outputs.tf
+++ infrastructure/batch/outputs.tf
@@ -10,10 +10,10 @@ output "job_definition_name_fargate" {
 
 output "job_queue_name_ec2" {
   description = "Name of Batch Job Queue"
-  value = aws_batch_job_queue.scpca_portal_fargate.name
+  value = aws_batch_job_queue.scpca_portal_ec2.name
 }
 
 output "job_definition_name_ec2" {
   description = "Name of Batch Job Definition"
-  value = aws_batch_job_definition.scpca_portal_fargate.name
+  value = aws_batch_job_definition.scpca_portal_ec2.name
 }
