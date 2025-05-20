8c30c6206b1f87c121109f648cc2f83dc84fae6b
Nozomi Ichihara <nozomione.online@gmail.com>
2025-05-20 13:35:54 -0400

    add labels to Modalities and FileFormats enum members and apply migrations

--- api/scpca_portal/enums/modalities.py
+++ api/scpca_portal/enums/modalities.py
@@ -5,5 +5,5 @@ class Modalities(TextChoices):
     BULK_RNA_SEQ = "BULK_RNA_SEQ", "Bulk RNA-seq"
     CITE_SEQ = "CITE_SEQ", "CITE-seq"
     MULTIPLEXED = "MULTIPLEXED", "Multiplexed"
-    SINGLE_CELL = "SINGLE_CELL", "Single Cell"
-    SPATIAL = "SPATIAL", "Spatial Data"
+    SINGLE_CELL = "SINGLE_CELL", "Single-cell"
+    SPATIAL = "SPATIAL", "Spatial Data"  # TODO: Remove 'Data' and translate via FE
