c5cab0cae2369feb0761217d3cd24d45c30d76d1
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-21 10:23:07 -0400

    move serializers.py into new serializers directory, add init file

---
+++ api/scpca_portal/serializers/__init__.py
@@ -0,0 +1,11 @@
+from .serializers import (
+    ComputedFileSerializer,
+    DatasetCreateSerializer,
+    DatasetDetailSerializer,
+    DatasetSerializer,
+    DatasetUpdateSerializer,
+    ProjectLeafSerializer,
+    ProjectSerializer,
+    ProjectSummarySerializer,
+    SampleSerializer,
+)
