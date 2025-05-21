f204dd0004c8ac03d816d2d2f55d9d60586ca496
Nozomi Ichihara <nozomione.online@gmail.com>
2025-05-21 08:44:07 -0400

    (fix) correct a typo and reapply migratinos

--- api/scpca_portal/enums/file_formats.py
+++ api/scpca_portal/enums/file_formats.py
@@ -5,4 +5,4 @@ class FileFormats(TextChoices):
     ANN_DATA = "ANN_DATA", "AnnData"
     SINGLE_CELL_EXPERIMENT = "SINGLE_CELL_EXPERIMENT", "Single-cell experiment"
     METADATA = "METADATA", "Metadata"
-    SUPPLEMENTARY = "SUPPLEMENTARY", "supplementary"
+    SUPPLEMENTARY = "SUPPLEMENTARY", "Supplementary"
