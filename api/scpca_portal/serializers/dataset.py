882ef29071956a173a963376e064488bc62be2db
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-22 12:16:12 -0400

    implement create dataset serializer inheriting from base serializer

--- api/scpca_portal/serializers/dataset.py
+++ api/scpca_portal/serializers/dataset.py
@@ -69,64 +69,15 @@ class DatasetDetailSerializer(DatasetSerializer):
     computed_file = ComputedFileSerializer(read_only=True, many=False)
 
 
-class DatasetCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dataset
-        fields = (
-            "data",
-            "email",
-            "format",
-            "start",
-            "id",
-            "stats",
-            "data_hash",
-            "metadata_hash",
-            "readme_hash",
-            "is_ccdl",
-            "ccdl_name",
-            "ccdl_project_id",
-            "started_at",
-            "is_started",
-            "is_processing",
-            "succeeded_at",
-            "is_succeeded",
-            "failed_at",
-            "is_failed",
-            "failed_reason",
-            "expires_at",
-            "is_expired",
-            "terminated_at",
-            "is_terminated",
-            "terminated_reason",
-            "computed_file",
-        )
-        read_only_fields = (
-            "id",
-            "stats",
-            "data_hash",
-            "metadata_hash",
-            "readme_hash",
-            "is_ccdl",
-            "ccdl_name",
-            "ccdl_project_id",
-            "started_at",
-            "is_started",
-            "is_processing",
-            "succeeded_at",
-            "is_succeeded",
-            "failed_at",
-            "is_failed",
-            "failed_reason",
-            "expires_at",
-            "is_expired",
-            "terminated_at",
-            "is_terminated",
-            "terminated_reason",
-            "computed_file",
+class DatasetCreateSerializer(DatasetSerializer):
+    class Meta(DatasetSerializer.Meta):
+        modifiable_fields = ("format", "data", "email", "start")
+        read_only_fields = tuple(
+            set(DatasetSerializer.Meta.read_only_fields) - set(modifiable_fields)
         )
 
 
-class DatasetUpdateSerializer(serializers.ModelSerializer):
+class DatasetUpdateSerializer(DatasetSerializer):
     class Meta:
         model = Dataset
         fields = (
