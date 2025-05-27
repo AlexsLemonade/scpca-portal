33dd686e31f7554f70ce58e4c28d45a65ac92ab7
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-27 13:51:43 -0400

    add process_dataset management command

---
+++ api/scpca_portal/management/commands/process_dataset.py
@@ -0,0 +1,41 @@
+import logging
+from argparse import BooleanOptionalAction
+
+from django.core.management.base import BaseCommand
+
+from scpca_portal import loader, notifications
+from scpca_portal.models import Dataset
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+class Command(BaseCommand):
+    help = """
+    This command is meant to be called as an entrypoint to AWS Batch Fargate job instance.
+    Individual files are computed according:
+        - To the project or sample id
+        - An appropriate corresponding download config
+
+    When computation is completed, files are uploaded to S3, and the job is marked as completed.
+
+    At which point the instance which generated this computed file will receive a new job
+    from the job queue and begin computing the next file.
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("--dataset-id", type=str)
+        parser.add_argument("--notify", type=bool, default=False, action=BooleanOptionalAction)
+
+    def handle(self, *args, **kwargs):
+        self.process_dataset(**kwargs)
+
+    def process_dataset(self, dataset_id: str, notify: bool, **kwargs) -> None:
+        dataset = Dataset.objects.filter(id=dataset_id).first()
+        if not dataset:
+            logger.error(f"{dataset} does not exist.")
+        loader.process_dataset(dataset)
+
+        if notify:
+            notifications.send_dataset_file_completed_email(dataset_id)
