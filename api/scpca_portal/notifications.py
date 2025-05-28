1386d87f5a2a5e3a5e4cf1f6b791bff8dd3e5c6a
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-05-28 15:06:16 -0400

    update notifications dataset file email method

--- api/scpca_portal/notifications.py
+++ api/scpca_portal/notifications.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 import boto3
 
 from scpca_portal.config.logging import get_and_configure_logger
+from scpca_portal.models import Job
 
 logger = get_and_configure_logger(__name__)
 ses = boto3.client("ses", region_name=settings.AWS_REGION)
@@ -14,7 +15,7 @@ TEMPLATE_FILE_PATH = TEMPLATE_ROOT / "default.html"
 
 def send_project_files_completed_email(
     project_id: str,
-    sender: str = settings.TEST_EMAIL_SENDER,
+    sender: str = settings.EMAIL_SENDER,
     recipient: str = settings.TEST_EMAIL_RECIPIENT,
 ) -> None:
     SENDER = sender
@@ -42,22 +43,18 @@ def send_project_files_completed_email(
     )
 
 
-def send_dataset_file_completed_email(
-    dataset_id: str,
-    sender: str = settings.TEST_EMAIL_SENDER,
-    recipient: str = settings.TEST_EMAIL_RECIPIENT,
-) -> None:
-    SENDER = sender
-    RECIPIENT = recipient
+def send_dataset_file_completed_email(job: Job) -> None:
+    SENDER = settings.EMAIL_SENDER
+    RECIPIENT = job.dataset.email
     CHARSET = "UTF-8"
 
-    SUBJECT = f"All files generated for {dataset_id}"
+    SUBJECT = f"All files generated for {job.dataset.id}"
     BODY_TEXT = (
         "Hot off the presses:\n\n"
-        f"All files have been generated for project {dataset_id}"
+        f"All files have been generated for project {job.dataset.id}"
         + "\n\nLove!,\nThe ScPCA Portal Team"
     )
-    BODY_HTML = render_to_string(TEMPLATE_FILE_PATH, context={"dataset_id": dataset_id})
+    BODY_HTML = render_to_string(TEMPLATE_FILE_PATH, context={"dataset_id": job.dataset.id})
 
     ses.send_email(
         Source=SENDER,
