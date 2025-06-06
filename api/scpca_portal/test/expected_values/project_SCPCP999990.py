34910371b640523fcda24ff323012792f993bbce
Nozomi Ichihara <nozomione.online@gmail.com>
2025-06-06 12:26:03 -0400

    (edit) adjust the existing tests to reflect Sample.modalities change

--- api/scpca_portal/test/expected_values/project_SCPCP999990.py
+++ api/scpca_portal/test/expected_values/project_SCPCP999990.py
@@ -25,8 +25,8 @@ class Project_SCPCP999990:
         "includes_merged_anndata": True,
         "includes_xenografts": False,
         "modalities": [
-            Modalities.BULK_RNA_SEQ.label,
-            Modalities.SPATIAL.label,
+            Modalities.BULK_RNA_SEQ,
+            Modalities.SPATIAL,
         ],
         "multiplexed_sample_count": 0,
         "organisms": ["Homo sapiens"],
--- api/scpca_portal/test/factories.py
+++ api/scpca_portal/test/factories.py
@@ -66,7 +66,7 @@ class LeafProjectFactory(factory.django.DjangoModelFactory):
     seq_units = "cell"
     technologies = "10Xv2_5prime, CITE-seq"
     has_bulk_rna_seq = True
-    modalities = ["CITE-seq"]
+    modalities = ["CITE_SEQ"]
     organisms = ["Homo sapiens"]
     sample_count = 60
 
