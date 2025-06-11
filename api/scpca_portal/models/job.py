651ded601c24015bda26c9503be6915e7c8f07cf
Nozomi Ichihara <nozomione.online@gmail.com>
2025-06-11 10:12:12 -0400

    (minor) fix a typo - event_hanlderto event_handler and update the docstring in Job::set_state

--- api/scpca_portal/models/job.py
+++ api/scpca_portal/models/job.py
@@ -311,18 +311,18 @@ class Job(TimestampedModel):
     def set_state(self, state: JobStates, reason: str = None):
         """
         Sets the job state and its corresponding timestamp.
-        Calls the associated dataset event handler.
+        Calls the associated dataset's on_job_<state> event handler.
         """
         state_str = state.lower()
         reason_attr = f"{state_str}_reason"
-        event_hanlder = f"on_job_{state_str}"
+        event_handler = f"on_job_{state_str}"
 
         self.state = state
         if hasattr(self, reason_attr):
             setattr(self, reason_attr, reason)
         self.update_state_at()
 
-        getattr(self.dataset, event_hanlder)()
+        getattr(self.dataset, event_handler)()
 
     def submit(self) -> bool:
         """
