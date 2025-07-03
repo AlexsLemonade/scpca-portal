143ad1b7e36a2de0915d5d738eee0d7f67d48956
Nozomi Ichihara <nozomione.online@gmail.com>
2025-07-03 14:10:01 -0400

    add the datasets resource to the api folder

---
+++ client/src/api/resources/datasets.js
@@ -0,0 +1,22 @@
+import { getAPIUrl } from 'helpers/getAPIUrl'
+import { request } from 'helpers/request'
+
+const datasets = {
+  create: (body, authorization) =>
+    request(getAPIUrl(`datasets`), {
+      method: 'POST',
+      authorization,
+      body: JSON.stringify(body)
+    }),
+  get: (id, authorization = '') =>
+    request(getAPIUrl(`datasets/${id}`), { authorization }), // token required for file downloads
+  list: () => request(getAPIUrl('datasets', { is_ccdl: true })), // for CCDL datasets
+  update: (id, body, authorization) =>
+    request(getAPIUrl(`datasets/${id}`), {
+      method: 'PUT',
+      authorization,
+      body: JSON.stringify(body)
+    })
+}
+
+export default datasets
