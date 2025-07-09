a67dd381d9034705bfa357895909cd698364a382
Nozomi Ichihara <nozomione.online@gmail.com>
2025-07-09 18:37:48 -0400

    (edit) remove no lponger required methods, add a getProjectData helper used via UI component to constract data slice for addProject, replace addSamples/removeSamples with setSamples (merging samples will be handled within UI component), and add comments for a subsequest issue

--- client/src/hooks/useDatasetManager.js
+++ client/src/hooks/useDatasetManager.js
@@ -10,12 +10,6 @@ export const useDatasetManager = () => {
     useContext(DatasetManagerContext)
   const { token, userFormat, setUserFormat } = useScPCAPortal()
 
-  /* Dataset Configs */
-  const saveFormat = (format) => {
-    // Sets the download format only when creating a new dataset.
-    setUserFormat(format)
-  }
-
   /* Dataset-level */
   const clearDataset = async (dataset) => {
     const updatedDataset = await updateDataset({ ...dataset, data: {} })
@@ -23,67 +17,68 @@ export const useDatasetManager = () => {
   }
 
   const createDataset = async (dataset) => {
+    // Asserts that format is defined and save if defined
     const newDataset = await api.datasets.create(dataset, token)
+    // If success, update myDataset
     return newDataset
   }
 
   const getDataset = async (dataset) => {
-    // A valid API token is required for dataset file downloads.
+    // A valid API token is required for dataset file downloads
     const latestDataset = await api.datasets.get(dataset.id, token)
+    // If success, updates myDataset
     return latestDataset
   }
 
   const processDataset = async (dataset) => {
+    // Sets the start flag to true to process the dataset
     const latestDataset = !dataset.id
       ? await createDataset(dataset)
       : await updateDataset(dataset)
+    // If success, removes myDataset(no longer editable)
     return latestDataset
   }
 
   const updateDataset = async (dataset) => {
     const latestDataset = await api.datasets.update(dataset.id, dataset, token)
+    // If success, updates myDataset
     return latestDataset
   }
 
   /* Project-level */
-  const addProject = async (dataset, project, modalities = []) => {
+  const addProject = async (dataset, project, projectData = {}) => {
+    // Copies the dataset before mutation
+    // Appends projectData to the specified project in dataset.data
     const updatedDataset = !dataset.id
       ? await createDataset(dataset)
       : await updateDataset(dataset)
     return updatedDataset
   }
 
-  const removeProject = async (dataset, project, modalities = []) => {
-    const updatedDataset = await updateDataset(dataset.id, dataset)
-    return updatedDataset
-  }
-
-  // Single-cell only
-  const mergeProject = async (dataset, project, modality = 'SINGLE_CELL') => {
-    const updatedDataset = !dataset.id
-      ? await createDataset(dataset)
-      : await updateDataset(dataset)
-    return updatedDataset
-  }
-
-  // Single-cell only
-  const unmergeProject = async (dataset, project, modality = 'SINGLE_CELL') => {
-    const updatedDataset = await updateDataset(dataset.id, dataset)
-    return updatedDataset
+  const getProjectData = (project, modality, merged = false) => {
+    // Returns an object that would populate dataset.data.[project.scpca_id]
+    const dataSlice = {}
+    dataSlice[modality] = merged
+      ? 'MERGED'
+      : project.samples.filter((s) => s.has_modality).map((s) => s.sample_id)
+    return dataSlice
   }
 
   const getProjectIDs = (dataset) => Object.keys(dataset.data)
 
-  /* Sample-level */
-  const addSamples = async (dataset, project, modality, samples) => {
-    const updatedDataset = !dataset.id
-      ? await createDataset(dataset)
-      : await updateDataset(dataset)
+  const removeProject = async (dataset, project) => {
+    // Copies the dataset before mutation
+    // Removes the speficied project from dataset.data
+    const updatedDataset = await updateDataset(dataset.id, dataset)
     return updatedDataset
   }
 
-  const removeSamples = async (dataset, project, modality, samples) => {
-    const updatedDataset = await updateDataset(dataset.id, dataset)
+  /* Sample-level */
+  const setSamples = async (dataset, project, modality, updatedSamples) => {
+    // 'updatedSamples' is either sampleIDs[] or 'MERGE'
+    const updatedDataset = !dataset.id
+      ? await createDataset(dataset)
+      : await updateDataset(dataset)
     return updatedDataset
   }
 
@@ -96,11 +91,9 @@ export const useDatasetManager = () => {
     getDataset,
     processDataset,
     addProject,
+    getProjectData,
     removeProject,
-    mergeProject,
-    unmergeProject,
     getProjectIDs,
-    addSamples,
-    removeSamples
+    setSamples
   }
 }
