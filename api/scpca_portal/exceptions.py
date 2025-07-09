f13b47baf700344a4dcb35976b85471b8ef0ca1b
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-09 12:47:13 -0400

    add JobError and DatasetError custom exceptions

---
+++ api/scpca_portal/exceptions.py
@@ -0,0 +1,10 @@
+class JobError(Exception):
+    """Base exception for job-related errors."""
+
+    pass
+
+
+class DatasetError(Exception):
+    """Base exception for dataset-related errors."""
+
+    pass
