1d6d2f148535e8b9474433aa83bc58939514dc4c
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-09 16:47:31 -0400

    add unique custom job and dataset errors

--- api/scpca_portal/exceptions.py
+++ api/scpca_portal/exceptions.py
@@ -1,15 +1,41 @@
 from scpca_portal.enums import ErrorMessages
 
 
-class JobError(Exception):
-    """Base exception for job-related errors."""
+class DatasetError(Exception):
+    def __init__(self, message=None):
+        super().__init__(message or ErrorMessages.DATASET_GENERIC)
 
+
+class DatasetLockedProjectError(DatasetError):
+    def __init__(self):
+        super().__init__(ErrorMessages.DATASET_LOCKED_PROJECT)
+
+
+class DatasetMissingLibrariesError(DatasetError):
+    def __init__(self):
+        super().__init__(ErrorMessages.DATASET_NO_LIBRARIES)
+
+
+class JobError(Exception):
     def __init__(self, message=None):
         super().__init__(message or ErrorMessages.JOB_GENERIC)
 
 
-class DatasetError(Exception):
-    """Base exception for dataset-related errors."""
+class JobNotPendingError(JobError):
+    def __init__(self):
+        super().__init__(ErrorMessages.JOB_NOT_PENDING)
 
-    def __init__(self, message=None):
-        super().__init__(message or ErrorMessages.DATASET_GENERIC)
+
+class JobMaxAttemptsExceeedError(JobError):
+    def __init__(self):
+        super().__init__(ErrorMessages.JOB_MAX_ATTEMPTS_EXCEEDED)
+
+
+class JobSubmissionFailedError(JobError):
+    def __init__(self):
+        super().__init__(ErrorMessages.JOB_SUBMISSION_FAILED)
+
+
+class JobInvalidRetryStateError(JobError):
+    def __init__(self):
+        super().__init__(ErrorMessages.JOB_INVALID_RETRY_STATE)
