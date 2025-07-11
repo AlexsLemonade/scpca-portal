917acc234da3f55b84256f9d3e1d10980e306e30
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-11 12:13:28 -0400

    handle corner case when get_lockfile_project_ids, which now leverages lockfile original file, is called during sync_original_files

--- api/scpca_portal/lockfile.py
+++ api/scpca_portal/lockfile.py
@@ -11,13 +11,19 @@ logger = get_and_configure_logger(__name__)
 LOCKFILE_S3_KEY = "projects.lock"
 
 
-def get_lockfile_project_ids() -> List[str]:
+def get_lockfile_project_ids(*, initial_sync=False) -> List[str]:
     """Return list of all projects ids present in the lockfile."""
     lockfile_original_file = OriginalFile.objects.filter(
         s3_key=LOCKFILE_S3_KEY, s3_bucket=settings.AWS_S3_INPUT_BUCKET_NAME
     ).first()
+    # create default lockfile original file in memory
+    # if method is called during initial syncing of original files
+    if initial_sync:
+        lockfile_original_file = OriginalFile(
+            s3_key=LOCKFILE_S3_KEY, s3_bucket=settings.AWS_S3_INPUT_BUCKET_NAME
+        )
 
-    if lockfile_original_file.size_in_bytes == 0:
+    if lockfile_original_file.size_in_bytes == 0 and not initial_sync:
         return []
 
     if lockfile_original_file.local_file_path.exists():
