91238b290142a57082e0a5affb0db28286e057b3
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-18 16:39:08 -0400

    fix errors in initial pydantic implementation

--- api/scpca_portal/validators.py
+++ api/scpca_portal/validators.py
@@ -1,28 +1,42 @@
 import re
-from typing import Dict, List, Optional, Union
+from typing import Any, Dict, Iterable, List
 
-from pydantic import BaseModel, Field, root_validator, validator
+from pydantic import BaseModel, Field, ValidationInfo, field_validator
 
 PROJECT_ID_REGEX = r"^SCPCP\d{6}$"
 SAMPLE_ID_REGEX = r"^SCPCS\d{6}$"
 
 
 class ProjectData(BaseModel):
-    includes_bulk: Optional[bool] = None
-    single_cell: Optional[Union[List[str], str]] = Field(default=None, alias="single_cell")
-    spatial: Optional[List[str]] = Field(default=None, alias="spatial")
+    includes_bulk: bool = Field(default=True)
+    single_cell: List[str] | str = Field(default_factory=list)
+    spatial: List[str] = Field(default_factory=list)
 
-    @validator("single_cell", "spatial", each_item=True)
-    def validate_samples_ids(cls, value):
-        if not re.match(SAMPLE_ID_REGEX, value):
-            raise ValueError(f"Invalid sample ID: {value}")
+    @field_validator("single_cell", "spatial", mode="after")
+    @classmethod
+    def validate_samples_ids(cls, value: Any, info: ValidationInfo):
+        if info.field_name == "single_cell" and isinstance(value, str) and value == "MERGED":
+            return value
+
+        if not isinstance(value, list):
+            raise TypeError(
+                "Expected a list of Sample IDs or a 'MERGED' string (for single-cell only)."
+            )
+
+        for sample_id in value:
+            if not re.match(SAMPLE_ID_REGEX, sample_id):
+                raise ValueError(f"Invalid sample ID: {sample_id}.")
+
+        return value
 
 
 class DatasetData(BaseModel):
-    __root__: Dict[str, ProjectData]
+    projects_data: Dict[str, ProjectData]
+    # __root__: Dict[str, ProjectData]  # alternative to projects_data declaration approach
 
-    @root_validator(pre=True)
-    def validate_project_ids(cls, values):
+    @field_validator("projects_data", mode="after")
+    @classmethod
+    def validate_project_ids(cls, values: Iterable[Any]):
         for project_id in values:
             if not re.match(PROJECT_ID_REGEX, project_id):
                 raise ValueError(f"Invalid project ID: {project_id}")
