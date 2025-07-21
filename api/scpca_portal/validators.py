1062a4e33280d8814d99500584e303f26f6ebed3
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-21 13:34:20 -0400

    clean up ProjectData and DatasetData validator classes

--- api/scpca_portal/validators.py
+++ api/scpca_portal/validators.py
@@ -1,7 +1,7 @@
 import re
-from typing import Any, Dict, Iterable, List
+from typing import Any, Dict, List
 
-from pydantic import BaseModel, Field, ValidationInfo, field_validator
+from pydantic import BaseModel, Field, RootModel, ValidationInfo, field_validator, model_validator
 
 PROJECT_ID_REGEX = r"^SCPCP\d{6}$"
 SAMPLE_ID_REGEX = r"^SCPCS\d{6}$"
@@ -14,32 +14,36 @@ class ProjectData(BaseModel):
 
     @field_validator("single_cell", "spatial", mode="after")
     @classmethod
-    def validate_samples_ids(cls, value: Any, info: ValidationInfo):
-        if info.field_name == "single_cell" and isinstance(value, str) and value == "MERGED":
-            return value
-
-        if not isinstance(value, list):
-            # TODO: add custom exceptions
-            raise TypeError(
-                "Expected a list of Sample IDs or a 'MERGED' string (for single-cell only)."
+    def validate_samples_ids(cls, modality_value: Any, info: ValidationInfo):
+        # Note: There's no actual need to check if field_name is "single_cell"
+        # because if a str is passed to the "spatial" field, pydantic type checking will catch it.
+        # The check is included here for extra clarity.
+        if info.field_name == "single_cell" and isinstance(modality_value, str):
+            if modality_value == "MERGED":
+                return modality_value
+            raise ValueError(
+                """
+                Invalid string value for 'single-cell' modality: {modality_value}.
+                Only valid value is 'MERGED'.
+                """
             )
 
-        for sample_id in value:
+        for sample_id in modality_value:
             if not re.match(SAMPLE_ID_REGEX, sample_id):
-                # TODO: add custom exceptions
-                raise ValueError(f"Invalid sample ID: {sample_id}.")
+                # TODO: add custom exception
+                raise ValueError(f"Invalid sample ID format: {sample_id}.")
 
-        return value
+        return modality_value
 
 
-class DatasetData(BaseModel):
-    __root__: Dict[str, ProjectData]
+class DatasetData(RootModel):
+    root: Dict[str, ProjectData]
 
-    @field_validator("projects_data", mode="after")
+    @model_validator(mode="after")
     @classmethod
-    def validate_project_ids(cls, values: Iterable[Any]):
-        for project_id in values:
+    def validate_project_ids(cls, instance):
+        for project_id in instance.root:
             if not re.match(PROJECT_ID_REGEX, project_id):
-                # TODO: add custom exceptions
-                raise ValueError(f"Invalid project ID: {project_id}")
-        return values
+                # TODO: add custom exception
+                raise ValueError(f"Invalid project ID format: {project_id}")
+        return instance
