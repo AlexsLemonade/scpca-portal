8433fe0fdbbbe65671965bef7c61b05875649d27
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-07-31 16:11:43 -0400

    remove is_ccdl filterset field form ccdl datset views, temporarily comment out format field until its filter is stable

--- api/scpca_portal/views/ccdl_dataset.py
+++ api/scpca_portal/views/ccdl_dataset.py
@@ -1,22 +1,21 @@
 from rest_framework import viewsets
 
-from rest_framework_extensions.mixins import NestedViewSetMixin
-
 from scpca_portal.models import Dataset
 from scpca_portal.serializers import DatasetDetailSerializer, DatasetSerializer
 
 
-class CCDLDatasetViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
-    queryset = Dataset.objects.all(is_ccdl=True).order_by("scpca_id")
+class CCDLDatasetViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Dataset.objects.filter(is_ccdl=True).order_by("ccdl_project_id")
     ordering_fields = "__all__"
     filterset_fields = (
         "id",
-        "is_ccdl",
         "ccdl_name",
         "ccdl_project_id",
-        "format",
+        # "format", # TODO: format is unqueriable due to an unknown error
+        #   the suspicion is that the enum value in the db
+        #   is not matching correctly to the string value in the request json
         # data  # TODO: consider if we want to query for modality on CCDL datasets
-        # or included project ids, modalities, and bulk in custom datasets
+        #   or included project ids, modalities, and bulk in custom datasets
     )
 
     def get_serializer_class(self):
