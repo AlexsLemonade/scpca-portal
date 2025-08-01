caa2a50572f684efeeedbb1d7b96e1e94f9da8fa
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-08-01 17:09:14 -0400

    add DatasetDataResourceExistence class with validate method to validators file, update DatasetData and ProjectData pydantic class names to DatasetDataModel and ProjectDataModel

--- api/scpca_portal/test/test_validators.py
+++ api/scpca_portal/test/test_validators.py
@@ -3,17 +3,17 @@ from django.test import TestCase
 from pydantic import ValidationError
 
 from scpca_portal.enums import Modalities
-from scpca_portal.validators import DatasetData, ProjectData
+from scpca_portal.validators import DatasetDataModel, ProjectDataModel
 
 
-class TestProjectData(TestCase):
+class TestProjectDataModel(TestCase):
     def test_valid_single_cell_list(self):
         project_data = {
             "includes_bulk": True,
             Modalities.SINGLE_CELL.value: ["SCPCS999990", "SCPCS999992"],
             Modalities.SPATIAL.value: ["SCPCS999991"],
         }
-        validated_project_data = ProjectData.model_validate(project_data)
+        validated_project_data = ProjectDataModel.model_validate(project_data)
 
         expected_single_cell_ids = ["SCPCS999990", "SCPCS999992"]
         self.assertEqual(validated_project_data.SINGLE_CELL, expected_single_cell_ids)
@@ -24,35 +24,35 @@ class TestProjectData(TestCase):
             Modalities.SINGLE_CELL.value: "MERGED",
             Modalities.SPATIAL.value: [],
         }
-        validated_project_data = ProjectData.model_validate(project_data)
+        validated_project_data = ProjectDataModel.model_validate(project_data)
         self.assertEqual(validated_project_data.SINGLE_CELL, "MERGED")
 
     def test_invalid_single_cell_sample_id(self):
         project_data = {Modalities.SINGLE_CELL.value: ["INVALID_SAMPLE_ID"]}
         with self.assertRaises(ValidationError) as context:
-            ProjectData.model_validate(project_data)
+            ProjectDataModel.model_validate(project_data)
         self.assertIn("Invalid sample ID format", str(context.exception))
 
     def test_invalid_single_cell_sample_id_passed_as_string(self):
         project_data = {Modalities.SINGLE_CELL.value: "SCPCS999990"}
         with self.assertRaises(ValidationError) as context:
-            ProjectData.model_validate(project_data)
+            ProjectDataModel.model_validate(project_data)
         self.assertIn("Sample IDs must be inside an Array", str(context.exception))
 
     def test_invalid_single_cell_merged_string(self):
         project_data = {Modalities.SINGLE_CELL.value: "NOT_MERGED"}
         with self.assertRaises(ValidationError) as context:
-            ProjectData.model_validate(project_data)
+            ProjectDataModel.model_validate(project_data)
         self.assertIn("Invalid string value for 'single-cell' modality", str(context.exception))
 
     def test_invalid_spatial_sample_id(self):
         project_data = {Modalities.SPATIAL.value: ["INVALID_SAMPLE_ID"]}
         with self.assertRaises(ValidationError) as context:
-            ProjectData.model_validate(project_data)
+            ProjectDataModel.model_validate(project_data)
         self.assertIn("Invalid sample ID format", str(context.exception))
 
 
-class TestDatasetData(TestCase):
+class TestDatasetDataModel(TestCase):
     def test_valid_dataset_data(self):
         data = {
             "SCPCP999990": {
@@ -66,19 +66,19 @@ class TestDatasetData(TestCase):
                 Modalities.SPATIAL.value: [],
             },
         }
-        validated_data = DatasetData.model_validate(data)
+        validated_data = DatasetDataModel.model_validate(data)
         self.assertIn("SCPCP999990", validated_data.root)
         self.assertEqual(validated_data.root["SCPCP999991"].SINGLE_CELL, "MERGED")
 
     def test_invalid_project_id(self):
         data = {"INVALID_PROJECT_ID": {Modalities.SINGLE_CELL.value: ["SCPCS999990"]}}
         with self.assertRaises(ValidationError) as context:
-            DatasetData.model_validate(data)
+            DatasetDataModel.model_validate(data)
         self.assertIn("Invalid project ID format", str(context.exception))
 
     def test_invalid_nested_sample_id(self):
         data = {"SCPCP999990": {Modalities.SINGLE_CELL.value: ["INVALID_SAMPLE_ID"]}}
         with self.assertRaises(ValidationError) as context:
-            DatasetData.model_validate(data)
+            DatasetDataModel.model_validate(data)
 
         self.assertIn("Invalid sample ID format", str(context.exception))
--- api/scpca_portal/validators.py
+++ api/scpca_portal/validators.py
@@ -3,13 +3,14 @@ from typing import Any, Dict, List
 
 from pydantic import BaseModel, Field, RootModel, ValidationInfo, field_validator, model_validator
 
-from scpca_portal.enums.modalities import Modalities
+from scpca_portal.enums import DatasetFormats, Modalities
+from scpca_portal.models.project import Project
 
 PROJECT_ID_REGEX = r"^SCPCP\d{6}$"
 SAMPLE_ID_REGEX = r"^SCPCS\d{6}$"
 
 
-class ProjectData(BaseModel):
+class ProjectDataModel(BaseModel):
     includes_bulk: bool = Field(default=True)
     SINGLE_CELL: List[str] | str = Field(default_factory=list, alias=Modalities.SINGLE_CELL.value)
     SPATIAL: List[str] = Field(default_factory=list, alias=Modalities.SPATIAL.value)
@@ -17,7 +18,7 @@ class ProjectData(BaseModel):
     @field_validator(Modalities.SINGLE_CELL.value, Modalities.SPATIAL.value, mode="after")
     @classmethod
     def validate_samples_ids(cls, modality_value: Any, info: ValidationInfo):
-        # Note: There's no actual need to check if field_name is "sINGLE_CELL"
+        # Note: There's no actual need to check if field_name is "SINGLE_CELL"
         # because if a str is passed to the "SPATIAL" field, pydantic type checking will catch it.
         # The check is included here for extra clarity.
         if info.field_name == Modalities.SINGLE_CELL.value and isinstance(modality_value, str):
@@ -43,8 +44,8 @@ class ProjectData(BaseModel):
         return modality_value
 
 
-class DatasetData(RootModel):
-    root: Dict[str, ProjectData]
+class DatasetDataModel(RootModel):
+    root: Dict[str, ProjectDataModel]
 
     @model_validator(mode="after")
     @classmethod
@@ -54,3 +55,30 @@ class DatasetData(RootModel):
                 # TODO: add custom exception
                 raise ValueError(f"Invalid project ID format: {project_id}")
         return instance
+
+
+class DatasetDataResourceExistence:
+
+    @staticmethod
+    def validate(data: Dict[str, Any], format: DatasetFormats) -> Dict:
+        """
+        Validates that projects and samples passed into the data attribute,
+        both exist and are correctly related.
+        Raises exceptions if projects, samples or their associations do not exist.
+        """
+        if format == DatasetFormats.ANN_DATA.value:
+            if any(
+                project_data.get(Modalities.SPATIAL.value, []) for project_data in data.values()
+            ):
+                # TODO: add custom exception
+                raise Exception("No Spatial data for ANNDATA.")
+
+        # validate that all projects exist
+        existing_ids = Project.objects.filter(scpca_id__in=data.keys()).values_list(
+            "scpca_id", flat=True
+        )
+        if missing_keys := set(data.keys()) - set(existing_ids):
+            raise Exception(f"The following projects do not exist: {list(missing_keys)}")
+
+        # TODO: sample modality existence check
+        return data
