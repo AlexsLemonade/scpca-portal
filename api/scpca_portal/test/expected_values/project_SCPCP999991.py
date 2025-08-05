5beeabbf4e255678ada71d364f002f0b97c48b9e
Nozomi Ichihara <nozomione.online@gmail.com>
2025-08-05 19:04:22 -0400

    adjuste tests values

--- api/scpca_portal/test/expected_values/project_SCPCP999991.py
+++ api/scpca_portal/test/expected_values/project_SCPCP999991.py
@@ -112,7 +112,7 @@ class Project_SCPCP999991:
             "is_cell_line": False,
             "is_xenograft": False,
             "multiplexed_with": [],
-            "sample_cell_count_estimate": 3419,
+            "sample_cell_count_estimate": 3424,
             "scpca_id": SCPCA_ID,
             "sex": "M",
             "seq_units": ["cell"],
