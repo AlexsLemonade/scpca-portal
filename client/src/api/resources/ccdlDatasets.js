6824d9c69f93f037bb06ec824123155b61cf80e9
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-08-06 10:54:55 -0400

    add ccdlDatasets route to client api index file

--- client/src/api/resources/ccdlDatasets.js
+++ client/src/api/resources/ccdlDatasets.js
@@ -4,7 +4,7 @@ import { request } from 'helpers/request'
 const ccdlDatasets = {
   get: (id, authorization = '') =>
     request(getAPIUrl(`datasets/${id}`), { authorization }), // token required for file downloads
-  list: (query) => request(getAPIUrl('datasets', query)),
+  list: (query) => request(getAPIUrl('datasets', query))
 }
 
 export default ccdlDatasets
