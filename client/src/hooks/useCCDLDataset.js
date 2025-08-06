604c9965c360339ebb76a3c438edb2ddabac3f4f
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-08-06 15:58:33 -0400

    rework useCCDLDataset hook to take into account new context

--- client/src/hooks/useCCDLDataset.js
+++ client/src/hooks/useCCDLDataset.js
@@ -1,25 +1,9 @@
-import { useEffect, useState } from 'react'
-import { api } from 'api'
+import { useContext } from 'react'
+import { CCDLDatasetContext } from 'contexts/CCDLDatasetContext'
 
-export const useCCDLDataset = (portalWide = false) => {
-  const [ccdlDatasets, setCCDLDatasets] = useState([])
-
-  // Set states for the portal metedata
-  useEffect(() => {
-    const getCCDLDatasets = async () => {
-      const resourceRequest = await api.ccdlDatasets.list({
-        ccdl_project_id__isnull: portalWide
-      })
-      const { results } = resourceRequest.response
-      setCCDLDatasets(results)
-    }
-
-    getCCDLDatasets()
-  }, [portalWide])
-
-  return {
-    ccdlDatasets
-  }
+export const useCCDLDataset = () => {
+  const { datasets } = useContext(CCDLDatasetContext)
+  return { datasets }
 }
 
 export default useCCDLDataset
