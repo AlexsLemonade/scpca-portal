8ad0db9958c9a48f1d5c0bbb649e030afaba9d6b
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-08-27 17:29:07 -0400

    improve error messages

--- api/scpca_portal/views/dataset.py
+++ api/scpca_portal/views/dataset.py
@@ -77,7 +77,7 @@ class DatasetViewSet(
         if token_id := self.request.META.get("HTTP_API_KEY"):
             token = APIToken.verify(token_id)
             if not token:
-                message = "Token header value is either invalid or inactive."
+                message = f"Token header value {token_id} is either invalid or inactive."
                 raise PermissionDenied({"message": message, "token_id": token_id})
 
             serializer_context.update({"token": token})
