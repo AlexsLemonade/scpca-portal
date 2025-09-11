5b4a353decf966bc553dee232a2c38caf049af97
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-09-11 12:24:19 -0400

    improve key name in portal wide datasets config

--- client/src/config/ccdlDatasetDownloadModal.js
+++ client/src/config/ccdlDatasetDownloadModal.js
@@ -1,15 +1,14 @@
 export const portalWideDatasets = {
   ALL_METADATA: {
-    modalTitleDataset: 'Downloading Portal-wide Sample Metadata'
+    title: 'Portal-wide Sample Metadata'
   },
   SINGLE_CELL_SINGLE_CELL_EXPERIMENT: {
-    modalTitleDataset:
-      'Downloading Portal-wide Data as SingleCellExperiment (R)'
+    title: 'Portal-wide Data as SingleCellExperiment (R)'
   },
   SINGLE_CELL_ANN_DATA: {
-    modalTitleDataset: 'Downloading Portal-wide Data as AnnData (Python)'
+    title: 'Portal-wide Data as AnnData (Python)'
   },
   SPATIAL_SINGLE_CELL_EXPERIMENT: {
-    modalTitleDataset: 'Downloading Portal-wide Spatial Data'
+    title: 'Portal-wide Spatial Data'
   }
 }
--- client/src/hooks/useCCDLDatasetDownloadModal.js
+++ client/src/hooks/useCCDLDatasetDownloadModal.js
@@ -17,7 +17,7 @@ export const useCCDLDatasetDownloadModal = (initialDatasets, isActive) => {
 
   const modalTitleAction = isDownloadReady ? 'Downloading' : 'Download'
   const modalTitleDataset =
-    portalWideDatasets[selectedDataset?.ccdl_name]?.modalTitleDataset
+    portalWideDatasets[selectedDataset?.ccdl_name]?.title
   const modalTitleResource = datasets[0].ccdl_project_id
     ? 'Project'
     : modalTitleDataset
