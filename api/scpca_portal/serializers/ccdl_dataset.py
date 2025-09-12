e16a4d734f3adcd9d97d2a12e62dd16d26c0205c
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-09-12 10:03:39 -0400

    add CCDLDatasetSerializer

---
+++ api/scpca_portal/serializers/ccdl_dataset.py
@@ -0,0 +1,58 @@
+from rest_framework import serializers
+
+from scpca_portal.models import Dataset
+
+
+class CCDLDatasetSerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
+    # it's necessary to rename data attr because serializer.Serializer needs it own data attr.
+    _data = serializers.JSONField(source="data", read_only=True)
+    email = serializers.EmailField(read_only=True, allow_null=True)
+    start = serializers.BooleanField(read_only=True)
+
+    format = serializers.CharField(read_only=True)
+    regenerated_from = serializers.UUIDField(read_only=True, allow_null=True)
+
+    data_hash = serializers.CharField(read_only=True, allow_null=True)
+    metadata_hash = serializers.CharField(read_only=True, allow_null=True)
+    readme_hash = serializers.CharField(read_only=True, allow_null=True)
+    combined_hash = serializers.CharField(read_only=True, allow_null=True)
+    current_data_hash = serializers.ReadOnlyField()
+    current_metadata_hash = serializers.ReadOnlyField()
+    current_readme_hash = serializers.ReadOnlyField()
+    current_combined_hash = serializers.SerializerMethodField(read_only=True)
+    is_hash_changed = serializers.ReadOnlyField()
+
+    includes_files_bulk = serializers.BooleanField(read_only=True)
+    includes_files_cite_seq = serializers.BooleanField(read_only=True)
+    includes_files_merged = serializers.BooleanField(read_only=True)
+    includes_files_multiplexed = serializers.BooleanField(read_only=True)
+
+    is_ccdl = serializers.BooleanField(read_only=True)
+    ccdl_name = serializers.CharField(read_only=True, allow_null=True)
+    ccdl_project_id = serializers.CharField(read_only=True, allow_null=True)
+    ccdl_modality = serializers.CharField(read_only=True, allow_null=True)
+
+    started_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_started = serializers.BooleanField(read_only=True)
+    pending_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_pending = serializers.BooleanField(read_only=True)
+    processing_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_processing = serializers.BooleanField(read_only=True)
+    succeeded_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_succeeded = serializers.BooleanField(read_only=True)
+    failed_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_failed = serializers.BooleanField(read_only=True)
+    failed_reason = serializers.CharField(read_only=True, allow_null=True)
+    expires_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_expired = serializers.BooleanField(read_only=True)
+    terminated_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    is_terminated = serializers.BooleanField(read_only=True)
+    terminated_reason = serializers.CharField(read_only=True, allow_null=True)
+
+    computed_file = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    def get_current_combined_hash(self, obj):
+        return Dataset.get_current_combined_hash(
+            obj.current_data_hash, obj.current_metadata_hash, obj.current_readme_hash
+        )
