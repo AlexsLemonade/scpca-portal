27a295ac074590d4530455bd48cddaff150ab44d
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-09-26 14:19:01 -0400

    update DatasetFileItems to account for project dataset permutations

--- client/src/components/DatasetFileItems.js
+++ client/src/components/DatasetFileItems.js
@@ -8,14 +8,9 @@ const Li = ({ children }) => (
   </Box>
 )
 
-export const DatasetFileItems = ({ dataset }) => {
+const PortalWideFileItems = ({ dataset }) => {
   return (
-    <Box
-      as="ul"
-      margin={{ top: '0', bottom: 'large' }}
-      pad={{ left: 'large' }}
-      style={{ listStyle: 'disc' }}
-    >
+    <>
       {dataset?.format === 'METADATA' ? (
         <Li>Sample metadata from all projects</Li>
       ) : (
@@ -25,6 +20,64 @@ export const DatasetFileItems = ({ dataset }) => {
           {dataset?.includes_files_bulk && <Li>Bulk RNA-Seq data</Li>}
           <Li>Project and Sample Metadata</Li>
         </>
+      )}
+    </>
+  )
+}
+
+const ProjectFileItems = ({ dataset }) => {
+  const {
+    format,
+    ccdl_modality: modality,
+    includes_files_bulk: hasBulk,
+    includes_files_cite_seq: hasCiteSeq,
+    includes_files_multiplexed: hasMultiplexed
+  } = dataset
+
+  let dataText = 'Single-cell data'
+  if (format === 'SINGLE_CELL_EXPERIMENT' && hasCiteSeq) {
+    dataText += ', CITE-seq data'
+  }
+
+  const singleCellText = `${dataText} as SingleCellExperiment (R)`
+  const anndataText = `${dataText} as AnnData (Python)`
+  const singleCellFormatText =
+    dataset?.format === 'SINGLE_CELL_EXPERIMENT' ? singleCellText : anndataText
+
+  const formatText =
+    modality === 'SINGLE_CELL' ? singleCellFormatText : 'Spatial data'
+
+  return (
+    <>
+      {dataset?.format !== 'METADATA' && (
+        <>
+          <Li>{formatText}</Li>
+          {hasMultiplexed && (
+            <Li>Multiplexed single-cell data as SingleCellExperiment (R)</Li>
+          )}
+          {hasBulk && <Li>Bulk RNA-Seq data</Li>}
+          {format === 'ANN_DATA' && hasCiteSeq && (
+            <Li>CITE-seq data as AnnData (Python)</Li>
+          )}
+        </>
+      )}
+      <Li>Project and Sample Metadata</Li>
+    </>
+  )
+}
+
+export const DatasetFileItems = ({ dataset }) => {
+  return (
+    <Box
+      as="ul"
+      margin={{ top: '0', bottom: 'large' }}
+      pad={{ left: 'large' }}
+      style={{ listStyle: 'disc' }}
+    >
+      {dataset?.ccdl_project_id ? (
+        <ProjectFileItems dataset={dataset} />
+      ) : (
+        <PortalWideFileItems dataset={dataset} />
       )}
     </Box>
   )
