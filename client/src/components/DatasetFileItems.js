47da074505fe2aa3def16a1e4ae8eac3ae7867b0
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-09-28 13:17:12 -0400

    refactor DatasetFileItems by abstracting outu logic into getCCDLDatasetFileItems helper

--- client/src/components/DatasetFileItems.js
+++ client/src/components/DatasetFileItems.js
@@ -1,72 +1,11 @@
 import React from 'react'
 import { Box } from 'grommet'
-import { getReadableFiles } from 'helpers/getReadable'
+import { getCCDLDatasetFileItems } from 'helpers/getCCDLDatasetFileItems'
 
-const Li = ({ children }) => (
-  <Box as="li" style={{ display: 'list-item' }}>
-    {children}
-  </Box>
-)
-
-const PortalWideFileItems = ({ dataset }) => {
-  return (
-    <>
-      {dataset?.format === 'METADATA' ? (
-        <Li>Sample metadata from all projects</Li>
-      ) : (
-        <>
-          <Li>{getReadableFiles(dataset?.ccdl_modality)}</Li>
-          {dataset?.includes_files_cite_seq && <Li>CITE-seq data</Li>}
-          {dataset?.includes_files_bulk && <Li>Bulk RNA-Seq data</Li>}
-          <Li>Project and Sample Metadata</Li>
-        </>
-      )}
-    </>
-  )
-}
-
-const ProjectFileItems = ({ dataset }) => {
-  const {
-    format,
-    ccdl_modality: modality,
-    includes_files_bulk: hasBulk,
-    includes_files_cite_seq: hasCiteSeq,
-    includes_files_multiplexed: hasMultiplexed
-  } = dataset
-
-  let dataText = 'Single-cell data'
-  if (format === 'SINGLE_CELL_EXPERIMENT' && hasCiteSeq) {
-    dataText += ', CITE-seq data'
-  }
-
-  const singleCellText = `${dataText} as SingleCellExperiment (R)`
-  const anndataText = `${dataText} as AnnData (Python)`
-  const singleCellFormatText =
-    dataset?.format === 'SINGLE_CELL_EXPERIMENT' ? singleCellText : anndataText
-
-  const formatText =
-    modality === 'SINGLE_CELL' ? singleCellFormatText : 'Spatial data'
-
-  return (
-    <>
-      {dataset?.format !== 'METADATA' && (
-        <>
-          <Li>{formatText}</Li>
-          {hasMultiplexed && (
-            <Li>Multiplexed single-cell data as SingleCellExperiment (R)</Li>
-          )}
-          {hasBulk && <Li>Bulk RNA-Seq data</Li>}
-          {format === 'ANN_DATA' && hasCiteSeq && (
-            <Li>CITE-seq data as AnnData (Python)</Li>
-          )}
-        </>
-      )}
-      <Li>Project and Sample Metadata</Li>
-    </>
-  )
-}
-
+// TODO: rename to CCDLDatasetFileItems as well as file name
 export const DatasetFileItems = ({ dataset }) => {
+  const items = getCCDLDatasetFileItems(dataset)
+
   return (
     <Box
       as="ul"
@@ -74,11 +13,11 @@ export const DatasetFileItems = ({ dataset }) => {
       pad={{ left: 'large' }}
       style={{ listStyle: 'disc' }}
     >
-      {dataset?.ccdl_project_id ? (
-        <ProjectFileItems dataset={dataset} />
-      ) : (
-        <PortalWideFileItems dataset={dataset} />
-      )}
+      {items.map((item) => (
+        <Box key={item} as="li" style={{ display: 'list-item' }}>
+          {item}
+        </Box>
+      ))}
     </Box>
   )
 }
