6e5488eda4391304dbad943225293ed3d6bade0a
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-10-01 10:42:52 -0400

    move JobError exception classes to own file

--- api/scpca_portal/exceptions/__init__.py
+++ api/scpca_portal/exceptions/__init__.py
@@ -3,3 +3,13 @@ from scpca_portal.exceptions.dataset_error import (
     DatasetLockedProjectError,
     DatasetMissingLibrariesError,
 )
+from scpca_portal.exceptions.job_error import (
+    JobError,
+    JobInvalidRetryStateError,
+    JobInvalidTerminateStateError,
+    JobSubmissionFailedError,
+    JobSubmitNotPendingError,
+    JobSyncNotProcessingError,
+    JobSyncStateFailedError,
+    JobTerminationFailedError,
+)
