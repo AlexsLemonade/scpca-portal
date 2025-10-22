6e34c647fb6c9896a79509c4e68a6b76ddbccb54
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-10-22 11:07:21 -0400

    reconstitute CCDLDatasetSerializer to derive from ModalSerializer instead of serializers.Serializer

--- api/scpca_portal/serializers/ccdl_dataset.py
+++ api/scpca_portal/serializers/ccdl_dataset.py
@@ -1,70 +1,65 @@
-from collections import OrderedDict
-
 from rest_framework import serializers
 
 from scpca_portal.models import Dataset
 from scpca_portal.serializers.computed_file import ComputedFileSerializer
 
 
-class CCDLDatasetSerializer(serializers.Serializer):
-    id = serializers.UUIDField(read_only=True)
-    # It's necessary to rename the data attr as serializer.Serializer needs its own data attr.
-    # The "_data" attr is renamed to "data" for output purposes in to_representation below.
-    _data = serializers.JSONField(source="data", read_only=True)
-    email = serializers.EmailField(read_only=True, allow_null=True)
-    start = serializers.BooleanField(read_only=True)
-    format = serializers.CharField(read_only=True)
-    regenerated_from = serializers.UUIDField(read_only=True, allow_null=True)
-
-    includes_files_bulk = serializers.BooleanField(read_only=True)
-    includes_files_cite_seq = serializers.BooleanField(read_only=True)
-    includes_files_merged = serializers.BooleanField(read_only=True)
-    includes_files_multiplexed = serializers.BooleanField(read_only=True)
-
-    is_ccdl = serializers.BooleanField(read_only=True)
-    ccdl_name = serializers.CharField(read_only=True, allow_null=True)
-    ccdl_project_id = serializers.CharField(read_only=True, allow_null=True)
-    ccdl_modality = serializers.CharField(read_only=True, allow_null=True)
-
-    started_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_started = serializers.BooleanField(read_only=True)
-    pending_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_pending = serializers.BooleanField(read_only=True)
-    processing_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_processing = serializers.BooleanField(read_only=True)
-    succeeded_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_succeeded = serializers.BooleanField(read_only=True)
-    failed_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_failed = serializers.BooleanField(read_only=True)
-    failed_reason = serializers.CharField(read_only=True, allow_null=True)
-    expires_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_expired = serializers.BooleanField(read_only=True)
-    terminated_at = serializers.DateTimeField(read_only=True, allow_null=True)
-    is_terminated = serializers.BooleanField(read_only=True)
-    terminated_reason = serializers.CharField(read_only=True, allow_null=True)
+class CCDLDatasetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dataset
+        fields = (
+            "id",
+            "data",
+            "email",
+            "start",
+            "format",
+            "regenerated_from",
+            "includes_files_bulk",
+            "includes_files_cite_seq",
+            "includes_files_merged",
+            "includes_files_multiplexed",
+            "is_ccdl",
+            "ccdl_name",
+            "ccdl_project_id",
+            "ccdl_modality",
+            "started_at",
+            "is_started",
+            "is_processing",
+            "succeeded_at",
+            "is_succeeded",
+            "failed_at",
+            "is_failed",
+            "failed_reason",
+            "expires_at",
+            "is_expired",
+            "terminated_at",
+            "is_terminated",
+            "terminated_reason",
+            "computed_file",
+        )
+        read_only_fields = fields
 
     computed_file = ComputedFileSerializer(read_only=True, many=False)
 
-    # Rename the "_data attr" to "data" for output json
-    def to_representation(self, instance):
-        instance_rep = super().to_representation(instance)
-        corrected_instance_rep = {
-            (k if k != "_data" else "data"): v for k, v in instance_rep.items()
-        }
-        return OrderedDict(corrected_instance_rep)
-
 
 class CCDLDatasetDetailSerializer(CCDLDatasetSerializer):
-    download_filename = serializers.SerializerMethodField(read_only=True)
-    download_url = serializers.SerializerMethodField(read_only=True)
-
-    def get_download_filename(self, obj):
-        dataset = Dataset.objects.filter(pk=obj.pk).first()
-        return dataset.download_filename
-
-    def get_download_url(self, obj):
-        dataset = Dataset.objects.filter(pk=obj.pk).first()
-        return dataset.download_url
+    class Meta(CCDLDatasetSerializer.Meta):
+        fields = (*CCDLDatasetSerializer.Meta.fields, "download_filename", "download_url")
+        extra_kwargs = {
+            "download_filename": {
+                "help_text": (
+                    "This will contain the download file's name. "
+                    "You must send a valid [token](#tag/token) in order to receive this."
+                )
+            },
+            "download_url": {
+                "help_text": (
+                    "This will contain a url to download the file. "
+                    "You must send a valid [token](#tag/token) "
+                    "for this attribute to be present in the response."
+                )
+            },
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
