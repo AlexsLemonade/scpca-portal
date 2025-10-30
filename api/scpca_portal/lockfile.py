30e8b1457ae93ae25962ee123afefaa6726e5969
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-10-30 15:19:26 -0400

    add get_is_locked_project and get_locked_project_ids functions to lockfile module

--- api/scpca_portal/lockfile.py
+++ api/scpca_portal/lockfile.py
@@ -9,6 +9,19 @@ from scpca_portal.models.original_file import OriginalFile
 logger = get_and_configure_logger(__name__)
 
 LOCKFILE_S3_KEY = "projects.lock"
+LOCKFILE_FILE_SUFFIX = "lock"
+
+
+def get_is_locked_project(
+    project_id: str, *, bucket: str = settings.AWS_S3_INPUT_BUCKET_NAME
+) -> bool:
+    project_lockfile = f"{project_id}.{LOCKFILE_FILE_SUFFIX}"
+    return s3.check_file_exists(project_lockfile, bucket=bucket)
+
+
+def get_locked_project_ids(*, bucket: str = settings.AWS_S3_INPUT_BUCKET_NAME) -> List[str]:
+    project_lockfile_paths = s3.list_files_by_suffix(LOCKFILE_FILE_SUFFIX, bucket=bucket)
+    return [path.stem for path in project_lockfile_paths]
 
 
 def get_lockfile_project_ids_with_file_check(
