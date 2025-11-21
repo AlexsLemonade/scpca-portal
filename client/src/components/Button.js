7d3b2568ec3376cb5069ccc3a177b645d4462bfd
David <davidsmejia@gmail.com>
2025-11-21 18:39:00 -0500

    use button props

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Box, Button as GrommetButton, Spinner } from 'grommet'
+import { Button as GrommetButton, Spinner } from 'grommet'
 import { useWaitForAsync } from 'hooks/useWaitForAsync'
 
-export const Button = ({ label, loading, onClick, ...props }) => {
+const LoadingSpinner = () => {
+  return (
+    <Spinner
+      border={[
+        { side: 'all', color: 'white', size: 'small' },
+        { side: 'right', color: 'transparent', size: 'small' },
+      ]}
+    />
+  )
+}
+
+export const Button = ({ label, loading = false, onClick = () => { }, ...props }) => {
   const [waiting, asyncOnClick] = useWaitForAsync(onClick)
   const disabled = waiting || props.disabled
-
-  const buttonLabel = loading ? (
-    <Box align="center">
-      <Spinner />
-    </Box>
-  ) : (
-    label
-  )
 
   return (
     <GrommetButton
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
-      label={buttonLabel}
+      icon={loading ? <LoadingSpinner />: undefined}
+      label={loading ? undefined : label}
       onClick={asyncOnClick}
       disabled={disabled}
       loading={loading}
