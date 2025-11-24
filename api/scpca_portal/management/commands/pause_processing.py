c407ab4fc09cf7443052d4054a403aae69494494
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-11-24 07:56:47 -0500

    improve logs

--- api/scpca_portal/management/commands/pause_processing.py
+++ api/scpca_portal/management/commands/pause_processing.py
@@ -17,11 +17,13 @@ class Command(BaseCommand):
         self.pause_processing()
 
     def pause_processing(self):
-        terminated_jobs = Job.terminate_processing(reason="Processing paused on re-deploy.")
+        termination_reason = "Processing paused via pause_processing command."
+        terminated_jobs = Job.terminate_processing(reason=termination_reason)
+
         retry_jobs = Job.create_retry_jobs(terminated_jobs)
 
         retry_job_count = len(retry_jobs)
         logger.info(
             f"{retry_job_count} processing job{pluralize(retry_job_count)} were paused "
-            "and added to the pending queue before deploy."
+            "and added to the pending queue."
         )
