42e361fbeec2b4a3e828e996bbc328a7fc0775a0
David <davidsmejia@gmail.com>
2025-12-04 13:50:11 -0500

    refactor dataset processing to use dataset_job_processor

--- api/scpca_portal/management/commands/process_dataset.py
+++ api/scpca_portal/management/commands/process_dataset.py
@@ -4,10 +4,7 @@ from argparse import BooleanOptionalAction
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from scpca_portal import notifications, utils
-from scpca_portal.enums import JobStates
-from scpca_portal.exceptions import DatasetLockedProjectError, DatasetMissingLibrariesError
-from scpca_portal.models import Job
+from scpca_portal.processors import DatasetJobProcessor
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -34,29 +31,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         self.process_dataset(**kwargs)
 
-    def process_dataset(self, job_id: str, **kwargs) -> None:
-        utils.create_data_dirs()
-
-        job = Job.objects.filter(id=job_id).first()
-        if not job:
-            logger.error(f"{job_id} does not exist.")
-            return
-        if not job.dataset:
-            logger.error(f"{job_id} does not have a dataset.")
-            return
-
-        try:
-            job.process_dataset_job()
-            job.apply_state(JobStates.SUCCEEDED)
-        except (DatasetLockedProjectError, DatasetMissingLibrariesError) as e:
-            # only locked datasets should generate retry jobs
-            # datasets without libraries are malformed and should not be retried
-            if isinstance(e, DatasetLockedProjectError):
-                job.create_retry_job()
-            job.apply_state(JobStates.FAILED)
-
-        job.save()
-        if job.dataset:  # TODO: Remove after the dataset release
-            job.dataset.save()
-            if job.dataset.email:
-                notifications.send_dataset_file_completed_email(job)
+    def process_dataset(self, job_id: str, update_s3: bool, **kwargs) -> None:
+        processor = DatasetJobProcessor(job_id)
+        processor.update_s3 = update_s3
+        processor.run()
