01f69ec640fd00882785c75d80cb2d1ea7ae15e7
David <davidsmejia@gmail.com>
2025-12-05 21:41:34 -0500

    remove prevent_destroy entirely

--- infrastructure/ses.tf
+++ infrastructure/ses.tf
@@ -4,26 +4,14 @@
 
 resource "aws_ses_domain_identity" "scpca_portal" {
   domain = local.ses_domain
-
-  lifecycle {
-    prevent_destroy = var.stage != "dev"
-  }
 }
 
 resource "aws_ses_domain_dkim" "scpca_portal" {
   domain = aws_ses_domain_identity.scpca_portal.domain
-
-  lifecycle {
-    prevent_destroy = var.stage != "dev"
-  }
 }
 
 resource "aws_ses_domain_mail_from" "scpca_portal" {
   domain                 = aws_ses_domain_identity.scpca_portal.domain
   mail_from_domain       = "mail.${local.ses_domain}"
   behavior_on_mx_failure = "UseDefaultValue"
-
-  lifecycle {
-    prevent_destroy = var.stage != "dev"
-  }
 }
