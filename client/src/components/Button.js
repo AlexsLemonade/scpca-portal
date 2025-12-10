b1c512a6ad15f14e14a823c5cdd93256219bf012
Nozomi Ichihara <nozomione.online@gmail.com>
2025-12-10 12:30:02 -0500

    (edit) upadte shared/Button to retain the original button width while loading

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button as GrommetButton, Spinner } from 'grommet'
+import { Box, Button as GrommetButton, Spinner, Text } from 'grommet'
 import { useWaitForAsync } from 'hooks/useWaitForAsync'
 
 const LoadingSpinner = () => {
@@ -26,8 +26,23 @@ export const Button = ({
     <GrommetButton
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
-      icon={loading ? <LoadingSpinner /> : undefined}
-      label={loading ? undefined : label}
+      label={
+        <Box style={{ position: 'relative' }}>
+          <Text color={loading ? 'transparent' : 'inherit'}>{label}</Text>
+          {loading && (
+            <Box
+              style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)'
+              }}
+            >
+              <LoadingSpinner />
+            </Box>
+          )}
+        </Box>
+      }
       onClick={asyncOnClick}
       disabled={disabled}
       loading={loading}
