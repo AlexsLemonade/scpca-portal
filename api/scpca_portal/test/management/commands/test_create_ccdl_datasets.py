9765d55fdc69e5483e5bb7723ccc6100366caa80
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2025-12-24 09:36:59 -0500

    simplify test_ignore_hash test and remove data setup function

--- api/scpca_portal/test/management/commands/test_create_ccdl_datasets.py
+++ api/scpca_portal/test/management/commands/test_create_ccdl_datasets.py
@@ -1,61 +1,26 @@
 from unittest.mock import patch
 
-from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
 
-from scpca_portal import loader, metadata_parser
 from scpca_portal.enums.job_states import JobStates
-from scpca_portal.models import Dataset, Job
 from scpca_portal.test.factories import JobFactory
 
 
 class TestCreateCCDLDatasets(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        bucket = settings.AWS_S3_INPUT_BUCKET_NAME
-        call_command("sync_original_files", bucket=bucket)
+    @patch("scpca_portal.models.Job.submit_ccdl_datasets")
+    def test_ignore_hash(self, mock_submit_ccdl_datasets):
+        mock_submit_ccdl_datasets.return_value = [], []
+        with patch(
+            "scpca_portal.models.Dataset.create_or_update_ccdl_datasets", return_value=([], [])
+        ) as mock_create_or_update_ccdl_datasets:
+            ignore_hash = False
+            call_command("create_ccdl_datasets", ignore_hash=ignore_hash)
+            mock_create_or_update_ccdl_datasets.assert_called_with(ignore_hash=ignore_hash)
 
-        loader.download_projects_metadata()
-        project_ids = metadata_parser.get_projects_metadata_ids(bucket=bucket)
-
-        loader.download_projects_related_metadata(project_ids)
-        for project_metadata in metadata_parser.load_projects_metadata(project_ids):
-            loader.create_project(
-                project_metadata,
-                submitter_whitelist={"scpca"},
-                input_bucket_name=bucket,
-                reload_existing=True,
-                update_s3=False,
-            )
-
-    @patch("scpca_portal.batch.submit_job")
-    def test_ignore_hash(self, mock_batch_submit_job):
-        mock_batch_job_id = "MOCK_JOB_ID"
-        mock_batch_submit_job.return_value = mock_batch_job_id
-
-        # There are 21 total datasets created (with one job per created dataset)
-        #     CCDL DATASET TYPE              Total   Projects   Portal Wide
-        #   - ALL_METADATA                   4       3          Yes
-        #   - SINGLE_CELL_SCE                4       3          Yes
-        #   - SINGLE_CELL_SCE_NO_MULTIPLEXED 1       1          No
-        #   - SINGLE_CELL_SCE_MERGED         3       2          Yes
-        #   - SINGLE_CELL_ANN_DATA           4       3          Yes
-        #   - SINGLE_CELL_ANN_DATA_MERGED    3       2          Yes
-        #   - SPATIAL_SCE                    2       1          Yes
-        call_command("create_ccdl_datasets")
-        self.assertEqual(Dataset.objects.count(), 21)
-        self.assertEqual(Job.objects.count(), 21)
-
-        # call command again to assert that no new jobs have been created
-        call_command("create_ccdl_datasets")
-        self.assertEqual(Dataset.objects.count(), 21)
-        self.assertEqual(Job.objects.count(), 21)
-
-        # call command again with ignore_hash which should create new jobs
-        call_command("create_ccdl_datasets", ignore_hash=True)
-        self.assertEqual(Dataset.objects.count(), 21)
-        self.assertEqual(Job.objects.count(), 42)
+            ignore_hash = True
+            call_command("create_ccdl_datasets", ignore_hash=ignore_hash)
+            mock_create_or_update_ccdl_datasets.assert_called_with(ignore_hash=ignore_hash)
 
     @patch("scpca_portal.models.Job.submit_ccdl_datasets")
     @patch("scpca_portal.models.Dataset.create_or_update_ccdl_datasets")
