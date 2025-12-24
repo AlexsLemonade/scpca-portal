63cc3bf3a7a99893ff780c43a0eee0dd0d56a141
David <davidsmejia@gmail.com>
2025-12-24 15:47:28 -0500

    lint

--- infrastructure/deploy.py
+++ infrastructure/deploy.py
@@ -148,7 +148,7 @@ def create_ssh_private_key_file():
         private_key_file.write(os.environ["SSH_PRIVATE_KEY"])
 
 
-    with open(PRIVATE_KEY_FILE_PATH, 'r') as file:
+    with open(PRIVATE_KEY_FILE_PATH, "r") as file:
         line_count = sum(1 for line in file)
         print(f"PRIVATE KEY HAS {line_count} lines.")
 
