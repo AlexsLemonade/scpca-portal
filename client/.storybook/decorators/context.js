f6e2ae8792ace4af2e8238cc3d4ddb56513b74af
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-02-06 09:55:28 -0500

    remove storybook stories with no longer available component and context references

--- client/.storybook/decorators/context.js
+++ client/.storybook/decorators/context.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { ScPCAPortalContextProvider } from 'contexts/ScPCAPortalContext'
-import { DatasetSamplesTableContextProvider } from 'contexts/DatasetSamplesTableContext'
 import { NotificationContextProvider } from 'contexts/NotificationContext'
 
 const Context = Story => <ScPCAPortalContextProvider>
   <NotificationContextProvider>
-    <DatasetSamplesTableContextProvider>
       <Story />
-    </DatasetSamplesTableContextProvider>
   </NotificationContextProvider>
 </ScPCAPortalContextProvider>;
 
