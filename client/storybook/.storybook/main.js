15d39a69f6abce0e04e744b9bf21b9d308a5a2a8
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-02-09 10:43:32 -0500

    convert storybook service to yarn workspace

--- client/storybook/.storybook/main.js
+++ client/storybook/.storybook/main.js
@@ -7,26 +7,44 @@ const envVars = {
 
 module.exports = {
   addons: [
-    '@storybook/addon-storysource',
-    '@storybook/addon-links',
+    // '@storybook/addon-storysource',
+    // '@storybook/addon-links',
     '@storybook/addon-essentials'
   ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
-  staticDirs: ['./../public'],
+  staticDirs: ['../../public'],
   stories: ['./stories/**/*.stories.@(js|mdx)'],
 
   webpackFinal: async (config) => {
     // Add src to imports (so this works with app webpack config)
-    config.resolve.modules.push(path.resolve(__dirname, './../src'))
-    config.resolve.alias['data'] = path.resolve(__dirname, './data')
+    config.resolve.modules.push(path.resolve(__dirname, '../../src'))
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      data: path.resolve(__dirname, './data'),
+      '@client': path.resolve(__dirname, '../../src'),
+      components: path.resolve(__dirname, '../../src/components'),
+      contexts: path.resolve(__dirname, '../../src/contexts'),
+      hooks: path.resolve(__dirname, '../../src/hooks')
+    }
 
     config.resolve.fallback = {
       ...(config.resolve.fallback || {}),
       zlib: false
     }
+
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    })
 
     // Add env vars for helpers
     config.plugins.forEach((plugin) => {
