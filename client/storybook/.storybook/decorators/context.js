a35910d8357ca3b4991f71ca30aea60906a8b4ac
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-02-10 17:10:36 -0500

    set datasets in local storage in storybook context

--- client/storybook/.storybook/decorators/context.js
+++ client/storybook/.storybook/decorators/context.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { ScPCAPortalContextProvider } from 'contexts/ScPCAPortalContext'
 import { NotificationContextProvider } from 'contexts/NotificationContext'
 import { MyDatasetContextProvider } from 'contexts/MyDatasetContext'
+import dataset from '../data/user_dataset.json'
 
 const Context = Story => {
+  if (typeof window !== 'undefined') {
+    window.localStorage.setItem('dataset', JSON.stringify(dataset))
+    window.localStorage.setItem('user-format', JSON.stringify(dataset.format))
+    window.localStorage.setItem('datasets', JSON.stringify([dataset.id]))
+  }
+
   return (
     <ScPCAPortalContextProvider>
       <MyDatasetContextProvider>
