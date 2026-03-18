5e40b196a453d022dd036b0356a200ab1dc2e8ca
Nozomi Ichihara <nozomione.online@gmail.com>
2026-03-18 18:37:07 -0400

    add a new API Example script, download-user-datset-with-samples-by-diagnosis

---
+++ api-examples/download-user-dataset-with-samples-by-diagnosis.py
@@ -0,0 +1,246 @@
+import json
+import os
+import urllib.parse
+from pprint import pp
+from urllib import request
+
+# STEP 1: SET UP BASE API CONFIGS
+
+# NOTE: UPDATE EMAIL OR SCRIPT WILL NOT WORK
+# By adding your email, you agree to the terms of service and privacy policy:
+# - Terms of Service: https://scpca.alexslemonade.org/terms-of-use
+# - Privacy Policy: https://scpca.alexslemonade.org/privacy-policy
+API_TOKEN_EMAIL = "user@example.com"  # NOTE: REPLACE THIS WITH A VALID EMAIL OR IT WILL ERROR OUT
+
+# Set this to True if you'd like to start processing your dataset for file download
+PROCESS_DATASET = False
+
+# Set this to True if you'd like for requested files to be downloaded at the end of the script
+# NOTE: Files are available for download only after dataset processing is complete (this may take some time).
+# If enabled, comment out all code except the "3. Create Your Dataset" section to avoid recreating and reprocessing the dataset.
+INITIATE_DOWNLOAD = False
+
+# This is where we will save the token for future calls
+API_TOKEN_FILENAME = ".token"
+
+if not API_TOKEN_EMAIL or "example" in API_TOKEN_EMAIL:
+    raise Exception("Please accept terms by adding a valid email for API_TOKEN_EMAIL")
+
+# This is all boilerplate to make it easier to make API calls
+# API_RESOURCES is pulled from the list shown on https://api.scpca.alexslemonade.org/v1/
+API_BASE = "http://localhost:8000/v1/" # TODO: Temporaily points to localhost for testing
+API_RESOURCES = [
+    "ccdl-datasets",
+    "computed-files",
+    "datasets",
+    "projects",
+    "samples",
+    "tokens",
+    "project-options",
+]
+API_ENDPOINTS = {resource: f"{API_BASE}{resource}/" for resource in API_RESOURCES}
+
+BASE_HEADERS = {"Accept": "application/json"}
+BASE_PARAMS = {"limit": 2000}  # Let's ignore pagination for now
+
+
+# STEP 2: SET UP API HELPER
+
+
+def request_api(
+    resource: str,
+    id: str | int | None = None,
+    *,  # Use the following as keyword arguments to prevent confusion
+    query: dict = {},
+    body: dict | None = None,
+    token: str | None = None,
+    method: str = "GET",
+) -> dict:
+    """
+    Generic API Wrapper.
+    Accepts:
+     - resource: A string that is defined in API_RESOURCES. Ex: "samples"
+     - id: A string, int, or undefined: Id of resource. Ex: project.scpca_id
+     - query: A query parameters as a dict to be urlencoded to tack onto the request.
+     - body: A dict that is the payload of your request.
+     - token: A authenticated API token to add to the request headers.
+     - method: HTTP Method: ex: "GET" or "POST" - defaults to "GET"
+    """
+    # Only continue if trying to access a known API resource
+    try:
+        resource_url = API_ENDPOINTS[resource]
+    except KeyError:
+        print(f"{resource} is not an API resource, options are {API_RESOURCES}")
+
+    # If ID is passed, tack it to end of request
+    if id is not None:
+        resource_url += f"{id}/"
+
+    if not id or query:  # Adding not id to always list out all items without pagination
+        # Convert lists to comma-separated strings
+        formatted_query = {}
+        for key, value in query.items():
+            if isinstance(value, list):
+                formatted_query[key] = ",".join(str(v) for v in value)
+            else:
+                formatted_query[key] = value
+
+        query_params = BASE_PARAMS.copy()
+        query_params.update(formatted_query)
+        resource_url += f"?{urllib.parse.urlencode(query_params)}"
+
+    # Convert body to JSON if provided
+    data = None
+    if body:
+        data = json.dumps(body).encode("utf-8")
+
+    # If passed, set headers
+    headers = BASE_HEADERS.copy()
+    if body:
+        headers["Content-Type"] = "application/json"
+    if token:
+        headers["API-KEY"] = token
+
+    print(f"{method}: {resource_url}")
+    if headers:
+        print("Headers:")
+        pp(headers)
+    if data:
+        print("Payload")
+        pp(body)
+
+    httprequest = request.Request(resource_url, data=data, headers=headers, method=method)
+
+    with request.urlopen(httprequest) as response:
+        resp = json.loads(response.read().decode())
+
+    return resp
+
+
+# STEP 3: DATASET DOWNLOAD
+
+# 1. Authenticate API TOKEN
+API_TOKEN = None
+# Here we are creating a token using the above helper method.
+# This also will save the file locally to API_TOKEN_FILENAME to use for future calls.
+# Please try to re-use your tokens and don't create a new one for every request.
+if not os.path.isfile(API_TOKEN_FILENAME):
+    print(f"Fetching token with {API_TOKEN_EMAIL}")
+    # This is the payload that you need to send to /tokens to get an active API_TOKEN
+    API_TOKEN_BODY = {"email": API_TOKEN_EMAIL, "is_activated": True}
+    try:
+        token_response = request_api("tokens", body=API_TOKEN_BODY, method="POST")
+    except Exception as e:
+        print("ERROR: The following error occurred while trying to create an API token:")
+        print(f"ERROR: {e}")
+        exit()
+
+    token = dict(token_response)
+    API_TOKEN = token.get("id")
+
+    if API_TOKEN:
+        print(f"Saving token to {API_TOKEN_FILENAME}")
+        with open(API_TOKEN_FILENAME, "w") as f:
+            f.write(API_TOKEN)
+else:
+    with open(API_TOKEN_FILENAME, "r") as f:
+        print("Using existing token")
+        API_TOKEN = f.readlines()[0].strip()
+
+# 2. Prepare Your Dataset
+# Set a data format (SINGLE_CELL_EXPERIMENT or ANN_DATA)
+data_format = "ANN_DATA"  # Required upon dataset creation
+
+
+# SAMPLES
+print(f"See available diagnoses on the portal by querying project-options: {API_BASE}/project-options")
+# Let's query samples in ANN_DATA format for the specified diagnosis
+# NOTE: The includes_anndata flag can be omitted for SINGLE_CELL_EXPERIMENT
+query = {"diagnoses": "Neuroblastoma", "has_single_cell_data": True, "includes_anndata": True}
+
+queried_samples = request_api("samples", query=query).get("results", [])
+
+print(f"Found {len(queried_samples)} samples for query:")
+pp(query)
+
+# Let's build your dataset
+dataset = {
+    "format": data_format,
+    "data": {},
+    "start": PROCESS_DATASET,  # Set True to process your dataset immediately
+    "email": API_TOKEN_EMAIL,  # Required for email notification
+}
+
+# Let's populate a data dictionary from the queried samples
+project_ids = set() # For querying the projects resource later for bulk data
+
+for sample in queried_samples:
+    project_id = sample["project"]
+    sample_id = sample["scpca_id"]
+   
+    # Initialize the project data 
+    if project_id not in dataset["data"]:
+        dataset["data"][project_id] = {
+            "SINGLE_CELL": set(),
+            "SPATIAL": set(),
+            "includes_bulk": False
+        }
+   
+    project_ids.add(project_id)
+    
+    if sample["has_single_cell_data"]:
+        dataset["data"][project_id]["SINGLE_CELL"].add(sample_id)
+        
+    if sample["has_spatial_data"]:
+        dataset["data"][project_id]["SPATIAL"].add(sample_id)
+
+# Fetch the projects to include bulk data if available 
+print(f"Fetching {len(project_ids)} projects fonud in the queried samples:")
+for project_id in project_ids:
+    if queried_project := request_api("projects", query={"scpca_id": project_id}).get("results", []):
+        dataset["data"][project_id]["includes_bulk"] = queried_project[0]["has_bulk_rna_seq"]
+
+# Lastly, sort and covert the sample ID sets to lists
+for project_data in dataset["data"].values():
+    project_data["SINGLE_CELL"] = sorted(project_data["SINGLE_CELL"])
+    project_data["SPATIAL"] = sorted(project_data["SPATIAL"])
+
+# 3. Create Your Dataset
+# See https://api.staging.scpca.alexslemonade.org/docs/swagger/#/datasets/datasets_create
+
+# DATASETS
+dataset = request_api(
+    "datasets",
+    body=dataset,
+    token=API_TOKEN,  # Required for dataset processing
+    method="POST",
+)
+
+if PROCESS_DATASET:
+    # You'll receive a download link via email once your dataset is processed.
+    print(f"Check your email {API_TOKEN_EMAIL} for the dataset download notification.")
+    # NOTE: You'll need to accept terms again when downloading your dataset via the email link in a browser.
+else:
+    # You can view your dataset via our public API
+    print(f"Your dataset has been created: {API_BASE}datasets/{dataset["id"]}")
+
+# 4. (Optional) Download Your Dataset
+# See https://api.staging.scpca.alexslemonade.org/docs/swagger/#/ccdl-datasets/ccdl_datasets_retrieve
+# NOTE: Instead of using the email link, you can download the processed dataset directly via the API.
+
+if INITIATE_DOWNLOAD:
+    dataset_id = dataset[
+        "id"
+    ]  # Add your processed dataset ID here (also available via the email link)
+
+    processed_dataset = request_api(
+        "datasets", id=dataset_id, token=API_TOKEN  # Required for dataset download
+    )
+
+    download_url = processed_dataset["download_url"]
+    print(f"Signed Download URL for your dataset {dataset_id}")
+    print(f"Downloading: {download_url}")
+
+    request.urlretrieve(download_url, processed_dataset["s3_key"])
+    print(f"Finished Downloading: {download_url}")
+
