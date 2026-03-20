c81191dc66afa9b1643c2b7cd7dd2470e0162919
Nozomi Ichihara <nozomione.online@gmail.com>
2026-03-20 13:57:31 -0400

    (edit) import the readable dictionaties for CCDL file items directly into getCCDLDatasetFileItems, and remove the getReadableCCDLFileItems method from the getReadable helper file (co-located it within the getCCDLDatasetFileItems file instead)

--- client/src/helpers/getCCDLDatasetFileItems.js
+++ client/src/helpers/getCCDLDatasetFileItems.js
@@ -1,9 +1,28 @@
-import { getReadable, getReadableCCDLFileItems } from 'helpers/getReadable'
+import { getReadable } from 'helpers/getReadable'
+import { readableCCDLFileItems, readableFiles } from 'config/readableNames'
+
+const readableFileDict = { ...readableCCDLFileItems, ...readableFiles }
+
+const keys = {
+  SINGLE_CELL: 'SINGLE_CELL_DATA',
+  SPATIAL: 'SPATIAL_DATA',
+  MULTIPLEXED_DATA: 'MULTIPLEXED_DATA',
+  BULK_DATA: 'BULK_DATA',
+  CITE_SEQ_DATA: 'CITE_SEQ_DATA',
+  PORTAL_WIDE_METADATA: 'PORTAL_WIDE_METADATA',
+  PROJECT_METADATA: 'PROJECT_METADATA'
+}
+
+const getReadableFile = (key) => {
+  const fileItem = readableFileDict[key]
+  if (!fileItem) {
+    console.error(`Key ${key} is not present in readable file items for CCDL`)
+  }
+  return fileItem
+}
 
 // takes a dataset and returns an array of readable file items
 export const getCCDLDatasetFileItems = (dataset) => {
-  const items = []
-
   const {
     ccdl_project_id: projectId,
     format,
@@ -13,48 +32,51 @@ export const getCCDLDatasetFileItems = (dataset) => {
     includes_files_multiplexed: hasMultiplexed
   } = dataset
 
+  const items = []
+
   const isMetadataDataset = format === 'METADATA'
   const portalWideMetadataOnly = isMetadataDataset && !projectId
   const combinedCiteSeqFile = hasCiteSeq && format === 'SINGLE_CELL_EXPERIMENT'
-  const seperateCiteSeqFile = hasCiteSeq && format === 'ANN_DATA'
+  const separateCiteSeqFile = hasCiteSeq && format === 'ANN_DATA'
 
+  const metadataKey = portalWideMetadataOnly
+    ? keys.PORTAL_WIDE_METADATA
+    : keys.PROJECT_METADATA
+
+  const readableModality = keys[modality] ? getReadableFile(keys[modality]) : ''
+  const readableFormat = getReadable(format)
+
+  // Modality file items
   if (modality === 'SINGLE_CELL') {
     const modalityItem = combinedCiteSeqFile
-      ? `${getReadableCCDLFileItems(
-          'SINGLE_CELL_DATA'
-        )}, ${getReadableCCDLFileItems('CITE_SEQ_DATA')}`
-      : getReadableCCDLFileItems('SINGLE_CELL_DATA')
-    items.push(`${modalityItem} as ${getReadable(format)}`)
+      ? `${readableModality}, ${getReadableFile(keys.CITE_SEQ_DATA)}`
+      : readableModality
+    items.push(`${modalityItem} as ${readableFormat}`)
   }
-
   if (modality === 'SPATIAL') {
     // exception to the rule: spaceranger as files is always one word lower
-    items.push(`${getReadableCCDLFileItems('SPATIAL_DATA')} as spaceranger`)
+    items.push(`${readableModality} as spaceranger`)
   }
 
-  if (hasMultiplexed && !isMetadataDataset) {
-    items.push(
-      `${getReadableCCDLFileItems('MULTIPLEXED_DATA')} as ${getReadable(
-        format
-      )}`
-    )
+  // Other modality/data file items
+  if (!isMetadataDataset) {
+    if (hasMultiplexed) {
+      items.push(
+        `${getReadableFile(keys.MULTIPLEXED_DATA)} as ${readableFormat}`
+      )
+    }
+    if (hasBulk) {
+      items.push(getReadableFile(keys.BULK_DATA))
+    }
   }
 
-  if (hasBulk && !isMetadataDataset) {
-    items.push(getReadableCCDLFileItems('BULK_DATA'))
+  // Cite-seq for AnnData
+  if (separateCiteSeqFile) {
+    items.push(`${getReadableFile(keys.CITE_SEQ_DATA)} as ${readableFormat}`)
   }
 
-  if (seperateCiteSeqFile) {
-    items.push(
-      `${getReadableCCDLFileItems('CITE_SEQ_DATA')} as ${getReadable(format)}`
-    )
-  }
-
-  const metadataItem = portalWideMetadataOnly
-    ? getReadableCCDLFileItems('PORTAL_WIDE_METADATA')
-    : getReadableCCDLFileItems('PROJECT_METADATA')
-
-  items.push(metadataItem)
+  // Metadata file items
+  items.push(getReadableFile(metadataKey))
 
   return items
 }
--- client/src/helpers/getReadable.js
+++ client/src/helpers/getReadable.js
@@ -1,23 +1,10 @@
-import {
-  readableNames,
-  readableFiles,
-  readableCCDLFileItems
-} from 'config/readableNames'
+import { readableNames, readableFiles } from 'config/readableNames'
 
 export const getReadable = (key, dict = readableNames) => dict[key] || key
 
-// Exclusively used for CCDL datasets
-export const getReadableCCDLFileItems = (key) => {
-  const value = readableCCDLFileItems[key] || readableFiles[key]
-  if (!value) {
-    console.error(`Key ${key} is not present in readableCCDLFileItems`)
-  }
-  return value
-}
-
 export const getReadableFiles = (key) => readableFiles[key] || getReadable(key)
 
-// Exclusively used to append 'Data' to the SPATIAL moality in the UI
+// Exclusively used to append 'Data' to the SPATIAL modality in the UI
 export const getReadableModality = (modality) =>
   `${getReadable(modality)}${modality === 'SPATIAL' ? ' Data' : ''}`
 
