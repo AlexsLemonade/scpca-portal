4c442702ccba8fce8ee2cc82402a2bc050f1918a
Nozomi Ichihara <nozomione.online@gmail.com>
2026-03-27 16:50:49 -0400

    (edit) refactor getReadable and translations, which stores keys, values, fileKeys, fileValues dictionaries (previously readableNames dictionaries)

--- client/src/helpers/getReadable.js
+++ client/src/helpers/getReadable.js
@@ -1,11 +1,8 @@
-import { readableNames, readableFiles } from 'config/readableNames'
+import { values } from 'config/translations'
 
-export const getReadable = (key, dict = readableNames) => dict[key] || key
-
-export const getReadableFiles = (key) => readableFiles[key] || getReadable(key)
-
-// Exclusively used to append 'Data' to the SPATIAL modality in the UI
-export const getReadableModality = (modality) =>
-  `${getReadable(modality)}${modality === 'SPATIAL' ? ' Data' : ''}`
+export const getReadable = (key, override = {}, base = values) => {
+  const translation = { ...base, ...override }
+  return translation[key] || key
+}
 
 export default getReadable
