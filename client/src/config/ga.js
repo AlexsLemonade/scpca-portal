bc85105beb7093ef97a0d58df5a4c9b86ce18332
David <davidsmejia@gmail.com>
2026-04-06 17:56:28 -0400

    add dataset tracking logic

--- client/src/config/ga.js
+++ client/src/config/ga.js
@@ -2,6 +2,16 @@
 // each key is a unique function name describing the event
 // the value is the function arguments to be sent to google
 
+
+const getDatasetDownloadEventName = (dataset) => {
+
+  if (dataset.ccdl_name) {
+    return `${dataset.ccdl_project_id || "Portal Wide"} - ${dataset.ccdl_name}`
+  }
+
+  return "User Dataset"
+}
+
 export const events = {
   donate: () => ['Donate', 'Clicked', window.location.href],
   filterChange: (filter, change) => [
@@ -9,5 +19,6 @@ export const events = {
     `${change} ${filter}`,
     window.location
   ],
-  download: (project, sample) => ['Download', 'Downloaded', sample || project]
+  download: (project, sample) => ['Download', 'Downloaded', sample || project],
+  dataset: (dataset) => ['Download', 'Downloaded', getDatasetDownloadEventName(dataset)]
 }
