36f14c77730d936e1cfe92f3c5ec62e4ea104442
Nozomi Ichihara <nozomione.online@gmail.com>
2026-04-15 17:21:17 -0400

    (clean up) remove unused responsive

--- client/src/components/CCDLDatasetDownloadOption.js
+++ client/src/components/CCDLDatasetDownloadOption.js
@@ -5,7 +5,6 @@ import { DatasetFileItems } from 'components/DatasetFileItems'
 import { DatasetCopyLinkButton } from 'components/DatasetCopyLinkButton'
 import { WarningMultiplexedSamples } from 'components/WarningMultiplexedSamples'
 import { WarningMergedObjects } from 'components/WarningMergedObjects'
-import { useResponsive } from 'hooks/useResponsive'
 import { formatBytes } from 'helpers/formatBytes'
 import { useCCDLDatasetDownloadModalContext } from 'hooks/useCCDLDatasetDownloadModalContext'
 
@@ -14,8 +13,6 @@ export const CCDLDatasetDownloadOption = () => {
     useCCDLDatasetDownloadModalContext()
 
   const downloadLabel = 'Download Project'
-
-  const { responsive } = useResponsive()
 
   return (
     <Grid
