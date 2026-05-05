db3cf79b7cd7a3e16fbf8df6181fcc6ec5b8bbfe
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-05-05 12:29:14 -0400

    clean up syncing file paths

--- infrastructure/api-configuration/certbot_renew_deploy_hook.sh
+++ infrastructure/api-configuration/certbot_renew_deploy_hook.sh
@@ -8,15 +8,15 @@ echo "$(date): Cert renewed. Syncing cert with S3 and reloading Ngninx." >> "$LO
 # Add the nginx.conf file that certbot setup to the zip dir.
 cp /etc/nginx/nginx.conf /etc/letsencrypt/
 
-cd /etc/letsencrypt/ || exit
-sudo zip -r ../letsencryptdir.zip "../$(basename "$PWD")"
+cd /etc/ || exit
+sudo zip -r letsencryptdir.zip letsencrypt/
 
 # Cleanup the extra copy.
 rm /etc/letsencrypt/nginx.conf
 
 # Sync with S3
-aws s3 cp /etc/letsencryptdir.zip "s3://${SCPCA_PORTAL_CERT_BUCKET}/"
-rm /etc/letsencryptdir.zip
+aws s3 cp letsencryptdir.zip "s3://${SCPCA_PORTAL_CERT_BUCKET}/"
+rm letsencryptdir.zip
 
 systemctl reload nginx
 echo "$(date): Syncing complete and Nginx reloaded." >> "$LOG"
