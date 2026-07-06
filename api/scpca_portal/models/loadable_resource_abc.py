58054864f676958f4a08f98c911f8d7e703e59d8
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-07-06 12:56:55 -0400

    add current hash property method

--- api/scpca_portal/models/loadable_resource_abc.py
+++ api/scpca_portal/models/loadable_resource_abc.py
@@ -1,7 +1,12 @@
-from django.db import models
+from abc import abstractmethod
 
+from django.db import models
+from django.db.models import QuerySet
+
+from scpca_portal import utils
 from scpca_portal.config.logging import get_and_configure_logger
 from scpca_portal.enums import LoadableResourceStates
+from scpca_portal.models.original_file import OriginalFile
 
 logger = get_and_configure_logger(__name__)
 
@@ -13,3 +18,13 @@ class LoadableResourceABC(models.Model):
     state = models.TextField(choices=LoadableResourceStates.choices)
     hash = models.CharField(max_length=32, null=True)
     loaded_at = models.DateTimeField(null=True)
+
+    @property
+    @abstractmethod
+    def original_files(self) -> QuerySet[OriginalFile]:
+        pass
+
+    @property
+    def current_hash(self) -> str:
+        original_file_hashes = self.original_files.values_list("hash", flat=True)
+        return utils.hash_values(original_file_hashes)
