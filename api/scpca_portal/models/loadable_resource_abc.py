bedd8fd0eb4ab4790b647fba10e6bb81c3529680
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-07-30 15:57:09 -0400

    add default of NEW to loaded_state, move docstring

--- api/scpca_portal/models/loadable_resource_abc.py
+++ api/scpca_portal/models/loadable_resource_abc.py
@@ -20,7 +20,9 @@ class LoadableResourceABC(TimestampedModel):
     class Meta:
         abstract = True
 
-    loaded_state = models.TextField(choices=LoadableResourceStates.choices)
+    loaded_state = models.TextField(
+        choices=LoadableResourceStates.choices, default=LoadableResourceStates.NEW
+    )
     loaded_hash = models.CharField(max_length=32, null=True)
     loaded_at = models.DateTimeField(null=True)
 
@@ -67,6 +69,10 @@ class LoadableResourceABC(TimestampedModel):
     @property
     @abstractmethod
     def loaded_original_files(self) -> QuerySet[OriginalFile]:
+        """
+        This property returns all original files associated with the resource
+        and its subordinate relations, whether downloadable or not.
+        """
         pass
 
     @property
