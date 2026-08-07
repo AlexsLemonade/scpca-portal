385c4b9c32ac7be73f931cdfcba2da4c872853cd
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-08-07 14:23:47 -0400

    improve code clarity and performance in sync_original_files command

--- api/scpca_portal/management/commands/sync_original_files.py
+++ api/scpca_portal/management/commands/sync_original_files.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from collections import defaultdict
 from datetime import datetime
 from typing import List
 
@@ -6,9 +7,9 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils.timezone import make_aware
 
-from scpca_portal import common, lockfile, s3, utils
+from scpca_portal import s3, utils
 from scpca_portal.config.logging import get_and_configure_logger
-from scpca_portal.models import OriginalFile, Project
+from scpca_portal.models import OriginalFile
 
 logger = get_and_configure_logger(__name__)
 
@@ -48,31 +49,39 @@ class Command(BaseCommand):
 
     def sync_original_files(self, bucket: str, allow_bucket_wipe: bool, **kwargs) -> None:
         logger.info("Initiating listing of bucket objects...")
-
-        locked_project_ids = lockfile.get_locked_project_ids()
-        Project.lock_projects(locked_project_ids)
-
         bucket_objects = s3.list_bucket_objects(bucket)
-        passed_objects = utils.exclude_dicts_by_value_substrings(
-            bucket_objects, "s3_key", locked_project_ids
-        )
-
-        if lockfile_objects := utils.filter_dicts_by_value_substrings(
-            bucket_objects, "s3_key", [common.LOCKFILE_FILE_SUFFIX]
-        ):
-            passed_objects += lockfile_objects
-
-        logger.info("Syncing database...")
         sync_timestamp = make_aware(datetime.now())
 
+        original_files_by_project_id = defaultdict(list)
+        lockfiles = []
+        for bucket_object in bucket_objects:
+            original_file = OriginalFile.get_from_dict(bucket_object, bucket, sync_timestamp)
+
+            project_key = original_file.project_id if original_file.project_id else "portal-wide"
+            original_files_by_project_id[project_key].append(original_file)
+
+            if original_file.is_lockfile:
+                lockfiles.append(original_file)
+
+        for lockfile in lockfiles:
+            del original_files_by_project_id[lockfile.project_id]
+
+        syncable_original_files = utils.flatten_contents(
+            [original_files_by_project_id.values(), lockfiles]
+        )
+
+        logger.info("Syncing database...")
         logger.info("Updating modified existing OriginalFiles.")
-        updated_files = OriginalFile.bulk_update_from_dicts(passed_objects, bucket, sync_timestamp)
+        updated_files = OriginalFile.bulk_update(syncable_original_files)
 
         logger.info("Inserting new OriginalFiles.")
-        created_files = OriginalFile.bulk_create_from_dicts(passed_objects, bucket, sync_timestamp)
+        created_files = OriginalFile.bulk_create(syncable_original_files)
 
         logger.info("Purging OriginalFiles that were deleted from s3.")
-        deleted_files = OriginalFile.purge_deleted_files(bucket, sync_timestamp, allow_bucket_wipe)
+        lockfile_project_ids = [lockfile.project_id for lockfile in lockfiles]
+        deleted_files = OriginalFile.purge_deleted_files(
+            bucket, sync_timestamp, lockfile_project_ids, allow_bucket_wipe
+        )
 
         logger.info("Database syncing complete!")
 
