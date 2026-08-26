a4b84c6fb15508091ea5528700c3c185b3a24057
Nozomi Ichihara <nozomione.online@gmail.com>
2026-08-26 17:27:02 -0400

    move the ccdlName to config/ccdlDatasets and include all varients (not required to handle the exclusion of invalid CCDI deep link combinations on the client)

--- client/app/src/config/federation/ccdiDatasets.js
+++
@@ -1,9 +0,0 @@
-// A list of allowed CCDL names for CCDI deep links
-// Aligns with api/scpca_portal/enums/ccdl_dataset_names.py
-// TODO: Multiplexed and spatial samples are excluded for now (revisit later)
-export const allowedCCDLNames = [
-  'SINGLE_CELL_SINGLE_CELL_EXPERIMENT',
-  'SINGLE_CELL_SINGLE_CELL_EXPERIMENT_MERGED',
-  'SINGLE_CELL_ANN_DATA',
-  'SINGLE_CELL_ANN_DATA_MERGED'
-]
