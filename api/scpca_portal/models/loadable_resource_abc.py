2f8e1ba09a69273f8ae7f5ff9dd9d56c42387057
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-08-27 16:57:06 -0400

    implement ModelOutputCounts TypedDict and have sync_model methods return output counts

--- api/scpca_portal/models/loadable_resource_abc.py
+++ api/scpca_portal/models/loadable_resource_abc.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
+from collections import Counter
 from datetime import datetime, timedelta
-from typing import Any, Dict, Iterable, List, Set
+from typing import Any, Dict, Iterable, List, Set, TypedDict
 
 from django.conf import settings
 from django.db import models
@@ -16,6 +17,14 @@ from scpca_portal.models.base import TimestampedModel
 from scpca_portal.models.original_file import OriginalFile
 
 logger = get_and_configure_logger(__name__)
+
+
+class ModelOutputCounts(TypedDict):
+    created: int
+    deleted: int
+    locked: int
+    unlocked: int
+    tainted: int
 
 
 class LoadableResourceABC(TimestampedModel):
@@ -215,7 +224,7 @@ class LoadableResourceABC(TimestampedModel):
         pass
 
     @classmethod
-    def taint_and_lock_objects(cls, metadata_dicts_by_ids: Dict[str, Dict]) -> None:
+    def taint_and_lock_objects(cls, metadata_dicts_by_ids: Dict[str, Dict]) -> tuple[int, int, int]:
         """
         This method handles three independent actions:
         1. Locking resources who's projects are associated with a lockfile in the OF table
@@ -223,6 +232,8 @@ class LoadableResourceABC(TimestampedModel):
         3. Determining whether unlocked loaded resources have been tainted since being locked.
             If so setting their state to TAINTED, and if not resetting their state to SYNCED.
         """
+        output_counts = Counter()
+
         lockfile_project_ids = list(
             OriginalFile.objects.filter(is_lockfile=True).values_list("project_id", flat=True)
         )
@@ -231,6 +242,7 @@ class LoadableResourceABC(TimestampedModel):
             **cls.get_lockfile_filter_kwargs(lockfile_project_ids)
         )
         cls.bulk_update_loaded_state(locked_resources, LoadableResourceStates.LOCKED)
+        output_counts.update({"locked": locked_resources.count()})
 
         unlocked_resources = cls.objects.filter(loaded_state=LoadableResourceStates.LOCKED).exclude(
             **cls.get_lockfile_filter_kwargs(lockfile_project_ids)
@@ -255,6 +267,10 @@ class LoadableResourceABC(TimestampedModel):
 
         cls.bulk_update_loaded_state(tainted_resources, LoadableResourceStates.TAINTED)
         cls.bulk_update_loaded_state(synced_resources, LoadableResourceStates.SYNCED)
+        output_counts.update({"tainted": len(tainted_resources)})
+        output_counts.update({"unlocked": len(synced_resources)})
+
+        return (output_counts["locked"], output_counts["unlocked"], output_counts["tainted"])
 
     @classmethod
     def sync_model(
@@ -262,11 +278,21 @@ class LoadableResourceABC(TimestampedModel):
         *,
         bucket: str = settings.AWS_S3_INPUT_BUCKET_NAME,
         skip_existing_file_download: bool = False,
-    ) -> None:
+    ) -> ModelOutputCounts:
         metadata_dicts_by_id = cls.get_metadata_dicts_by_id(
             bucket=bucket, skip_existing_file_download=skip_existing_file_download
         )
 
-        cls.create_new_objects(metadata_dicts_by_id)
-        cls.remove_deleted_objects(metadata_dicts_by_id)
-        cls.taint_and_lock_objects(metadata_dicts_by_id)
+        created_count = len(cls.create_new_objects(metadata_dicts_by_id))
+        deleted_count, _ = cls.remove_deleted_objects(metadata_dicts_by_id)
+        locked_count, unlocked_count, tainted_count = cls.taint_and_lock_objects(
+            metadata_dicts_by_id
+        )
+
+        return {
+            "created": created_count,
+            "deleted": deleted_count,
+            "locked": locked_count,
+            "unlocked": unlocked_count,
+            "tainted": tainted_count,
+        }
