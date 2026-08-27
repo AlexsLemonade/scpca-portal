9e7bd4f070b4c2ba74e0ff5e1e7b46e1be2d3864
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-08-27 16:58:57 -0400

    log out output counts in management command

--- api/scpca_portal/management/commands/sync_models.py
+++ api/scpca_portal/management/commands/sync_models.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
+from operator import itemgetter
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from scpca_portal.config.logging import get_and_configure_logger
 from scpca_portal.models import Library, Project, Sample
+from scpca_portal.models.loadable_resource_abc import ModelOutputCounts
 
 logger = get_and_configure_logger(__name__)
 
@@ -21,15 +23,30 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs) -> None:
         self.sync_models(**kwargs)
 
+    def format_model_changes(self, model_name: str, model_output_counts: ModelOutputCounts) -> str:
+        created, deleted, locked, unlocked, tainted = itemgetter(
+            "created", "deleted", "locked", "unlocked", "tainted"
+        )(model_output_counts)
+        return (
+            f"{model_name}: {created} created, {deleted} deleted, {locked} locked, "
+            f"{tainted} locked resources tainted, "
+            f"and {unlocked} locked resources unlocked and untouched."
+        )
+
     def sync_models(self, bucket: str, skip_existing_file_download: bool, **kwargs) -> None:
         logger.info("Syncing models...")
 
-        Project.sync_model(bucket=bucket, skip_existing_file_download=skip_existing_file_download)
-        Sample.sync_model(bucket=bucket, skip_existing_file_download=skip_existing_file_download)
-        Library.sync_model(bucket=bucket, skip_existing_file_download=skip_existing_file_download)
-
-        # TODO: Should the sync_model methods return anything that could be logged out here?
-        # For example, how many Projects, Samples, and Libraries have been created and deleted?
-        # Tainted/Locked/Unlocked as well?
+        project_output_counts = Project.sync_model(
+            bucket=bucket, skip_existing_file_download=skip_existing_file_download
+        )
+        sample_output_counts = Sample.sync_model(
+            bucket=bucket, skip_existing_file_download=skip_existing_file_download
+        )
+        library_output_counts = Library.sync_model(
+            bucket=bucket, skip_existing_file_download=skip_existing_file_download
+        )
 
         logger.info("Models syncing complete.")
+        logger.info(self.format_model_changes("Project", project_output_counts))
+        logger.info(self.format_model_changes("Sample", sample_output_counts))
+        logger.info(self.format_model_changes("Library", library_output_counts))
