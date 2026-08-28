b561023f11cd71b5e07eb2a11f87fe56e31b5b0f
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-08-28 12:31:07 -0400

    clean up sync_aggregations method on abc

--- api/scpca_portal/models/aggregatable_resource_abc.py
+++ api/scpca_portal/models/aggregatable_resource_abc.py
@@ -1,7 +1,8 @@
 from abc import abstractmethod
-from typing import List, Self
+from typing import Self
 
 from django.db import models
+from django.db.models import QuerySet
 
 from scpca_portal.config.logging import get_and_configure_logger
 from scpca_portal.models.base import TimestampedModel
@@ -16,23 +17,15 @@ class AggregatableResourceABC(TimestampedModel):
     aggregation_hash = models.CharField(max_length=32, null=True)
 
     @classmethod
-    def sync_aggregations(cls, synced_resource_ids: List[str]) -> None:
-        aggregating_resources = cls.get_aggregating_resources(synced_resource_ids)
+    def sync_aggregations(cls, resources: QuerySet[Self]) -> None:
+        aggregating_resources = [resource for resource in resources if resource.needs_aggregations]
 
-        for resource in aggregating_resources:
-            resource.update_aggregations()
-            resource.aggregation_hash = resource.current_aggregation_hash
+        for aggregating_resource in aggregating_resources:
+            aggregating_resource.update_aggregations()
+            aggregating_resource.aggregation_hash = aggregating_resource.current_aggregation_hash
 
         fields_to_update = [f.name for f in cls._meta.concrete_fields if not f.primary_key]
         cls.objects.bulk_update(aggregating_resources, fields=fields_to_update)
-
-    @classmethod
-    def get_aggregating_resources(cls, synced_resource_ids: List[str]) -> List[Self]:
-        return [
-            resource
-            for resource in cls.objects.filter(scpca_id__in=synced_resource_ids)
-            if resource.needs_aggregations
-        ]
 
     @property
     @abstractmethod
