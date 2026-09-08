e1bd8ed512f720c89b9defa05d2c1086db50ec5e
Avrohom Gottlieb <avrohomgottlieb511@gmail.com>
2026-09-08 12:19:08 -0400

    fix lockfile kwargs bug

--- api/scpca_portal/models/project.py
+++ api/scpca_portal/models/project.py
@@ -254,7 +254,7 @@ class Project(CommonDataAttributes, LoadableResourceABC, AggregatableResourceABC
 
     @staticmethod
     def get_lockfile_filter_kwargs(lockfile_project_ids: List) -> Dict:
-        return {"project__scpca_id__in": lockfile_project_ids}
+        return {"scpca_id__in": lockfile_project_ids}
 
     def purge(self, delete_from_s3: bool = False) -> None:
         """Purges project and its related data."""
