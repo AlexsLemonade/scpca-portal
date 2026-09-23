1a1d72db8316b502b9be55259b5d9326d03cee06
Nozomi Ichihara <nozomione.online@gmail.com>
2026-09-23 17:56:53 -0400

    update the ProjectAdditionalProcessing component to accept a projectId prop and use getDocsUrl helper

--- client/app/src/components/ProjectAdditionalProcessing.js
+++ client/app/src/components/ProjectAdditionalProcessing.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import { Box, Text } from 'grommet'
+import { getDocsUrl } from 'helpers/getDocsUrl'
 import { Link } from 'components/Link'
 import { Icon } from 'components/Icon'
 
-export const ProjectAdditionalProcessing = ({ additionalProcessing }) => {
-  if (!additionalProcessing || !Object.keys(additionalProcessing).length)
+export const ProjectAdditionalProcessing = ({
+  projectId,
+  additionalProcessingDetails
+}) => {
+  if (!Object.keys(additionalProcessingDetails).length)
     return <Text italic>Not Specified</Text>
 
-  const { additional_processing: processingName, link } = additionalProcessing
+  const {
+    additional_processing: additionalProcessing,
+    has_additional_documentation: hasLink
+  } = additionalProcessingDetails
+
+  const docsLink = `${projectId}.html`
 
   return (
     <Box direction="row" gap="xsmall">
-      <Text>{processingName}</Text>
-      {link && (
-        <Link href={link} newTab>
+      <Text>{additionalProcessing}</Text>
+      {hasLink && (
+        <Link href={getDocsUrl(docsLink)} newTab>
           <Icon size="small" name="Help" />
         </Link>
       )}
